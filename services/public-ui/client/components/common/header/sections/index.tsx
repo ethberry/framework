3c0881bcd7b51ea62c33c752ade9b5b696949711
TrejGun <trejgun@gmail.com>
2021-09-05 15:03:27 +0800

    feat: better avatar

--- services/public-ui/client/components/common/header/sections/index.tsx
+++ services/public-ui/client/components/common/header/sections/index.tsx
@@ -81,10 +81,14 @@ export const Sections: FC = () => {
         onClick={handleMenuOpen}
         color="inherit"
       >
-        <Avatar
-          alt={`${user.profile.firstName} ${user.profile.lastName}`}
-          src={user.profile.imageUrl ?? "/img/icons/user.png"}
-        />
+        {user.profile.imageUrl ? (
+          <Avatar alt={`${user.profile.firstName} ${user.profile.lastName}`} src={user.profile.imageUrl} />
+        ) : (
+          <Avatar>
+            {user.profile.firstName.substr(0, 1)}
+            {user.profile.lastName.substr(0, 1)}
+          </Avatar>
+        )}
       </IconButton>
       <Menu id="material-appbar" anchorEl={anchor} open={!!anchor} onClose={handleMenuClose}>
         <MenuItem
