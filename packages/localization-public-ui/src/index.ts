59522707bc3cbec3b72c4df922924079958513c8
TrejGun <trejgun@gmail.com>
2021-12-20 13:40:29 +0800

    fix: i18n

--- packages/localization-public-ui/src/index.ts
+++ packages/localization-public-ui/src/index.ts
@@ -4,11 +4,8 @@ import { EnabledLanguages } from "@gemunion/framework-constants";
 import en from "../i18n/en.json";
 // @ts-ignore
 import ru from "../i18n/ru.json";
-// @ts-ignore
-import ua from "../i18n/ua.json";
 
 export const i18n: Record<EnabledLanguages, any> = {
   [EnabledLanguages.EN]: en,
   [EnabledLanguages.RU]: ru,
-  [EnabledLanguages.UA]: ua,
 };
