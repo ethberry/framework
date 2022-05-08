b09c177281b1fdcbaf78ec4466429d39508d6e8f
TrejGun <trejgun@gmail.com>
2022-05-08 08:25:20 +0800

    test: sample test

--- services/admin-api/src/common/test/entity.ts
+++ services/admin-api/src/common/test/entity.ts
@@ -1,6 +1,6 @@
 import { v4 } from "uuid";
 
-import { IUser } from "@framework/types";
+import { IUser, UserRole, UserStatus } from "@framework/types";
 import { EnabledLanguages, imageUrl } from "@framework/constants";
 
 export const generateTestUser = (data: Partial<IUser> = {}): Partial<IUser> => {
@@ -12,6 +12,9 @@ export const generateTestUser = (data: Partial<IUser> = {}): Partial<IUser> => {
       password: "97a609f782839fa886c8ae797d8d66f4a5138c2b02fb0dcab39ff74b85bc35fe", // My5up3r5tr0ngP@55w0rd
       imageUrl,
       comment: "Fraud!",
+      sub: v4(),
+      userStatus: UserStatus.ACTIVE,
+      userRoles: [UserRole.USER],
     },
     data,
   );
