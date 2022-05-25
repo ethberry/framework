cd152a624862ab9d1cf8290fc5bc564d1cf723ad
TrejGun <trejgun@gmail.com>
2022-05-25 09:38:47 +0800

    fix: copy over collections

--- microservices/json/src/erc1155/collection/collection.entity.ts
+++ microservices/json/src/erc1155/collection/collection.entity.ts
@@ -2,7 +2,7 @@ import { Column, Entity, OneToMany } from "typeorm";
 import { Mixin } from "ts-mixer";
 
 import { ContractBaseEntity, SearchableEntity } from "@gemunion/nest-js-module-typeorm-helpers";
-import { Erc1155CollectionStatus, IErc1155Collection } from "@framework/types";
+import { Erc1155CollectionStatus, Erc1155TokenTemplate, IErc1155Collection } from "@framework/types";
 import { ns } from "@framework/constants";
 
 import { Erc1155TokenEntity } from "../token/token.entity";
@@ -12,11 +12,20 @@ export class Erc1155CollectionEntity extends Mixin(ContractBaseEntity, Searchabl
   @Column({ type: "varchar" })
   public imageUrl: string;
 
+  @Column({ type: "varchar" })
+  public baseTokenURI: string;
+
   @Column({
     type: "enum",
     enum: Erc1155CollectionStatus,
   })
   public collectionStatus: Erc1155CollectionStatus;
+
+  @Column({
+    type: "enum",
+    enum: Erc1155TokenTemplate,
+  })
+  public template: Erc1155TokenTemplate;
 
   @OneToMany(_type => Erc1155TokenEntity, token => token.erc1155Collection)
   public erc1155Tokens: Array<Erc1155TokenEntity>;
