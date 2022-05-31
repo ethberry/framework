7ab8d362d84e6426a44afcfe9c6ea4a6f0d6a2cd
TrejGun <trejgun@gmail.com>
2022-05-31 12:52:52 +0800

    feat: add confirmation to increment nonce dialog

--- services/market-ui/src/components/buttons/seaport/increment-nonce/index.tsx
+++ services/market-ui/src/components/buttons/seaport/increment-nonce/index.tsx
@@ -1,19 +1,42 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Button } from "@mui/material";
 import { FormattedMessage } from "react-intl";
+
+import { ConfirmationDialog } from "@gemunion/mui-dialog-confirmation";
 
 import { useSeaport } from "../../../providers/seaport";
 
 export const SeaportIncrementNonceButton: FC = () => {
+  const [isConfirmationDialogOpen, setIsConfirmationDialogOpen] = useState(false);
+
   const seaport = useSeaport();
 
   const handleIncrementNonce = () => {
+    setIsConfirmationDialogOpen(true);
+  };
+
+  const handleConfirm = () => {
     return seaport.incrementNonce();
   };
 
+  const handleCancel = () => {
+    setIsConfirmationDialogOpen(false);
+  };
+
   return (
-    <Button onClick={handleIncrementNonce} data-testid="SeaportIncrementNonceButton">
-      <FormattedMessage id="form.buttons.nonce" />
-    </Button>
+    <>
+      <Button onClick={handleIncrementNonce} data-testid="SeaportIncrementNonceButton">
+        <FormattedMessage id="form.buttons.nonce" />
+      </Button>
+      <ConfirmationDialog
+        maxWidth="xs"
+        data-testid="SeaportIncrementNonceDialog"
+        onConfirm={handleConfirm}
+        onCancel={handleCancel}
+        open={isConfirmationDialogOpen}
+      >
+        <FormattedMessage id="dialogs.nonce" />
+      </ConfirmationDialog>
+    </>
   );
 };
