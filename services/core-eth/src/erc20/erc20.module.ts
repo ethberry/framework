6f35a04b404cd4980a1290738af5db2c45410a22
TrejGun <trejgun@gmail.com>
2022-06-02 19:33:29 +0800

    chore: some bullshit

--- services/core-eth/src/erc20/erc20.module.ts
+++ services/core-eth/src/erc20/erc20.module.ts
@@ -3,7 +3,6 @@ import { Module } from "@nestjs/common";
 import { Erc20StakingModule } from "./staking/staking.module";
 import { Erc20TokenModule } from "./token/token.module";
 
-// todo move out staking for Log's sake
 @Module({
   imports: [Erc20TokenModule, Erc20StakingModule],
 })
