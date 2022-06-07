38c010ae440c1c8c2402263be5c0c93cc5b25934
Ubudragon <gemuniongroup@gmail.com>
2022-06-07 22:38:26 +0800

    refactor: env test

--- scripts/copy-env.sh
+++ scripts/copy-env.sh
@@ -6,9 +6,14 @@ set -e # this will cause the shell to exit immediately if any command exits with
 
 cp -rf $PWD/services/admin-api/.env.sample $PWD/services/admin-api/.env.test
 cp -rf $PWD/services/admin-ui/.env.sample $PWD/services/admin-ui/.env.test
+cp -rf $PWD/services/mobile-api/.env.sample $PWD/services/mobile-api/.env.test
 cp -rf $PWD/services/market-api/.env.sample $PWD/services/market-api/.env.test
 cp -rf $PWD/services/market-ui/.env.sample $PWD/services/market-ui/.env.test
 cp -rf $PWD/services/core-eth/.env.sample $PWD/services/core-eth/.env.test
+cp -rf $PWD/services/seaport-eth/.env.sample $PWD/services/seaport-eth/.env.test
+cp -rf $PWD/microservices/eml/.env.sample $PWD/microservices/eml/.env.test
+cp -rf $PWD/microservices/game/.env.sample $PWD/microservices/game/.env.test
+cp -rf $PWD/microservices/json/.env.sample $PWD/microservices/json/.env.test
 
 echo -e "\033[34mAll done!\n\033[0m";
 
