424660058bba454ddac3583ee8c75060a29f4e6e
TrejGun <trejgun@gmail.com>
2022-06-08 18:30:53 +0800

    fix: validation for erc20 token

--- services/admin-api/src/erc20/token/dto/update.ts
+++ services/admin-api/src/erc20/token/dto/update.ts
@@ -1,5 +1,5 @@
 import { ApiPropertyOptional } from "@nestjs/swagger";
-import { IsEnum, IsJSON, IsOptional, Validate } from "class-validator";
+import { IsEnum, IsJSON, IsOptional, IsString, Validate } from "class-validator";
 import { Transform } from "class-transformer";
 
 import { ForbidEnumValues } from "@gemunion/nest-js-validators";
@@ -10,12 +10,18 @@ import { IErc20TokenUpdateDto } from "../interfaces";
 export class Erc20TokenUpdateDto implements IErc20TokenUpdateDto {
   @ApiPropertyOptional()
   @IsOptional()
+  @IsString({ message: "typeMismatch" })
+  public title: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
   @IsJSON({ message: "patternMismatch" })
   public description: string;
 
   @ApiPropertyOptional({
     enum: Erc20TokenStatus,
   })
+  @IsOptional()
   @Transform(({ value }) => value as Erc20TokenStatus)
   @IsEnum(Erc20TokenStatus, { message: "badInput" })
   @Validate(ForbidEnumValues, [Erc20TokenStatus.NEW])
