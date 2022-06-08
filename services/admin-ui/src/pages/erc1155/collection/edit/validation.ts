f8c5e0a03333c6b31a16509cfee4bcd9be7929eb
TrejGun <trejgun@gmail.com>
2022-06-08 18:22:16 +0800

    fix: validation for collections

--- services/admin-ui/src/pages/erc1155/collection/edit/validation.ts
+++ services/admin-ui/src/pages/erc1155/collection/edit/validation.ts
@@ -5,6 +5,5 @@ import { draftValidationSchema } from "@gemunion/yup-rules";
 export const validationSchema = Yup.object().shape({
   title: Yup.string().required("form.validations.valueMissing"),
   description: draftValidationSchema,
-  address: Yup.string().required("form.validations.valueMissing"),
   imageUrl: Yup.string().required("form.validations.valueMissing"),
 });
