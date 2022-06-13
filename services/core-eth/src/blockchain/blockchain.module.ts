d5ae99b62fa3a16a7c84ada2ed98c3c71bf70263
Ubudragon <gemuniongroup@gmail.com>
2022-06-13 18:12:45 +0800

    fix: log modules

--- services/core-eth/src/blockchain/blockchain.module.ts
+++ services/core-eth/src/blockchain/blockchain.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 
 import { ContractManagerModule } from "./contract-manager/contract-manager.module";
+import { ContractManagerModuleEth } from "./contract-manager/contract-manager.module.eth";
 import { AccessControlModule } from "./access-control/access-control.module";
 import { StakingModule } from "./staking/staking.module";
 
 @Module({
-  imports: [ContractManagerModule, AccessControlModule, StakingModule],
+  imports: [ContractManagerModule, ContractManagerModuleEth, AccessControlModule, StakingModule],
 })
 export class BlockchainModule {}
--- services/core-eth/src/erc721/marketplace/marketplace.module.ts
+++ services/core-eth/src/erc721/marketplace/marketplace.module.ts
@@ -1,16 +1,19 @@
 import { Logger, Module } from "@nestjs/common";
 
+import { ContractManagerModule } from "../../blockchain/contract-manager/contract-manager.module";
+
 import { Erc721TokenModule } from "../token/token.module";
 import { Erc721DropboxModule } from "../dropbox/dropbox.module";
 import { Erc721TemplateModule } from "../template/template.module";
 import { Erc721MarketplaceServiceEth } from "./marketplace.service.eth";
 import { Erc721MarketplaceControllerEth } from "./marketplace.controller.eth";
 import { Erc721MarketplaceHistoryModule } from "./marketplace-history/marketplace-history.module";
-import { ContractManagerModule } from "../../blockchain/contract-manager/contract-manager.module";
+import { Erc721MarketplaceLogModule } from "./marketplace-log/marketplace-log.module";
 
 @Module({
   imports: [
     ContractManagerModule,
+    Erc721MarketplaceLogModule,
     Erc721MarketplaceHistoryModule,
     Erc721TokenModule,
     Erc721DropboxModule,
