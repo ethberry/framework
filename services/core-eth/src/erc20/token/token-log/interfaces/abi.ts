45ffb5a4f2455e96e06bd8656862c2b26406cc74
Ubudragon <gemuniongroup@gmail.com>
2022-06-13 20:46:04 +0800

    fix: abi

--- services/core-eth/src/erc20/token/token-log/interfaces/abi.ts
+++ services/core-eth/src/erc20/token/token-log/interfaces/abi.ts
@@ -4,7 +4,7 @@ import { FormatTypes, Interface } from "@ethersproject/abi";
 import ERC20SimpleSol from "@framework/core-contracts/artifacts/contracts/ERC20/ERC20Simple.sol/ERC20Simple.json";
 import ERC20BlackListSol from "@framework/core-contracts/artifacts/contracts/ERC20/ERC20BlackList.sol/ERC20BlackList.json";
 
-const iface1 = new Interface(ERC20SimpleSol.abi).format(FormatTypes.minimal) as Array<string>;
-const iface2 = new Interface(ERC20BlackListSol.abi).format(FormatTypes.minimal) as Array<string>;
+const iface1 = new Interface(ERC20SimpleSol.abi).format(FormatTypes.full) as Array<string>;
+const iface2 = new Interface(ERC20BlackListSol.abi).format(FormatTypes.full) as Array<string>;
 
 export const Erc20abi = [...new Set(iface1.concat(iface2))];
