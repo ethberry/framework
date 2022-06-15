56fd6179188a9c391371d711562a2319e56d0e5e
TrejGun <trejgun@gmail.com>
2022-06-15 20:41:51 +0800

    feat: check if wallet already in use

--- services/market-api/src/profile/profile.service.ts
+++ services/market-api/src/profile/profile.service.ts
@@ -2,25 +2,30 @@ import { BadRequestException, Injectable } from "@nestjs/common";
 import { IMetamaskDto, MetamaskService } from "@gemunion/nest-js-module-metamask";
 
 import { UserEntity } from "../user/user.entity";
+import { UserService } from "../user/user.service";
 import { IProfileUpdateDto } from "./interfaces";
 
 @Injectable()
 export class ProfileService {
-  constructor(private readonly metamaskService: MetamaskService) {}
+  constructor(private readonly metamaskService: MetamaskService, private readonly userService: UserService) {}
 
   public update(userEntity: UserEntity, dto: IProfileUpdateDto): Promise<UserEntity> {
     Object.assign(userEntity, dto);
     return userEntity.save();
   }
 
-  public attach(userEntity: UserEntity, dto: IMetamaskDto): Promise<UserEntity> {
+  public async attach(userEntity: UserEntity, dto: IMetamaskDto): Promise<UserEntity> {
     const { signature, wallet, nonce } = dto;
 
     if (!this.metamaskService.isValidSignature({ signature, wallet: wallet.toLowerCase(), nonce })) {
       throw new BadRequestException("signatureDoesNotMatch");
     }
 
-    Object.assign(userEntity, { wallet });
+    const count = await this.userService.count({ wallet });
+    if (count) {
+      throw new BadRequestException("walletAlreadyInUse");
+    }
+
     return userEntity.save();
   }
 
