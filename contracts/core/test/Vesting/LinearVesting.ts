776ba19bd7521548db5622ece03ce87132f8c414
TrejGun <trejgun@gmail.com>
2022-07-11 14:04:18 +0800

    test: vesting

--- contracts/core/test/Vesting/LinearVesting.ts
+++ contracts/core/test/Vesting/LinearVesting.ts
@@ -5,15 +5,16 @@ import { time } from "@openzeppelin/test-helpers";
 
 import { LinearVesting } from "../../typechain-types";
 
-describe.skip("LinearVesting", function () {
+describe("LinearVesting", function () {
   const span = 2500;
   let vestingInstance: LinearVesting;
 
   beforeEach(async function () {
     [this.owner, this.receiver] = await ethers.getSigners();
-    const vestingFactory = await ethers.getContractFactory("LinearVesting");
 
-    vestingInstance = await vestingFactory.deploy(this.receiver.address, Math.ceil(Date.now() / 1000), 10000);
+    const current = await time.latest();
+    const vestingFactory = await ethers.getContractFactory("LinearVesting");
+    vestingInstance = await vestingFactory.deploy(this.receiver.address, current.toNumber(), 10000);
 
     await this.owner.sendTransaction({
       to: vestingInstance.address,
@@ -25,40 +26,40 @@ describe.skip("LinearVesting", function () {
     const tx1 = await vestingInstance["release()"]();
     await expect(tx1).to.changeEtherBalances([vestingInstance, this.receiver], [0, 0]);
 
-    const current1 = await time.latestBlock();
-    await time.advanceBlockTo(current1.add(web3.utils.toBN(span)));
+    const current1 = await time.latest();
+    await time.increaseTo(current1.add(web3.utils.toBN(span)));
 
     const tx2 = await vestingInstance["release()"]();
     await expect(tx2).to.changeEtherBalances(
       [vestingInstance, this.receiver],
-      [constants.WeiPerEther.div(4).mul(-1), constants.WeiPerEther.div(4)],
+      [constants.WeiPerEther.div(100).mul(25).mul(-1), constants.WeiPerEther.div(100).mul(25)],
     );
 
-    const current2 = await time.latestBlock();
-    await time.advanceBlockTo(current2.add(web3.utils.toBN(span)));
+    const current2 = await time.latest();
+    await time.increaseTo(current2.add(web3.utils.toBN(span)));
 
     const tx3 = await vestingInstance["release()"]();
     await expect(tx3).to.changeEtherBalances(
       [vestingInstance, this.receiver],
-      [constants.WeiPerEther.div(4).mul(-1), constants.WeiPerEther.div(4)],
+      [constants.WeiPerEther.div(100).mul(25).mul(-1), constants.WeiPerEther.div(100).mul(25)],
     );
 
-    const current3 = await time.latestBlock();
-    await time.advanceBlockTo(current3.add(web3.utils.toBN(span)));
+    const current3 = await time.latest();
+    await time.increaseTo(current3.add(web3.utils.toBN(span)));
 
     const tx4 = await vestingInstance["release()"]();
     await expect(tx4).to.changeEtherBalances(
       [vestingInstance, this.receiver],
-      [constants.WeiPerEther.div(4).mul(-1), constants.WeiPerEther.div(4)],
+      [constants.WeiPerEther.div(100).mul(25).mul(-1), constants.WeiPerEther.div(100).mul(25)],
     );
 
-    const current4 = await time.latestBlock();
-    await time.advanceBlockTo(current4.add(web3.utils.toBN(span)));
+    const current4 = await time.latest();
+    await time.increaseTo(current4.add(web3.utils.toBN(span)));
 
     const tx5 = await vestingInstance["release()"]();
     await expect(tx5).to.changeEtherBalances(
       [vestingInstance, this.receiver],
-      [constants.WeiPerEther.div(4).mul(-1), constants.WeiPerEther.div(4)],
+      [constants.WeiPerEther.div(100).mul(25).mul(-1), constants.WeiPerEther.div(100).mul(25)],
     );
   });
 });
