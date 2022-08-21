8227f8b3aa0b9e94f3fc227fcf40339d59c6a91f
Ubudragon <gemuniongroup@gmail.com>
2022-08-21 17:33:50 +0800

    refactor: deploy

--- contracts/core/scripts/utils/blockAwait.ts
+++ contracts/core/scripts/utils/blockAwait.ts
@@ -6,13 +6,13 @@ function delay(ms: number) {
 
 export const blockAwait = async function (blockDelay = 2): Promise<void> {
   const initialBlock = await ethers.provider.getBlock("latest");
-  console.info("initialBlock:", initialBlock.number);
+  console.info("BlockAwait:", initialBlock.number);
   let currentBlock;
   let delayB;
   do {
     await delay(5000);
     currentBlock = await ethers.provider.getBlock("latest");
     delayB = currentBlock.number - initialBlock.number;
-    console.info("delay is:", delayB);
+    console.info("remaining delay:", blockDelay - delayB);
   } while (delayB < blockDelay);
 };
--- services/admin-api/src/migrations/1652962207610-seed-contract-manager.ts
+++ services/admin-api/src/migrations/1652962207610-seed-contract-manager.ts
@@ -9,35 +9,54 @@ export class SeedContractManager1652962207610 implements MigrationInterface {
     const contractManagerAddress = process.env.CONTRACT_MANAGER_ADDR || wallet;
     const exchangeAddr = process.env.EXCHANGE_ADDR || wallet;
     const stakingAddr = process.env.STAKING_ADDR || wallet;
+    const claimAddr = process.env.CLAIM_PROXY_ADDR || wallet;
+    const lotteryAddr = process.env.LOTTERY_ADDR || wallet;
+    const erc20Addr = process.env.ERC20_SIMPLE_ADDR || wallet;
+    const erc721Addr = process.env.ERC721_LOTTERY_ADDR || wallet;
 
     const lastBlock = process.env.STARTING_BLOCK || 0;
 
     await queryRunner.query(`
-      INSERT INTO ${ns}.contract_manager (
-        address,
-        contract_type,
-        from_block,
-        created_at,
-        updated_at
-      ) VALUES (
-        '${contractManagerAddress}',
-        'CONTRACT_MANAGER',
-        '${lastBlock}',
-        '${currentDateTime}',
-        '${currentDateTime}'
-      ), (
-        '${exchangeAddr}',
-        'EXCHANGE',
-        '${lastBlock}',
-        '${currentDateTime}',
-        '${currentDateTime}'
-      ), (
-        '${stakingAddr}',
-        'STAKING',
-        '${lastBlock}',
-        '${currentDateTime}',
-        '${currentDateTime}'
-      );
+        INSERT INTO ${ns}.contract_manager (address,
+                                            contract_type,
+                                            from_block,
+                                            created_at,
+                                            updated_at)
+        VALUES ('${contractManagerAddress}',
+                'CONTRACT_MANAGER',
+                '${lastBlock}',
+                '${currentDateTime}',
+                '${currentDateTime}'),
+               ('${exchangeAddr}',
+                'EXCHANGE',
+                '${lastBlock}',
+                '${currentDateTime}',
+                '${currentDateTime}'),
+               ('${stakingAddr}',
+                'STAKING',
+                '${lastBlock}',
+                '${currentDateTime}',
+                '${currentDateTime}'),
+               ('${lotteryAddr}',
+                'LOTTERY',
+                '${lastBlock}',
+                '${currentDateTime}',
+                '${currentDateTime}'),
+               ('${claimAddr}',
+                'CLAIM',
+                '${lastBlock}',
+                '${currentDateTime}',
+                '${currentDateTime}'),
+               ('${erc20Addr}',
+                'ERC20_TOKEN',
+                '${lastBlock}',
+                '${currentDateTime}',
+                '${currentDateTime}'),
+               ('${erc721Addr}',
+                'ERC721_TOKEN',
+                '${lastBlock}',
+                '${currentDateTime}',
+                '${currentDateTime}');
     `);
   }
 
