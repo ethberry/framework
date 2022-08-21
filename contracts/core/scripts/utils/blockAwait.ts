c783837ff50ea22982fef14cb23cb7cb3e5d410e
Ubudragon <gemuniongroup@gmail.com>
2022-08-21 15:57:24 +0800

    refactor: deploy

--- contracts/core/scripts/utils/blockAwait.ts
+++ contracts/core/scripts/utils/blockAwait.ts
@@ -1,15 +1,18 @@
 import { ethers } from "hardhat";
 
-export const blockAwait = async function (blockDelay = 2): Promise<number> {
-  return new Promise(resolve => {
-    let initialBlockNumber = 0;
-    ethers.provider.on("block", (blockNumber: number) => {
-      if (!initialBlockNumber) {
-        initialBlockNumber = blockNumber;
-      }
-      if (blockNumber === initialBlockNumber + blockDelay) {
-        resolve(blockNumber);
-      }
-    });
-  });
+function delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export const blockAwait = async function (blockDelay = 2): Promise<void> {
+  const initialBlock = await ethers.provider.getBlock("latest");
+  console.info("initialBlock:", initialBlock.number);
+  let currentBlock;
+  let delayB;
+  do {
+    await delay(5000);
+    currentBlock = await ethers.provider.getBlock("latest");
+    delayB = currentBlock.number - initialBlock.number;
+    console.info("delay is:", delayB);
+  } while (delayB < blockDelay);
 };
