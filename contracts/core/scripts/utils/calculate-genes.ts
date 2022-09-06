6dc81b27859cbdaad647d5cf0ac6c70099ed1a79
TrejGun <trejgun@gmail.com>
2022-09-06 17:57:17 +0800

    feat: genes package

--- contracts/core/scripts/utils/calculate-genes.ts
+++ contracts/core/scripts/utils/calculate-genes.ts
@@ -1,6 +1,8 @@
 import { ethers } from "hardhat";
 import { BigNumber } from "ethers";
 
+import { encodeNumbers, decodeNumber } from "@gemunion/genes";
+
 // Patch BigNumber
 Object.defineProperties(BigNumber.prototype, {
   toJSON: {
@@ -9,42 +11,6 @@ Object.defineProperties(BigNumber.prototype, {
     },
   },
 });
-
-const encodeNumbers = ({
-  strength,
-  dexterity,
-  constitution,
-  intelligence,
-  wisdom,
-  charisma,
-}: {
-  strength: number;
-  dexterity: number;
-  constitution: number;
-  intelligence: number;
-  wisdom: number;
-  charisma: number;
-}) => {
-  let encoded = BigNumber.from(0);
-  encoded = encoded.or(BigNumber.from(strength).shl(160));
-  encoded = encoded.or(BigNumber.from(dexterity).shl(128));
-  encoded = encoded.or(BigNumber.from(constitution).shl(96));
-  encoded = encoded.or(BigNumber.from(intelligence).shl(64));
-  encoded = encoded.or(BigNumber.from(wisdom).shl(32));
-  encoded = encoded.or(BigNumber.from(charisma).shl(0));
-  return encoded;
-};
-
-const decodeNumber = (encoded: BigNumber) => {
-  return {
-    strength: encoded.shr(160).mask(32).toNumber(),
-    dexterity: encoded.shr(128).mask(32).toNumber(),
-    constitution: encoded.shr(96).mask(32).toNumber(),
-    intelligence: encoded.shr(64).mask(32).toNumber(),
-    wisdom: encoded.shr(32).mask(32).toNumber(),
-    charisma: encoded.shr(0).mask(32).toNumber(),
-  };
-};
 
 export const transform = (args: any): Record<string, any> => {
   return JSON.parse(JSON.stringify(Object.fromEntries(Object.entries(args).splice(args.length)))) as Record<
