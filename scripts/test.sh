d6bd2bfb511c83f76829895b86c1024518cfebef
Ubudragon <gemuniongroup@gmail.com>
2022-09-07 21:52:21 +0800

    fix: test env [skip ci]

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,6 +8,9 @@ set -e # this will cause the shell to exit immediately if any command exits with
 export NODE_ENV=$NODE_ENV
 export POSTGRES_URL=$POSTGRES_URL
 export CHAIN_ID=$CHAIN_ID
+export PRIVATE_KEY=$PRIVATE_KEY
+export RINKEBY_RPC_URL=$RINKEBY_RPC_URL
+export GOERLI_RPC_URL=$GOERLI_RPC_URL
 
 lerna bootstrap --concurrency 1 --hoist --ignore-scripts
 lerna run build --stream
