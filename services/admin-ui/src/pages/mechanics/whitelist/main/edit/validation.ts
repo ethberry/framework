d4f613f8f1be4bb0cd0c858a89ad028b35954466
TrejGun <trejgun@gmail.com>
2022-09-14 09:26:47 +0800

    chore: select reward for whitelist

---
+++ services/admin-ui/src/pages/mechanics/whitelist/main/edit/validation.ts
@@ -0,0 +1,7 @@
+import * as Yup from "yup";
+
+import { assetValidationSchema } from "../../../../../components/inputs/price/price-schema";
+
+export const validationSchema = Yup.object().shape({
+  item: assetValidationSchema,
+});
--- services/admin-ui/src/pages/mechanics/whitelist/main/index.tsx
+++ services/admin-ui/src/pages/mechanics/whitelist/main/index.tsx
@@ -1,4 +1,4 @@
-import { FC, Fragment } from "react";
+import { FC, Fragment, useState } from "react";
 import { FormattedMessage, useIntl } from "react-intl";
 import { Button, IconButton, List, ListItem, ListItemSecondaryAction, ListItemText, Pagination } from "@mui/material";
 import { Add, Delete, TimerOutlined } from "@mui/icons-material";
@@ -14,7 +14,13 @@ import { useMetamask } from "@gemunion/react-hooks-eth";
 import WhitelistSol from "@framework/core-contracts/artifacts/contracts/Mechanics/Whitelist/Whitelist.sol/Whitelist.json";
 
 import { WhitelistSearchForm } from "./form";
+import { IWhitelistGenerateDto, WhitelistGenerateDialog } from "./edit";
 import { AccountDialog } from "../../../../components/dialogs/account";
+import { emptyItem } from "../../../../components/inputs/price/empty-price";
+
+export interface IProof {
+  proof: string;
+}
 
 export const Whitelist: FC = () => {
   const {
@@ -43,22 +49,35 @@ export const Whitelist: FC = () => {
     },
   });
 
+  const [isGenerateDialogOpen, setIsGenerateDialogOpen] = useState(false);
+
   const { formatMessage } = useIntl();
 
-  const { fn } = useApiCall(async api => {
-    return api.fetchJson({
-      url: `/whitelist/generate`,
-    });
-  });
+  const { fn } = useApiCall(
+    async api => {
+      return api.fetchJson({
+        url: `/whitelist/generate`,
+      });
+    },
+    { success: false },
+  );
 
-  const metaFn = useMetamask((proof: string, web3Context: Web3ContextType) => {
+  const metaFn = useMetamask((values: IWhitelistGenerateDto, proof: IProof, web3Context: Web3ContextType) => {
     const contract = new Contract(process.env.WHITELIST_ADDR, WhitelistSol.abi, web3Context.provider?.getSigner());
-    return contract.setReward(proof, []) as Promise<void>;
+    return contract.setReward(proof, values.item) as Promise<void>;
   });
 
-  const handleUpload = async () => {
-    const { proof } = await fn();
-    return metaFn(proof);
+  const handleUpload = () => {
+    setIsGenerateDialogOpen(true);
+  };
+
+  const handleGenerateCancel = () => {
+    setIsGenerateDialogOpen(false);
+  };
+
+  const handleGenerateConfirm = async (values: Record<string, any>) => {
+    const proof = await fn();
+    return metaFn(values, proof);
   };
 
   return (
@@ -121,6 +140,13 @@ export const Whitelist: FC = () => {
         message={selected.id ? "dialogs.edit" : "dialogs.create"}
         testId="WhitelistEditDialog"
         initialValues={selected}
+      />
+
+      <WhitelistGenerateDialog
+        onCancel={handleGenerateCancel}
+        onConfirm={handleGenerateConfirm}
+        open={isGenerateDialogOpen}
+        initialValues={{ item: emptyItem }}
       />
     </Fragment>
   );
