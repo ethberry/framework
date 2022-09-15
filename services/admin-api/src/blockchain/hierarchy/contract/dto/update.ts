9717cacc356e76cf71210c87a26385cd230d7d6c
TrejGun <trejgun@gmail.com>
2022-09-15 09:58:39 +0800

    fix: revert

--- services/admin-api/src/blockchain/hierarchy/contract/dto/update.ts
+++ services/admin-api/src/blockchain/hierarchy/contract/dto/update.ts
@@ -2,13 +2,13 @@ import { ApiPropertyOptional } from "@nestjs/swagger";
 import { IsEnum, IsOptional, IsString, IsUrl, Validate } from "class-validator";
 import { Transform } from "class-transformer";
 
-import { SearchableDto } from "@gemunion/collection";
+import { SearchableOptionalDto } from "@gemunion/collection";
 import { ForbidEnumValues } from "@gemunion/nest-js-validators";
 import { ContractStatus } from "@framework/types";
 
 import { IContractUpdateDto } from "../interfaces";
 
-export class ContractUpdateDto extends SearchableDto implements IContractUpdateDto {
+export class ContractUpdateDto extends SearchableOptionalDto implements IContractUpdateDto {
   @ApiPropertyOptional()
   @IsOptional()
   @IsUrl({}, { message: "patternMismatch" })
