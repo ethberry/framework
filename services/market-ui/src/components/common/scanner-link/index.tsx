bcfe1a3b9b0813c36ec456782cc2beece8cb2b36
TrejGun <trejgun@gmail.com>
2022-09-15 08:25:08 +0800

    fix: scanner link

--- services/market-ui/src/components/common/scanner-link/index.tsx
+++ services/market-ui/src/components/common/scanner-link/index.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import { Link } from "@mui/material";
+import { useWeb3React } from "@web3-react/core";
 
 import { networks } from "@gemunion/provider-wallet";
 
@@ -10,6 +11,7 @@ export interface IScannerLinkProps {
 export const ScannerLink: FC<IScannerLinkProps> = props => {
   const { address } = props;
 
-  // TODO get chainId from connection
-  return <Link href={`${networks[~~process.env.CHAIN_ID].blockExplorerUrls[0]}/address/${address}`}>{address}</Link>;
+  const { chainId = 1 } = useWeb3React();
+
+  return <Link href={`${networks[chainId].blockExplorerUrls[0]}/address/${address}`}>{address}</Link>;
 };
