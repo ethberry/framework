5a29b32266e9709262a0ef3788c983dfab920ac7
Karafizi Arthur <karafizi.artur@gmail.com>
2022-09-15 13:56:20 +0800

    fix: validation for contactId for infura

--- services/market-ui/src/pages/integrations/ipfs/infura/validation.ts
+++ services/market-ui/src/pages/integrations/ipfs/infura/validation.ts
@@ -1,6 +1,10 @@
 import * as Yup from "yup";
 
 export const validationSchema = Yup.object().shape({
+  contractId: Yup.number()
+    .required("form.validations.valueMissing")
+    .integer("form.validations.badInput")
+    .min(1, "form.validations.valueMissing"),
   tokenId: Yup.number()
     .required("form.validations.valueMissing")
     .integer("form.validations.badInput")
