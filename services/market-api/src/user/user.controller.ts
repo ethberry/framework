c1ff5d3f1e8add7ba626f40207bfb679adc2358f
TrejGun <trejgun@gmail.com>
2022-10-05 10:02:16 +0800

    fix: cleanup user module

--- services/market-api/src/user/user.controller.ts
+++ services/market-api/src/user/user.controller.ts
@@ -1,34 +1,15 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Put, Query, UseInterceptors } from "@nestjs/common";
+import { Controller, Get, Param, ParseIntPipe, UseInterceptors } from "@nestjs/common";
 import { ApiBearerAuth } from "@nestjs/swagger";
 
-import { NotFoundInterceptor, PaginationInterceptor, Roles } from "@gemunion/nest-js-utils";
-import { UserRole } from "@framework/types";
+import { NotFoundInterceptor } from "@gemunion/nest-js-utils";
 
 import { UserService } from "./user.service";
 import { UserEntity } from "./user.entity";
-import { UserSearchDto, UserUpdateDto } from "./dto";
 
 @ApiBearerAuth()
-@Roles(UserRole.ADMIN)
 @Controller("/users")
 export class UserController {
   constructor(private readonly userService: UserService) {}
-
-  @Get("/")
-  @UseInterceptors(PaginationInterceptor)
-  public search(@Query() dto: UserSearchDto): Promise<[Array<UserEntity>, number]> {
-    return this.userService.search(dto);
-  }
-
-  @Get("/autocomplete")
-  public autocomplete(): Promise<Array<UserEntity>> {
-    return this.userService.autocomplete();
-  }
-
-  @Put("/:id")
-  public update(@Param("id", ParseIntPipe) id: number, @Body() dto: UserUpdateDto): Promise<UserEntity> {
-    return this.userService.update({ id }, dto);
-  }
 
   @Get("/:id")
   @UseInterceptors(NotFoundInterceptor)
