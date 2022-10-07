4b15ac758abd43bcb748728cdde318f16838a008
TrejGun <trejgun@gmail.com>
2022-10-07 10:44:25 +0800

    fix: exchange utils

--- contracts/core/contracts/Mechanics/Exchange/ExchangeUtils.sol
+++ contracts/core/contracts/Mechanics/Exchange/ExchangeUtils.sol
@@ -10,12 +10,12 @@ import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 import "../../ERC1155/interfaces/IERC1155Simple.sol";
 import "../../ERC721/interfaces/IERC721Simple.sol";
 import "../../ERC721/interfaces/IERC721Random.sol";
-
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "./interfaces/IAsset.sol";
 
 contract ExchangeUtils {
   using Address for address;
