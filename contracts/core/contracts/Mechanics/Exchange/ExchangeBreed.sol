9df1ce858b41fdc8b6abb1282960566889debe7c
Ubudragon <gemuniongroup@gmail.com>
2022-10-11 15:57:47 +0800

    feat: breed limits

--- contracts/core/contracts/Mechanics/Exchange/ExchangeBreed.sol
+++ contracts/core/contracts/Mechanics/Exchange/ExchangeBreed.sol
@@ -16,6 +16,17 @@ import "./interfaces/IAsset.sol";
 import "../../ERC721/interfaces/IERC721Upgradeable.sol";
 
 abstract contract ExchangeBreed is SignatureValidator, ExchangeUtils, AccessControl, Pausable {
+
+  uint64 public _pregnancyTimeLimit = 0;
+  uint64 public _pregnancyCountLimit = 0;
+
+  struct Pregnancy {
+    uint256 timestamp;
+    uint64 count;
+  }
+
+  mapping(address => mapping(uint256 => Pregnancy)) private _breeds;
+
   event Breed(address from, uint256 externalId, Asset item, Asset price);
 
   function breed(
@@ -37,8 +48,40 @@ abstract contract ExchangeBreed is SignatureValidator, ExchangeUtils, AccessCont
     address ownerOf2 = IERC721(price.token).ownerOf(item.tokenId);
     require(ownerOf2 == account, "Exchange: Wrong signer");
 
+    pregnancyCheckup(item, price);
+
     emit Breed(account, params.externalId, item, price);
 
     IERC721Random(item.token).mintRandom(account, params.externalId);
+  }
+
+  function pregnancyCheckup(Asset memory item, Asset memory price) internal {
+    // Check pregnancy count
+    uint64 breedCountItem = _breeds[item.token][item.tokenId].count;
+    uint64 breedCountPrice = _breeds[price.token][price.tokenId].count;
+
+    require(breedCountItem >= _pregnancyCountLimit, "Exchange: pregnancy count exceeded item");
+    require(breedCountPrice >= _pregnancyCountLimit, "Exchange: pregnancy count exceeded price");
+
+    // Check pregnancy time
+    uint64 breedTime = uint64(block.timestamp);
+
+    uint256 breedTimeItem = _breeds[item.token][item.tokenId].timestamp;
+    uint256 breedTimePrice = _breeds[price.token][price.tokenId].timestamp;
+
+    require(breedTime - breedTimeItem > _pregnancyTimeLimit, "Exchange: pregnancy time limit item");
+    require(breedTime - breedTimePrice > _pregnancyTimeLimit, "Exchange: pregnancy time limit price");
+
+    // Update Pregnancy
+    _breeds[item.token][item.tokenId].count += 1;
+    _breeds[price.token][price.tokenId].count += 1;
+
+    _breeds[item.token][item.tokenId].timestamp = breedTime;
+    _breeds[price.token][price.tokenId].timestamp = breedTime;
+  }
+
+  function setPregnancyLimits(uint64 timeLimit, uint64 countLimit) public onlyRole(DEFAULT_ADMIN_ROLE) {
+    _pregnancyTimeLimit = timeLimit;
+    _pregnancyCountLimit = countLimit;
   }
 }
