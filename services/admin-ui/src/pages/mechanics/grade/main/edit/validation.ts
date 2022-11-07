7762991403e8f7450ed34daa7d66d3c247479f5a
Karafizi Arthur <karafizi.artur@gmail.com>
2022-11-07 08:42:49 +0800

    fix: growthRate validation

--- services/admin-ui/src/pages/mechanics/grade/main/edit/validation.ts
+++ services/admin-ui/src/pages/mechanics/grade/main/edit/validation.ts
@@ -1,8 +1,13 @@
 import * as Yup from "yup";
 
+import { GradeStrategy } from "@framework/types";
+
 import { assetValidationSchema } from "../../../../../components/inputs/price/price-schema";
 
 export const validationSchema = Yup.object().shape({
-  growthRate: Yup.number().required("form.validations.valueMissing"),
+  growthRate: Yup.number().when("gradeStrategy", {
+    is: (gradeStrategy: GradeStrategy) => gradeStrategy === GradeStrategy.EXPONENTIAL,
+    then: Yup.number().required("form.validations.valueMissing"),
+  }),
   price: assetValidationSchema,
 });
