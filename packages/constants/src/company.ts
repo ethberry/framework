d638031e2b8f9326805a8662f3336c910313bfff
Ubudragon <gemuniongroup@gmail.com>
2022-12-16 12:06:17 +0800

    feat: add gemunion besu 13377

--- packages/constants/src/company.ts
+++ packages/constants/src/company.ts
@@ -1,5 +1,5 @@
 export const companyName = "Gemunion Framework";
 export const ns = "gemunion";
 
-export const availableChains = [1, 5, 56, 137, 1337, 80001];
+export const availableChains = [1, 5, 56, 137, 1337, 13377, 80001];
 export const testChainId = 13378;
