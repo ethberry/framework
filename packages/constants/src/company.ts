2a76aa42da2302bfcbd5702ae9682f50504df608
TrejGun <trejgun@gmail.com>
2022-12-20 20:26:24 +0800

    fix: test networks

--- packages/constants/src/company.ts
+++ packages/constants/src/company.ts
@@ -1,5 +1,5 @@
 export const companyName = "Gemunion Framework";
 export const ns = "gemunion";
 
-export const availableChains = [1, 5, 56, 137, 1337, 13377, 80001];
+export const availableChains = [1, 5, 56, 137, 13377, 13378, 80001];
 export const testChainId = 13378;
