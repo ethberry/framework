9d80381054b7df017edc1d8767bd5d63c9cc7a51
TrejGun <trejgun@gmail.com>
2022-12-27 21:55:24 +0800

    test: contracts mocks

---
+++ contracts/core/contracts/MOCKS/import.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+
+// Author: TrejGun
+// Email: trejgun+gemunion@gmail.com
+// Website: https://gemunion.io/
+
+pragma solidity ^0.8.9;
+
+import "@gemunion/contracts-mocks/contracts/ERC20NonReceiver.sol";
+import "@gemunion/contracts-mocks/contracts/ERC721NonReceiver.sol";
+import "@gemunion/contracts-mocks/contracts/ERC721Receiver.sol";
+import "@gemunion/contracts-mocks/contracts/ERC1155NonReceiver.sol";
+import "@gemunion/contracts-mocks/contracts/ERC1155Receiver.sol";
