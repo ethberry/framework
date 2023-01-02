887ffd10866e777abf7ff85d362768358a45f874
TrejGun <trejgun@gmail.com>
2023-01-02 14:00:46 +0800

    feat: add withdraw method to wallet

--- contracts/core/contracts/Mechanics/Wallet/Wallet.sol
+++ contracts/core/contracts/Mechanics/Wallet/Wallet.sol
@@ -8,6 +8,12 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
 
-contract Wallet is PaymentSplitter {
+import "../Exchange/ExchangeUtils.sol";
+
+contract Wallet is PaymentSplitter, ExchangeUtils {
   constructor(address[] memory payees, uint256[] memory shares) payable PaymentSplitter(payees, shares) {}
+
+  function withdraw(Asset[] memory price) public {
+    return spend(price, address(this), _msgSender());
+  }
 }
