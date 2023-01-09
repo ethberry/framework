ac1651a313a390d2ae86ecb016a363fb8475b99d
JenyaFomin <jenya.stasik@gmail.com>
2023-01-09 10:10:00 +0800

    fetch: blockchain.mechanics.asset add transactions to update method

--- services/admin-api/src/blockchain/mechanics/asset/asset.service.ts
+++ services/admin-api/src/blockchain/mechanics/asset/asset.service.ts
@@ -1,11 +1,12 @@
-import { forwardRef, Inject, Injectable, NotFoundException } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { DeepPartial, FindOneOptions, FindOptionsWhere, Repository } from "typeorm";
+import { DataSource, DeepPartial, FindOneOptions, FindOptionsWhere, Repository } from "typeorm";
 
 import { AssetEntity } from "./asset.entity";
 import { AssetComponentEntity } from "./asset-component.entity";
-import { TemplateService } from "../../hierarchy/template/template.service";
+// import { TemplateService } from "../../hierarchy/template/template.service";
 import { IAssetDto, TokenType } from "@framework/types";
+import { TemplateEntity } from "../../hierarchy/template/template.entity";
 
 @Injectable()
 export class AssetService {
@@ -14,72 +15,102 @@ export class AssetService {
     private readonly assetEntityRepository: Repository<AssetEntity>,
     @InjectRepository(AssetComponentEntity)
     private readonly assetComponentEntityRepository: Repository<AssetComponentEntity>,
-    @Inject(forwardRef(() => TemplateService))
-    private readonly templateService: TemplateService,
-  ) {}
+    // @Inject(forwardRef(() => TemplateService))
+    // private readonly templateService: TemplateService,
+    private dataSource: DataSource,
+  ) { }
 
   public async create(dto: DeepPartial<AssetEntity>): Promise<AssetEntity> {
     return this.assetEntityRepository.create(dto).save();
   }
 
   public async update(asset: AssetEntity, dto: IAssetDto): Promise<AssetEntity> {
+
     // TODO transactions?
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
 
-    // patch NATIVE and ERC20 tokens
-    for (const component of dto.components) {
-      if (component.tokenType === TokenType.NATIVE || component.tokenType === TokenType.ERC20) {
-        const templateEntity = await this.templateService.findOne({ contractId: component.contractId });
-        if (!templateEntity) {
-          throw new NotFoundException("templateNotFound");
+    try {
+
+      // patch NATIVE and ERC20 tokens
+      for (const component of dto.components) {
+        if (component.tokenType === TokenType.NATIVE || component.tokenType === TokenType.ERC20) {
+
+          const templateEntity = await queryRunner.manager.findOne(TemplateEntity, { where: { contractId: component.contractId } });
+          // const templateEntity = await this.templateService.findOne({ contractId: component.contractId });
+
+          if (!templateEntity) {
+            throw new NotFoundException("templateNotFound");
+          }
+          component.templateId = templateEntity.id;
         }
-        component.templateId = templateEntity.id;
       }
+      
+      if (dto.components.length) {
+
+        // remove old
+        await Promise.allSettled(
+          asset.components
+            .filter(oldItem => !dto.components.find(newItem => newItem.id === oldItem.id))
+            .map(oldItem => queryRunner.manager.remove(oldItem)),
+        );
+
+        // change existing
+        const changedComponents = await Promise.allSettled(
+          asset.components
+            .filter(oldItem => dto.components.find(newItem => newItem.id === oldItem.id))
+            .map(oldItem => {
+              Object.assign(
+                oldItem,
+                dto.components.find(newItem => newItem.id === oldItem.id),
+                // this prevents typeorm to override ids using existing relations
+                { template: void 0, contract: void 0 },
+              );
+              return queryRunner.manager.save(oldItem)
+            }),
+        ).then(values =>
+          values
+            .filter(c => c.status === "fulfilled")
+            .map(c => <PromiseFulfilledResult<AssetComponentEntity>>c)
+            .map(c => c.value),
+        );
+
+        // add new
+        const newComponents = await Promise.allSettled(
+          dto.components
+            .filter(newItem => !newItem.id)
+            .map(newItem => {
+              return queryRunner.manager.create(AssetComponentEntity, { ...newItem, assetId: asset.id }).save()
+              // return queryRunner.manager.save(this.assetComponentEntityRepository.create())
+              // return this.assetComponentEntityRepository.create({ ...newItem, assetId: asset.id }).save()
+            }),
+        ).then(values =>
+          values
+            .filter(c => c.status === "fulfilled")
+            .map(c => <PromiseFulfilledResult<AssetComponentEntity>>c)
+            .map(c => c.value),
+        );
+
+        Object.assign(asset, { components: [...changedComponents, ...newComponents] });
+      }
+      await queryRunner.manager.save(asset)
+
+      // throw Error("just an Error")
+
+      await queryRunner.commitTransaction()
+
+    } catch (e) {
+      await queryRunner.rollbackTransaction()
+      // console.log(await queryRunner.manager.findOne(AssetEntity, { ...asset }))
+      // throw new Exceptio
+
+    } finally {
+      await queryRunner.release()
+
+      return asset
+
     }
-
-    if (dto.components.length) {
-      // remove old
-      await Promise.allSettled(
-        asset.components
-          .filter(oldItem => !dto.components.find(newItem => newItem.id === oldItem.id))
-          .map(oldItem => oldItem.remove()),
-      );
-
-      // change existing
-      const changedComponents = await Promise.allSettled(
-        asset.components
-          .filter(oldItem => dto.components.find(newItem => newItem.id === oldItem.id))
-          .map(oldItem => {
-            Object.assign(
-              oldItem,
-              dto.components.find(newItem => newItem.id === oldItem.id),
-              // this prevents typeorm to override ids using existing relations
-              { template: void 0, contract: void 0 },
-            );
-            return oldItem.save();
-          }),
-      ).then(values =>
-        values
-          .filter(c => c.status === "fulfilled")
-          .map(c => <PromiseFulfilledResult<AssetComponentEntity>>c)
-          .map(c => c.value),
-      );
-
-      // add new
-      const newComponents = await Promise.allSettled(
-        dto.components
-          .filter(newItem => !newItem.id)
-          .map(newItem => this.assetComponentEntityRepository.create({ ...newItem, assetId: asset.id }).save()),
-      ).then(values =>
-        values
-          .filter(c => c.status === "fulfilled")
-          .map(c => <PromiseFulfilledResult<AssetComponentEntity>>c)
-          .map(c => c.value),
-      );
-
-      Object.assign(asset, { components: [...changedComponents, ...newComponents] });
-    }
-
-    return asset.save();
   }
 
   public findOne(
