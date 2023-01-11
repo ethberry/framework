70dde62ac8a2e674f5e27d6f039a1f0aafa32236
TrejGun <trejgun@gmail.com>
2023-01-11 17:16:51 +0800

    feat: show wallet address

--- services/market-ui/src/pages/my-wallet/index.tsx
+++ services/market-ui/src/pages/my-wallet/index.tsx
@@ -1,17 +1,23 @@
 import { FC } from "react";
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 
 import { Breadcrumbs, PageHeader } from "@gemunion/mui-page-layout";
+import { useUser } from "@gemunion/provider-user";
+import { IUser } from "@framework/types";
 
 import { AttachWalletButton } from "./attach-wallet";
 import { AllowanceButton } from "./allowance";
 
 export const MyWallet: FC = () => {
+  const user = useUser<IUser>();
+
   return (
     <Grid>
       <Breadcrumbs path={["profile"]} />
 
       <PageHeader message="pages.my-wallet.title" />
+
+      <Typography sx={{ mb: 2 }}>Connected wallet: {user.profile.wallet || "N/A"}</Typography>
 
       <AttachWalletButton />
       <AllowanceButton />
