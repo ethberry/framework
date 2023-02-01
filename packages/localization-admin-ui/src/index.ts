1adb05014119002bdc530f4e4ab3df83065f48c1
JenyaFomin <jenya.stasik@gmail.com>
2023-02-01 21:51:44 +0800

    feat(packages): change localization, and add new user fields (country, gender)

--- packages/localization-admin-ui/src/index.ts
+++ packages/localization-admin-ui/src/index.ts
@@ -3,10 +3,30 @@ import { EnabledLanguages } from "@framework/constants";
 // @ts-ignore
 import ar from "../i18n/ar.json";
 // @ts-ignore
+import countriesAr from "../i18n/ar/countries.json";
+// @ts-ignore
+import schedulesAr from "../i18n/ar/schedule.json";
+// @ts-ignore
 import en from "../i18n/en.json";
 // @ts-ignore
+import countriesEn from "../i18n/en/countries.json";
+// @ts-ignore
+import schedulesEn from "../i18n/en/schedule.json";
+// @ts-ignore
 import ru from "../i18n/ru.json";
+// @ts-ignore
+import countriesRu from "../i18n/ru/countries.json";
+// @ts-ignore
+import schedulesRu from "../i18n/ru/schedule.json";
 
+en.enums.country = countriesEn;
+en.enums.schedule = schedulesEn;
+
+ar.enums.country = countriesAr;
+ar.enums.schedule = schedulesAr;
+
+ru.enums.country = countriesRu;
+ru.enums.schedule = schedulesRu;
 export const i18n: Record<EnabledLanguages, any> = {
   [EnabledLanguages.AR]: ar,
   [EnabledLanguages.EN]: en,
