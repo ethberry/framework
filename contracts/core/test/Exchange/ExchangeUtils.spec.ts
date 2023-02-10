774f297d064b7f13ebc13fd3a945cc5837e749af
TrejGun <trejgun@gmail.com>
2023-02-10 21:25:57 +0800

    test: ExchangeUtils

--- contracts/core/test/Exchange/ExchangeUtils.spec.ts
+++ contracts/core/test/Exchange/ExchangeUtils.spec.ts
@@ -1,44 +1,14 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { constants } from "ethers";
 
 import { amount, baseTokenURI, royalty, tokenName, tokenSymbol } from "@gemunion/contracts-constants";
-import { templateId } from "../constants";
+import { templateId, tokenId } from "../constants";
 
-describe("ExchangeMock", function () {
+describe("ExchangeUtils", function () {
   describe("spendFrom", function () {
-    it("should spendFrom ERC20", async function () {
-      const [_owner, receiver] = await ethers.getSigners();
-      const oneEth = ethers.constants.WeiPerEther;
-
-      const exchangeMockFactory = await ethers.getContractFactory("ExchangeMock");
-      const exchangeMockInstance = await exchangeMockFactory.deploy();
-
-      const erc20Factory = await ethers.getContractFactory("ERC20Simple");
-      const erc20Instance = await erc20Factory.deploy(tokenName, tokenSymbol, oneEth.mul(amount));
-
-      await erc20Instance.mint(receiver.address, oneEth.mul(amount));
-
-      await erc20Instance.connect(receiver).approve(exchangeMockInstance.address, oneEth.mul(amount));
-
-      const tx = await exchangeMockInstance.connect(receiver).testSpendFrom([
-        {
-          tokenType: 1,
-          token: erc20Instance.address,
-          tokenId: 0,
-          amount: oneEth.mul(amount),
-        },
-      ]);
-
-      await expect(tx).changeTokenBalances(
-        erc20Instance,
-        [receiver, exchangeMockInstance],
-        [oneEth.mul(amount).mul(-1), oneEth.mul(amount)],
-      );
-    });
-
     it("should spendFrom ETH", async function () {
       const [_owner, receiver] = await ethers.getSigners();
-      const oneEth = ethers.constants.WeiPerEther;
 
       const exchangeMockFactory = await ethers.getContractFactory("ExchangeMock");
       const exchangeMockInstance = await exchangeMockFactory.deploy();
@@ -47,15 +17,40 @@ describe("ExchangeMock", function () {
         [
           {
             tokenType: 0,
-            token: ethers.constants.AddressZero,
+            token: constants.AddressZero,
             tokenId: 0,
-            amount: oneEth,
+            amount,
           },
         ],
-        { value: oneEth },
+        { value: amount },
       );
 
-      await expect(tx).changeEtherBalances([receiver, exchangeMockInstance], [oneEth.mul(-1), oneEth]);
+      await expect(tx).changeEtherBalances([receiver, exchangeMockInstance], [-amount, amount]);
+    });
+
+    it("should spendFrom ERC20", async function () {
+      const [_owner, receiver] = await ethers.getSigners();
+
+      const exchangeMockFactory = await ethers.getContractFactory("ExchangeMock");
+      const exchangeMockInstance = await exchangeMockFactory.deploy();
+
+      const erc20Factory = await ethers.getContractFactory("ERC20Simple");
+      const erc20Instance = await erc20Factory.deploy(tokenName, tokenSymbol, amount);
+
+      await erc20Instance.mint(receiver.address, amount);
+
+      await erc20Instance.connect(receiver).approve(exchangeMockInstance.address, amount);
+
+      const tx = await exchangeMockInstance.connect(receiver).testSpendFrom([
+        {
+          tokenType: 1,
+          token: erc20Instance.address,
+          tokenId: 0,
+          amount,
+        },
+      ]);
+
+      await expect(tx).changeTokenBalances(erc20Instance, [receiver, exchangeMockInstance], [-amount, amount]);
     });
 
     it("should spendFrom ERC721", async function () {
@@ -69,18 +64,43 @@ describe("ExchangeMock", function () {
 
       await erc721Instance.mintCommon(receiver.address, templateId);
 
-      await erc721Instance.connect(receiver).approve(exchangeMockInstance.address, 1);
+      await erc721Instance.connect(receiver).approve(exchangeMockInstance.address, templateId);
 
       await exchangeMockInstance.connect(receiver).testSpendFrom([
         {
           tokenType: 2,
           token: erc721Instance.address,
           tokenId: 1,
-          amount: 0,
+          amount,
         },
       ]);
 
       const balance = await erc721Instance.balanceOf(exchangeMockInstance.address);
+      expect(balance).to.equal(1);
+    });
+
+    it("should spendFrom ERC998", async function () {
+      const [_owner, receiver] = await ethers.getSigners();
+
+      const exchangeMockFactory = await ethers.getContractFactory("ExchangeMock");
+      const exchangeMockInstance = await exchangeMockFactory.deploy();
+
+      const erc998Factory = await ethers.getContractFactory("ERC998Simple");
+      const erc998Instance = await erc998Factory.deploy(tokenName, tokenSymbol, royalty, baseTokenURI);
+
+      await erc998Instance.mintCommon(receiver.address, templateId);
+      await erc998Instance.connect(receiver).approve(exchangeMockInstance.address, tokenId);
+
+      await exchangeMockInstance.connect(receiver).testSpendFrom([
+        {
+          tokenType: 3,
+          token: erc998Instance.address,
+          tokenId,
+          amount,
+        },
+      ]);
+
+      const balance = await erc998Instance.balanceOf(exchangeMockInstance.address);
       expect(balance).to.equal(1);
     });
 
@@ -93,100 +113,68 @@ describe("ExchangeMock", function () {
       const erc1155Factory = await ethers.getContractFactory("ERC1155Simple");
       const erc1155Instance = await erc1155Factory.deploy(royalty, baseTokenURI);
 
-      await erc1155Instance.mint(receiver.address, 1, amount, "0x");
+      await erc1155Instance.mint(receiver.address, tokenId, amount, "0x");
 
       await erc1155Instance.connect(receiver).setApprovalForAll(exchangeMockInstance.address, true);
-      // await erc1155Instance.connect(receiver).approve(exchangeMockInstance.address, 1);
 
       await exchangeMockInstance.connect(receiver).testSpendFrom([
         {
           tokenType: 4,
           token: erc1155Instance.address,
-          tokenId: 1,
+          tokenId,
           amount,
         },
       ]);
 
-      const balance = await erc1155Instance.balanceOf(exchangeMockInstance.address, 1);
+      const balance = await erc1155Instance.balanceOf(exchangeMockInstance.address, tokenId);
       expect(balance).to.equal(amount);
     });
+  });
 
-    it("should spendFrom ERC998", async function () {
+  describe("spend", function () {
+    it("should spend ETH", async function () {
       const [_owner, receiver] = await ethers.getSigners();
 
       const exchangeMockFactory = await ethers.getContractFactory("ExchangeMock");
       const exchangeMockInstance = await exchangeMockFactory.deploy();
 
-      const erc998Factory = await ethers.getContractFactory("ERC998Simple");
-      const erc998Instance = await erc998Factory.deploy(tokenName, tokenSymbol, royalty, baseTokenURI);
+      await receiver.sendTransaction({ to: exchangeMockInstance.address, value: amount });
 
-      await erc998Instance.mintCommon(receiver.address, 1);
-      await erc998Instance.connect(receiver).approve(exchangeMockInstance.address, templateId);
-
-      await exchangeMockInstance.connect(receiver).testSpendFrom([
+      const tx = await exchangeMockInstance.connect(receiver).testSpend([
         {
-          tokenType: 3,
-          token: erc998Instance.address,
-          tokenId: 1,
-          amount: 0,
+          tokenType: 0,
+          token: constants.AddressZero,
+          tokenId,
+          amount,
         },
       ]);
 
-      const balance = await erc998Instance.balanceOf(exchangeMockInstance.address);
-      expect(balance).to.equal(1);
+      await expect(tx).changeEtherBalances([exchangeMockInstance, receiver], [-amount, amount]);
     });
-  });
 
-  describe("spend", function () {
     it("should spend ERC20", async function () {
       const [_owner, receiver] = await ethers.getSigners();
-      const oneEth = ethers.constants.WeiPerEther;
 
       const exchangeMockFactory = await ethers.getContractFactory("ExchangeMock");
       const exchangeMockInstance = await exchangeMockFactory.deploy();
 
       const erc20Factory = await ethers.getContractFactory("ERC20Simple");
-      const erc20Instance = await erc20Factory.deploy(tokenName, tokenSymbol, oneEth.mul(amount));
+      const erc20Instance = await erc20Factory.deploy(tokenName, tokenSymbol, amount);
 
-      await erc20Instance.mint(exchangeMockInstance.address, oneEth.mul(amount));
+      await erc20Instance.mint(exchangeMockInstance.address, amount);
 
-      await erc20Instance.connect(receiver).approve(exchangeMockInstance.address, oneEth.mul(amount));
+      await erc20Instance.connect(receiver).approve(exchangeMockInstance.address, amount);
 
       const tx = await exchangeMockInstance.connect(receiver).testSpend([
         {
           tokenType: 1,
           token: erc20Instance.address,
-          tokenId: 0,
-          amount: oneEth.mul(amount),
+          tokenId,
+          amount,
         },
       ]);
 
-      await expect(tx).changeTokenBalances(
-        erc20Instance,
-        [exchangeMockInstance, receiver],
-        [oneEth.mul(amount).mul(-1), oneEth.mul(amount)],
-      );
-    });
-
-    it("should spend ETH", async function () {
-      const [_owner, receiver] = await ethers.getSigners();
-      const oneEth = ethers.constants.WeiPerEther;
-
-      const exchangeMockFactory = await ethers.getContractFactory("ExchangeMock");
-      const exchangeMockInstance = await exchangeMockFactory.deploy();
-
-      await receiver.sendTransaction({ to: exchangeMockInstance.address, value: oneEth });
-
-      const tx = await exchangeMockInstance.connect(receiver).testSpend([
-        {
-          tokenType: 0,
-          token: ethers.constants.AddressZero,
-          tokenId: 0,
-          amount: oneEth,
-        },
-      ]);
-
-      await expect(tx).changeEtherBalances([exchangeMockInstance, receiver], [oneEth.mul(-1), oneEth]);
+      await expect(tx).changeTokenBalances(erc20Instance, [exchangeMockInstance, receiver], [-amount, amount]);
     });
 
     it("should spend ERC721", async function () {
@@ -204,40 +192,13 @@ describe("ExchangeMock", function () {
         {
           tokenType: 2,
           token: erc721Instance.address,
-          tokenId: 1,
-          amount: 0,
+          tokenId,
+          amount,
         },
       ]);
 
       const balance = await erc721Instance.balanceOf(receiver.address);
       expect(balance).to.equal(1);
-    });
-
-    it("should spend ERC1155", async function () {
-      const [_owner, receiver] = await ethers.getSigners();
-
-      const exchangeMockFactory = await ethers.getContractFactory("ExchangeMock");
-      const exchangeMockInstance = await exchangeMockFactory.deploy();
-
-      const erc1155Factory = await ethers.getContractFactory("ERC1155Simple");
-      const erc1155Instance = await erc1155Factory.deploy(royalty, baseTokenURI);
-
-      await erc1155Instance.mint(exchangeMockInstance.address, 1, amount, "0x");
-
-      // await erc1155Instance.connect(receiver).setApprovalForAll(exchangeMockInstance.address, true);
-      // await erc1155Instance.connect(receiver).approve(exchangeMockInstance.address, 1);
-
-      await exchangeMockInstance.connect(receiver).testSpend([
-        {
-          tokenType: 4,
-          token: erc1155Instance.address,
-          tokenId: 1,
-          amount,
-        },
-      ]);
-
-      const balance = await erc1155Instance.balanceOf(receiver.address, 1);
-      expect(balance).to.equal(amount);
     });
 
     it("should spend ERC998", async function () {
@@ -255,13 +216,37 @@ describe("ExchangeMock", function () {
         {
           tokenType: 3,
           token: erc998Instance.address,
-          tokenId: 1,
-          amount: 0,
+          tokenId,
+          amount,
         },
       ]);
 
       const balance = await erc998Instance.balanceOf(receiver.address);
       expect(balance).to.equal(1);
+    });
+
+    it("should spend ERC1155", async function () {
+      const [_owner, receiver] = await ethers.getSigners();
+
+      const exchangeMockFactory = await ethers.getContractFactory("ExchangeMock");
+      const exchangeMockInstance = await exchangeMockFactory.deploy();
+
+      const erc1155Factory = await ethers.getContractFactory("ERC1155Simple");
+      const erc1155Instance = await erc1155Factory.deploy(royalty, baseTokenURI);
+
+      await erc1155Instance.mint(exchangeMockInstance.address, tokenId, amount, "0x");
+
+      await exchangeMockInstance.connect(receiver).testSpend([
+        {
+          tokenType: 4,
+          token: erc1155Instance.address,
+          tokenId,
+          amount,
+        },
+      ]);
+
+      const balance = await erc1155Instance.balanceOf(receiver.address, tokenId);
+      expect(balance).to.equal(amount);
     });
   });
 });
