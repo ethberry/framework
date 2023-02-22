761f88dbdc72e77e4035b43b154db87c47240aee
JenyaFomin <jenya.stasik@gmail.com>
2023-02-22 18:06:53 +0800

    fix: make changes from Trej Gun

--- contracts/core/contracts/MOCKS/WalletMock.sol
+++ contracts/core/contracts/MOCKS/WalletMock.sol
@@ -6,17 +6,10 @@
 
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 import "@gemunion/contracts-erc20/contracts/extensions/ERC1363Receiver.sol";
 
-contract WalletMock is AccessControl, ERC1363Receiver, ERC1155Holder, ERC721Holder {
-    function supportsInterface(
-    bytes4 interfaceId
-  ) public view virtual override(AccessControl, ERC1155Receiver) returns (bool) {
-    return super.supportsInterface(interfaceId);
-  }
-
+contract WalletMock is ERC1363Receiver, ERC1155Holder, ERC721Holder {
   receive() external payable {}
 }
--- contracts/core/test/Mechanics/Wrapper/Wrapper.ts
+++ contracts/core/test/Mechanics/Wrapper/Wrapper.ts
@@ -19,7 +19,7 @@ import { deployERC1155 } from "../../ERC1155/shared/fixtures";
 import { constants } from "ethers";
 
 describe("Wrapper", function () {
-  const erc721WrapperFactory = () => deployERC721("ERC721TokenWrapperTest");
+  const factory = () => deployERC721("ERC721TokenWrapperTest");
   const erc20Factory = () => deployERC20();
   const erc721Factory = () => deployERC721();
   const erc1155Factory = () => deployERC1155();
@@ -27,15 +27,15 @@ describe("Wrapper", function () {
     const _erc998Factory = await ethers.getContractFactory("ERC998Simple");
     return _erc998Factory.deploy(tokenName, tokenSymbol, royalty, baseTokenURI);
   };
-  const wrapperWalletFactory = async () => (await ethers.getContractFactory("WrapperMock")).deploy();
+  const walletMockFactory = async () => (await ethers.getContractFactory("WrapperMock")).deploy();
 
-  shouldBehaveLikeAccessControl(erc721WrapperFactory)(DEFAULT_ADMIN_ROLE, MINTER_ROLE);
-  shouldBehaveLikeERC721Simple(erc721WrapperFactory);
+  shouldBehaveLikeAccessControl(factory)(DEFAULT_ADMIN_ROLE, MINTER_ROLE);
+  shouldBehaveLikeERC721Simple(factory);
 
   it("should wrap NATIVE and unwrap NATIVE", async function () {
     const [owner] = await ethers.getSigners();
 
-    const erc721WrapperInstance = await erc721WrapperFactory();
+    const erc721WrapperInstance = await factory();
 
     const items = [
       {
@@ -47,17 +47,17 @@ describe("Wrapper", function () {
     ];
 
     const tx = erc721WrapperInstance.mintBox(owner.address, 1, items, { value: amount });
-    await expect(tx).to.changeEtherBalance(owner, -amount);
+    await expect(tx).to.changeEtherBalances([owner, erc721WrapperInstance], [-amount, amount]);
 
     const tx1 = erc721WrapperInstance.unpack(1);
-    await expect(tx1).to.changeEtherBalance(owner, amount);
+    await expect(tx1).to.changeEtherBalances([owner, erc721WrapperInstance], [amount, -amount]);
   });
 
   it("should wrap ERC20 and unwrap ERC20", async function () {
     const [owner] = await ethers.getSigners();
 
     const erc20Instance = await erc20Factory();
-    const erc721WrapperInstance = await erc721WrapperFactory();
+    const erc721WrapperInstance = await factory();
 
     await erc20Instance.mint(owner.address, amount);
     await erc20Instance.approve(erc721WrapperInstance.address, amount);
@@ -73,19 +73,19 @@ describe("Wrapper", function () {
 
     const tx = erc721WrapperInstance.mintBox(owner.address, 1, items);
     await expect(tx).to.emit(erc20Instance, "Transfer");
-    await expect(tx).to.changeTokenBalance(erc20Instance, owner, -amount);
+    await expect(tx).to.changeTokenBalances(erc20Instance, [owner, erc721WrapperInstance], [-amount, amount]);
 
     const tx1 = erc721WrapperInstance.unpack(1);
     await expect(tx1).to.emit(erc20Instance, "Transfer");
-    await expect(tx1).to.changeTokenBalance(erc20Instance, owner, amount);
+    await expect(tx1).to.changeTokenBalances(erc20Instance, [owner, erc721WrapperInstance], [amount, -amount]);
   });
 
   it("should wrap ERC20 and unwrap ERC20 on Contract. Emit ERC1363 TransferReceived", async function () {
     const [owner] = await ethers.getSigners();
 
     const erc20Instance = await erc20Factory();
-    const erc721WrapperInstance = await erc721WrapperFactory();
-    const wrapperWalletInstance = await wrapperWalletFactory();
+    const erc721WrapperInstance = await factory();
+    const walletMockInstance = await walletMockFactory();
 
     await erc20Instance.mint(owner.address, amount);
     await erc20Instance.approve(erc721WrapperInstance.address, amount);
@@ -99,22 +99,26 @@ describe("Wrapper", function () {
       },
     ];
 
-    const tx = erc721WrapperInstance.mintBox(wrapperWalletInstance.address, 1, items);
+    const tx = erc721WrapperInstance.mintBox(walletMockInstance.address, 1, items);
     await expect(tx).to.emit(erc20Instance, "Transfer");
-    await expect(tx).to.changeTokenBalance(erc20Instance, owner, -amount);
+    await expect(tx).to.changeTokenBalances(erc20Instance, [owner, erc721WrapperInstance], [-amount, amount]);
 
     // Calling WrapperWalletMock.unpack
-    const tx1 = wrapperWalletInstance.unpack(erc721WrapperInstance.address, 1);
+    const tx1 = walletMockInstance.unpack(erc721WrapperInstance.address, 1);
     await expect(tx1).to.emit(erc20Instance, "Transfer");
-    await expect(tx1).to.emit(wrapperWalletInstance, "TransferReceived");
-    await expect(tx1).to.changeTokenBalance(erc20Instance, wrapperWalletInstance.address, amount);
+    await expect(tx1).to.emit(walletMockInstance, "TransferReceived");
+    await expect(tx1).to.changeTokenBalances(
+      erc20Instance,
+      [walletMockInstance, erc721WrapperInstance],
+      [amount, -amount],
+    );
   });
 
   it("should wrap ERC721 and unwrap ERC721", async function () {
     const [owner] = await ethers.getSigners();
 
     const erc721Instance = await erc721Factory();
-    const erc721WrapperInstance = await erc721WrapperFactory();
+    const erc721WrapperInstance = await factory();
 
     await erc721Instance.mintCommon(owner.address, 1);
     await erc721Instance.setApprovalForAll(erc721WrapperInstance.address, true);
@@ -141,7 +145,7 @@ describe("Wrapper", function () {
     const [owner] = await ethers.getSigners();
 
     const erc998Instance = await erc998Factory();
-    const erc721WrapperInstance = await erc721WrapperFactory();
+    const erc721WrapperInstance = await factory();
 
     await erc998Instance.mintCommon(owner.address, 1);
     await erc998Instance.setApprovalForAll(erc721WrapperInstance.address, true);
@@ -168,7 +172,7 @@ describe("Wrapper", function () {
     const [owner] = await ethers.getSigners();
 
     const erc1155Instance = await erc1155Factory();
-    const erc721WrapperInstance = await erc721WrapperFactory();
+    const erc721WrapperInstance = await factory();
 
     await erc1155Instance.mint(owner.address, 1, amount, "0x");
     await erc1155Instance.setApprovalForAll(erc721WrapperInstance.address, true);
@@ -191,44 +195,10 @@ describe("Wrapper", function () {
     expect(balanace2).to.be.equal(0);
   });
 
-  it("should wrap NATIVE & ERC20 and unwrap NATIVE & ERC20", async function () {
+  it("should wrap NATIVE, ERC20 & ERC721 & ERC998 & ERC1155 and unwrap NATIVE, ERC20 & ERC721 & ERC998 & ERC1155", async function () {
     const [owner] = await ethers.getSigners();
 
-    const erc721WrapperInstance = await erc721WrapperFactory();
-
-    const erc20Instance = await erc20Factory();
-
-    await erc20Instance.mint(owner.address, amount);
-    await erc20Instance.approve(erc721WrapperInstance.address, amount);
-
-    const items = [
-      {
-        tokenType: 0,
-        token: constants.AddressZero,
-        tokenId: 1,
-        amount,
-      },
-      {
-        tokenType: 1,
-        token: erc20Instance.address,
-        tokenId: 1,
-        amount,
-      },
-    ];
-
-    const tx = await erc721WrapperInstance.mintBox(owner.address, 1, items, { value: amount });
-    await expect(tx).to.changeTokenBalance(erc20Instance, owner, -amount);
-    await expect(tx).to.changeEtherBalance(owner, -amount);
-
-    const tx1 = await erc721WrapperInstance.unpack(1);
-    await expect(tx1).to.changeTokenBalance(erc20Instance, owner, amount);
-    await expect(tx1).to.changeEtherBalance(owner, amount);
-  });
-
-  it("should wrap NATIVE, ERC20 & ERC721 & ERC998 & ERC1155 and unwrap NATIVE, ERC20 & ERC721  & ERC998 & ERC1155", async function () {
-    const [owner] = await ethers.getSigners();
-
-    const erc721WrapperInstance = await erc721WrapperFactory();
+    const erc721WrapperInstance = await factory();
 
     const erc20Instance = await erc20Factory();
     const erc721Instance = await erc721Factory();
@@ -288,8 +258,8 @@ describe("Wrapper", function () {
     expect(balanace03).to.be.equal(1);
 
     const tx1 = erc721WrapperInstance.mintBox(owner.address, 1, items, { value: amount });
-    await expect(tx1).to.changeTokenBalance(erc20Instance, owner, -amount);
-    await expect(tx1).to.changeEtherBalance(owner, -amount);
+    await expect(tx1).to.changeTokenBalances(erc20Instance, [owner, erc721WrapperInstance], [-amount, amount]);
+    await expect(tx1).to.changeEtherBalances([owner, erc721WrapperInstance], [-amount, amount]);
     const balanace11 = await erc721Instance.balanceOf(owner.address);
     expect(balanace11).to.be.equal(0);
     const balanace12 = await erc998Instance.balanceOf(owner.address);
@@ -298,8 +268,8 @@ describe("Wrapper", function () {
     expect(balanace13).to.be.equal(0);
 
     const tx2 = erc721WrapperInstance.unpack(1);
-    await expect(tx2).to.changeTokenBalance(erc20Instance, owner, amount);
-    await expect(tx2).to.changeEtherBalance(owner, amount);
+    await expect(tx2).to.changeTokenBalances(erc20Instance, [owner, erc721WrapperInstance], [amount, -amount]);
+    await expect(tx2).to.changeEtherBalances([owner, erc721WrapperInstance], [amount, -amount]);
     const balanace21 = await erc721Instance.balanceOf(owner.address);
     expect(balanace21).to.be.equal(1);
     const balanace22 = await erc998Instance.balanceOf(owner.address);
