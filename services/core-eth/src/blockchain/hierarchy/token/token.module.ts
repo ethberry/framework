273140bdcc366f61ffe0dc171ef8d0144891a577
TrejGun <trejgun@gmail.com>
2023-03-09 17:39:27 +0800

    fix: remove leftovers

--- services/core-eth/src/blockchain/hierarchy/token/token.module.ts
+++ services/core-eth/src/blockchain/hierarchy/token/token.module.ts
@@ -1,18 +1,15 @@
-import { Logger, Module } from "@nestjs/common";
+import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
-
-import { ethersRpcProvider, ethersSignerProvider } from "@gemunion/nestjs-ethers";
 
 import { TokenEntity } from "./token.entity";
 import { TokenService } from "./token.service";
 import { TokenServiceEth } from "./token.service.eth";
-import { ContractModule } from "../contract/contract.module";
 import { EventHistoryModule } from "../../event-history/event-history.module";
 
 @Module({
-  imports: [ConfigModule, ContractModule, EventHistoryModule, TypeOrmModule.forFeature([TokenEntity])],
-  providers: [Logger, ethersRpcProvider, ethersSignerProvider, TokenService, TokenServiceEth],
+  imports: [ConfigModule, EventHistoryModule, TypeOrmModule.forFeature([TokenEntity])],
+  providers: [TokenService, TokenServiceEth],
   exports: [TokenService, TokenServiceEth],
 })
 export class TokenModule {}
--- services/core-eth/src/blockchain/hierarchy/token/token.service.eth.ts
+++ services/core-eth/src/blockchain/hierarchy/token/token.service.eth.ts
@@ -1,23 +1,16 @@
-import { Inject, Injectable, Logger, LoggerService, NotFoundException } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { Log } from "@ethersproject/abstract-provider";
-import { providers } from "ethers";
 
-import { ETHERS_RPC, ILogEvent } from "@gemunion/nestjs-ethers";
+import { ILogEvent } from "@gemunion/nestjs-ethers";
 
 import { IERC721TokenApprovedForAllEvent, IERC721TokenApproveEvent } from "@framework/types";
 
 import { TokenService } from "./token.service";
-import { ContractService } from "../contract/contract.service";
 import { EventHistoryService } from "../../event-history/event-history.service";
 
 @Injectable()
 export class TokenServiceEth {
   constructor(
-    @Inject(Logger)
-    protected readonly loggerService: LoggerService,
-    @Inject(ETHERS_RPC)
-    protected readonly jsonRpcProvider: providers.JsonRpcProvider,
-    protected readonly contractService: ContractService,
     protected readonly tokenService: TokenService,
     protected readonly eventHistoryService: EventHistoryService,
   ) {}
