5450e9334d1ea93fa6f65b3e5bce4b834432277c
Ubudragon <gemuniongroup@gmail.com>
2023-04-01 21:44:35 +0800

    feat: mint mystery menu

--- services/admin-ui/src/components/menu/mechanics/mystery/index.tsx
+++ services/admin-ui/src/components/menu/mechanics/mystery/index.tsx
@@ -17,6 +17,7 @@ import { BlacklistMenuItem } from "../../extensions/blacklist-add";
 import { UnBlacklistMenuItem } from "../../extensions/blacklist-remove";
 import { WhitelistMenuItem } from "../../extensions/whitelist-add";
 import { UnWhitelistMenuItem } from "../../extensions/whitelist-remove";
+import { MintMenuItem } from "../../hierarchy/contract/mint";
 
 export interface IMysteryActionsMenu {
   contract: IContract;
@@ -55,7 +56,7 @@ export const MysteryActionsMenu: FC<IMysteryActionsMenu> = props => {
         <ContractGrantRoleMenuItem contract={contract} />
         <ContractRevokeRoleMenuItem contract={contract} />
         <ContractRenounceRoleMenuItem contract={contract} />
-
+        <MintMenuItem contract={contract} />
         <AllowanceMenuItem contract={contract} />
         <RoyaltyMenuItem contract={contract} />
         <TransferMenuItem contract={contract} />
