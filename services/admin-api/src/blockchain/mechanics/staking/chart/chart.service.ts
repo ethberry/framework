2cc621e241ce6908f31707c2db89773e335aef60
TrejGun <trejgun@gmail.com>
2023-04-08 07:57:17 +0800

    fix: chart

--- services/admin-api/src/blockchain/mechanics/staking/chart/chart.service.ts
+++ services/admin-api/src/blockchain/mechanics/staking/chart/chart.service.ts
@@ -16,13 +16,10 @@ export class StakingChartService {
   ) {}
 
   public async amountChart(dto: IStakingChartSearchDto): Promise<any> {
-    const { emptyReward } = dto;
-
-    return emptyReward ? this.amountChartWithoutReward(dto) : this.amountChartWithReward(dto);
-  }
-
-  public async amountChartWithReward(dto: IStakingChartSearchDto): Promise<any> {
     const { deposit, reward, startTimestamp, endTimestamp } = dto;
+
+    const depositCondition = `AND deposit_contract.contract_type = $3 AND  deposit_contract.id = $4`;
+    const rewardCondition = `AND reward_contract.contract_type = $5  AND  reward_contract.id = $6`;
 
     // prettier-ignore
     const queryString = `
@@ -42,15 +39,9 @@ export class StakingChartService {
                 LEFT JOIN
             ${ns}.contract as reward_contract ON reward_component.contract_id = reward_contract.id
         WHERE
-            deposit_contract.contract_type = $1
-          AND
-            deposit_contract.id = $2
-          AND
-            reward_contract.contract_type = $3
-          AND
-            reward_contract.id = $4
-          AND
-            (staking_deposit.created_at >= $5 AND staking_deposit.created_at < $6)
+            (staking_deposit.created_at >= $1 AND staking_deposit.created_at < $2)
+            ${deposit.tokenType ? depositCondition : ""}
+            ${reward.tokenType ? rewardCondition : ""}
         GROUP BY
             date
         ORDER BY
@@ -59,54 +50,12 @@ export class StakingChartService {
 
     return Promise.all([
       this.entityManager.query(queryString, [
+        startTimestamp,
+        endTimestamp,
         deposit.tokenType,
         deposit.contractId,
         reward.tokenType,
         reward.contractId,
-        startTimestamp,
-        endTimestamp,
-      ]),
-      0,
-    ]);
-  }
-
-  public async amountChartWithoutReward(dto: IStakingChartSearchDto): Promise<any> {
-    const { deposit, startTimestamp, endTimestamp } = dto;
-
-    // prettier-ignore
-    const queryString = `
-        SELECT
-            COUNT(staking_deposit.id)::INTEGER AS count,
-            date_trunc('day', staking_deposit.created_at) as date
-        FROM
-            ${ns}.staking_deposit
-                LEFT JOIN
-            ${ns}.staking_rules ON staking_rules.id = staking_deposit.staking_rule_id
-                LEFT JOIN
-            ${ns}.asset_component as deposit_component ON deposit_component.asset_id = staking_rules.deposit_id
-                LEFT JOIN
-            ${ns}.contract as deposit_contract ON deposit_component.contract_id = deposit_contract.id
-        WHERE
-            deposit_contract.contract_type = $1
-          AND
-            deposit_contract.id = $2
-          AND
-            staking_rules.reward_id IS NULL
-          AND
-            (staking_deposit.created_at >= $3 AND staking_deposit.created_at < $4)
-        GROUP BY
-            date
-        ORDER BY
-            date
-    `;
-
-    // prettier-ignore
-    return Promise.all([
-      this.entityManager.query(queryString, [
-        deposit.tokenType,
-        deposit.contractId,
-        startTimestamp,
-        endTimestamp,
       ]),
       0,
     ]);
@@ -115,13 +64,16 @@ export class StakingChartService {
   public async volumeChart(dto: IStakingChartSearchDto): Promise<any> {
     const { deposit, reward, startTimestamp, endTimestamp } = dto;
 
+    const depositCondition = `AND deposit_contract.contract_type = $3 AND  deposit_contract.id = $4`;
+    const rewardCondition = `AND reward_contract.contract_type = $5  AND  reward_contract.id = $6`;
+
     // prettier-ignore
     const queryString = `
       WITH cte AS (SELECT start_date::timestamptz,
                           COUNT(staking_deposit.id)     AS transaction_count,
                           SUM(deposit_component.amount) AS amount
                    FROM ${ns}.staking_deposit
-                            LEFT JOIN
+                            INNER JOIN
                         ${ns}.staking_rules ON staking_rules.id = staking_deposit.staking_rule_id
                             LEFT JOIN
                         ${ns}.asset_component as deposit_component ON deposit_component.asset_id = staking_rules.deposit_id
@@ -133,12 +85,10 @@ export class StakingChartService {
                         ${ns}.contract as deposit_contract ON deposit_component.contract_id = deposit_contract.id
                             LEFT JOIN
                         ${ns}.contract as reward_contract ON reward_component.contract_id = reward_contract.id
-                   WHERE staking_deposit.start_timestamp <= $6
-                     AND (staking_deposit.start_timestamp + (staking_rules.duration_amount || 'second')::INTERVAL) >= $5
-                     AND deposit_contract.contract_type = $1
-                     AND deposit_contract.id = $2
-                     AND reward_contract.contract_type = $3
-                     AND reward_contract.id = $4
+                   WHERE 
+                     staking_deposit.start_timestamp <= $2 AND (staking_deposit.start_timestamp + (staking_rules.duration_amount || 'second')::INTERVAL) >= $1
+                     ${deposit.tokenType ? depositCondition : ""}
+                     ${reward.tokenType ? rewardCondition : ""}
       
                    GROUP BY start_date
                    ORDER BY start_date)
@@ -146,17 +96,17 @@ export class StakingChartService {
              transaction_count,
              amount
       FROM cte
-      WHERE start_date BETWEEN $5 AND $6;
+      WHERE start_date BETWEEN $1 AND $2;
     `;
 
     return Promise.all([
       this.entityManager.query(queryString, [
+        startTimestamp,
+        endTimestamp,
         deposit.tokenType,
         deposit.contractId,
         reward.tokenType,
         reward.contractId,
-        startTimestamp,
-        endTimestamp,
       ]),
       0,
     ]);
