d9b5ceedd7d7eb8ba02caef8857f77041081ae00
Karafizi Arthur <karafizi.artur@gmail.com>
2023-04-09 22:09:36 +0800

    fix: mint of mystery boxes and contracts; show box content in the mint dialog

--- services/admin-ui/src/components/menu/mechanics/mystery/mint-box/dialog/validation.ts
+++
@@ -1,9 +0,0 @@
-import { object } from "yup";
-
-import { templateAssetValidationSchema } from "@gemunion/mui-inputs-asset";
-import { addressValidationSchema } from "@gemunion/yup-rules-eth";
-
-export const validationSchema = object().shape({
-  account: addressValidationSchema,
-  template: templateAssetValidationSchema,
-});
---
+++ services/admin-ui/src/components/tables/mysterybox-content/index.tsx
@@ -0,0 +1,50 @@
+import { FC } from "react";
+import { Grid, Paper, Typography } from "@mui/material";
+import { FormattedMessage } from "react-intl";
+
+import { IMysterybox } from "@framework/types";
+
+import { formatEther } from "../../../utils/money";
+
+export interface IMysteryboxContentProps {
+  mysterybox: IMysterybox;
+}
+
+export const MysteryboxContent: FC<IMysteryboxContentProps> = props => {
+  const { mysterybox } = props;
+
+  return (
+    <Paper elevation={1} sx={{ mt: 1, p: 2 }}>
+      <Grid container>
+        <Grid xs={4} item>
+          <Typography fontWeight={500}>
+            <FormattedMessage id="form.labels.tokenType" />
+          </Typography>
+        </Grid>
+        <Grid xs={4} item>
+          <Typography fontWeight={500}>
+            <FormattedMessage id="form.labels.template" />
+          </Typography>
+        </Grid>
+        <Grid xs={4} item>
+          <Typography fontWeight={500}>
+            <FormattedMessage id="form.labels.amount" />
+          </Typography>
+        </Grid>
+      </Grid>
+      {mysterybox.item!.components.map(component => (
+        <Grid key={component.id} container>
+          <Grid xs={4} item>
+            {component.tokenType}
+          </Grid>
+          <Grid xs={4} item>
+            {component.template!.title}
+          </Grid>
+          <Grid xs={4} item>
+            {formatEther(component.amount, component.contract!.decimals, component.contract!.symbol)}
+          </Grid>
+        </Grid>
+      ))}
+    </Paper>
+  );
+};
