bdaa0c44138356fb9817c461f10782de28adb795
JenyaFomin <jenya.stasik@gmail.com>
2023-04-11 11:33:40 +0800

    test: add tests for blacklisted sender and receiver

--- contracts/core/test/ERC721/ERC721Blacklist.spec.ts
+++ contracts/core/test/ERC721/ERC721Blacklist.spec.ts
@@ -1,3 +1,6 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
 import { shouldBehaveLikeAccessControl, shouldSupportsInterface } from "@gemunion/contracts-mocha";
 import { DEFAULT_ADMIN_ROLE, InterfaceId, MINTER_ROLE } from "@gemunion/contracts-constants";
 import { shouldBehaveLikeBlackList } from "@gemunion/contracts-access-list";
@@ -17,4 +20,34 @@ describe("ERC721Blacklist", function () {
   shouldMintCommon(factory);
 
   shouldSupportsInterface(factory)(InterfaceId.IERC165, InterfaceId.IAccessControl, InterfaceId.IERC721);
+
+  describe("Blacklisted", function () {
+    it("shoud fail: sender is blacklisted", async function () {
+      const [_owner, recever] = await ethers.getSigners();
+
+      const erc721Instance = await factory();
+      await erc721Instance.mintCommon(recever.address, 1);
+
+      await erc721Instance.blacklist(recever.address);
+
+      const tx = erc721Instance
+        .connect(recever)
+        ["safeTransferFrom(address,address,uint256)"](recever.address, _owner.address, 1);
+
+      await expect(tx).to.be.rejectedWith("Blacklist: sender is blacklisted");
+    });
+
+    it("shoud fail: receiver is blacklisted", async function () {
+      const [owner, recever] = await ethers.getSigners();
+
+      const erc721Instance = await factory();
+      await erc721Instance.mintCommon(owner.address, 1);
+
+      await erc721Instance.blacklist(recever.address);
+
+      const tx = erc721Instance["safeTransferFrom(address,address,uint256)"](owner.address, recever.address, 1);
+
+      await expect(tx).to.be.rejectedWith("Blacklist: receiver is blacklisted");
+    });
+  });
 });
