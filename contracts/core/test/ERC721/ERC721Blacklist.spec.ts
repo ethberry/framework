c630993e3511796b3e0aec44eff0193f2df778f7
JenyaFomin <jenya.stasik@gmail.com>
2023-04-11 11:55:27 +0800

    test: add ERC721Blacklist behavior and add mintRandom tests

--- contracts/core/test/ERC721/ERC721Blacklist.spec.ts
+++ contracts/core/test/ERC721/ERC721Blacklist.spec.ts
@@ -1,6 +1,3 @@
-import { expect } from "chai";
-import { ethers } from "hardhat";
-
 import { shouldBehaveLikeAccessControl, shouldSupportsInterface } from "@gemunion/contracts-mocha";
 import { DEFAULT_ADMIN_ROLE, InterfaceId, MINTER_ROLE } from "@gemunion/contracts-constants";
 import { shouldBehaveLikeBlackList } from "@gemunion/contracts-access-list";
@@ -9,45 +6,17 @@ import { shouldMintCommon } from "./shared/mintCommon";
 
 import { shouldBehaveLikeERC721Simple } from "./shared/simple";
 import { deployERC721 } from "./shared/fixtures";
+import { shouldBehaveLikeERC721BlackList } from "./shared/blacklist";
 
 describe("ERC721Blacklist", function () {
   const factory = () => deployERC721(this.title);
 
   shouldBehaveLikeAccessControl(factory)(DEFAULT_ADMIN_ROLE, MINTER_ROLE);
   shouldBehaveLikeBlackList(factory);
+  shouldBehaveLikeERC721BlackList(factory);
 
   shouldBehaveLikeERC721Simple(factory);
   shouldMintCommon(factory);
 
   shouldSupportsInterface(factory)(InterfaceId.IERC165, InterfaceId.IAccessControl, InterfaceId.IERC721);
-
-  describe("Blacklisted", function () {
-    it("shoud fail: sender is blacklisted", async function () {
-      const [_owner, recever] = await ethers.getSigners();
-
-      const erc721Instance = await factory();
-      await erc721Instance.mintCommon(recever.address, 1);
-
-      await erc721Instance.blacklist(recever.address);
-
-      const tx = erc721Instance
-        .connect(recever)
-        ["safeTransferFrom(address,address,uint256)"](recever.address, _owner.address, 1);
-
-      await expect(tx).to.be.rejectedWith("Blacklist: sender is blacklisted");
-    });
-
-    it("shoud fail: receiver is blacklisted", async function () {
-      const [owner, recever] = await ethers.getSigners();
-
-      const erc721Instance = await factory();
-      await erc721Instance.mintCommon(owner.address, 1);
-
-      await erc721Instance.blacklist(recever.address);
-
-      const tx = erc721Instance["safeTransferFrom(address,address,uint256)"](owner.address, recever.address, 1);
-
-      await expect(tx).to.be.rejectedWith("Blacklist: receiver is blacklisted");
-    });
-  });
 });
--- contracts/core/test/ERC721/ERC721BlacklistUpgradeableRentableRandom.spec.ts
+++ contracts/core/test/ERC721/ERC721BlacklistUpgradeableRentableRandom.spec.ts
@@ -7,15 +7,19 @@ import { shouldBehaveLikeERC721Simple } from "./shared/simple";
 import { deployERC721 } from "./shared/fixtures";
 import { shouldBehaveLikeUpgradeable } from "../Mechanics/Grade/upgrade";
 import { FrameworkInterfaceId } from "../constants";
+import { shouldMintRandom } from "./shared/random/mintRandom";
+import { shouldBehaveLikeERC721BlackList } from "./shared/blacklist";
 
 describe("ERC721BlacklistUpgradeableRentableRandom", function () {
-  const factory = () => deployERC721(this.title);
+  const factory = () => deployERC721(this.title + "Hardhat");
 
   shouldBehaveLikeAccessControl(factory)(DEFAULT_ADMIN_ROLE, MINTER_ROLE, METADATA_ROLE);
   shouldBehaveLikeBlackList(factory);
+  shouldBehaveLikeERC721BlackList(factory);
 
   shouldBehaveLikeERC721Simple(factory);
   shouldMintCommon(factory);
+  shouldMintRandom(factory);
   shouldBehaveLikeUpgradeable(factory);
 
   shouldSupportsInterface(factory)(
