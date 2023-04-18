34d2114f9a0bd50e0fb2aa94cf66fb59b6bc1ca3
Ubudragon <gemuniongroup@gmail.com>
2023-04-18 18:50:10 +0800

    feat: use EnumerableSet for minters[] and manipulators[]

--- contracts/core/contracts/ContractManager/AbstractFactory.sol
+++ contracts/core/contracts/ContractManager/AbstractFactory.sol
@@ -6,6 +6,7 @@
 
 pragma solidity ^0.8.13;
 
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
@@ -20,16 +21,15 @@ abstract contract AbstractFactory is EIP712, AccessControl {
   bytes32 constant METADATA_ROLE = keccak256("METADATA_ROLE");
 
   using ECDSA for bytes32;
+  using EnumerableSet for EnumerableSet.AddressSet;
 
   mapping(bytes32 => bool) private _expired;
 
   bytes internal constant PARAMS_SIGNATURE = "Params(bytes32 nonce,bytes bytecode)";
   bytes32 private constant PARAMS_TYPEHASH = keccak256(abi.encodePacked(PARAMS_SIGNATURE));
 
-  address[] _minters;
-  mapping(address => bool) private mintersExists;
-  address[] _manipulators;
-  mapping(address => bool) private manipulatorsExists;
+  EnumerableSet.AddressSet private _minters;
+  EnumerableSet.AddressSet private _manipulators;
 
   struct Params {
     bytes32 nonce;
@@ -50,13 +50,11 @@ abstract contract AbstractFactory is EIP712, AccessControl {
     require((role == MINTER_ROLE || role == METADATA_ROLE), "ContractManager: Wrong role");
 
     if (role == MINTER_ROLE) {
-      require(!mintersExists[factory], "ContractManager: Factory exists");
-      _minters.push(factory);
-      mintersExists[factory] = true;
+      require(!EnumerableSet.contains(_minters, factory), "ContractManager: Factory exists");
+      EnumerableSet.add(_minters, factory);
     } else if (role == METADATA_ROLE) {
-      require(!manipulatorsExists[factory], "ContractManager: Factory exists");
-      _manipulators.push(factory);
-      manipulatorsExists[factory] = true;
+      require(!EnumerableSet.contains(_manipulators, factory), "ContractManager: Factory exists");
+      EnumerableSet.add(_manipulators, factory);
     }
   }
 
@@ -67,63 +65,35 @@ abstract contract AbstractFactory is EIP712, AccessControl {
     );
 
     if (role == MINTER_ROLE) {
-      for (uint256 i = 0; i < _minters.length; i++) {
-        if (_minters[i] == factory) {
-          for (uint256 j = i; j < _minters.length - 1; j++) {
-            _minters[j] = _minters[j + 1];
-          }
-          _minters.pop(); // delete the last item
-        }
-      }
+      EnumerableSet.remove(_minters, factory);
     } else if (role == METADATA_ROLE) {
-      for (uint256 i = 0; i < _manipulators.length; i++) {
-        if (_manipulators[i] == factory) {
-          for (uint256 j = i; j < _manipulators.length - 1; j++) {
-            _manipulators[j] = _manipulators[j + 1];
-          }
-          _manipulators.pop(); // delete the last item
-        }
-      }
+      EnumerableSet.remove(_manipulators, factory);
     } else if (role == DEFAULT_ADMIN_ROLE) {
-      for (uint256 i = 0; i < _minters.length; i++) {
-        if (_minters[i] == factory) {
-          for (uint256 j = i; j < _minters.length - 1; j++) {
-            _minters[j] = _minters[j + 1];
-          }
-          _minters.pop(); // delete the last item
-        }
-      }
-      for (uint256 i = 0; i < _manipulators.length; i++) {
-        if (_manipulators[i] == factory) {
-          for (uint256 j = i; j < _manipulators.length - 1; j++) {
-            _manipulators[j] = _manipulators[j + 1];
-          }
-          _manipulators.pop(); // delete the last item
-        }
-      }
+      EnumerableSet.remove(_minters, factory);
+      EnumerableSet.remove(_manipulators, factory);
     }
   }
 
   // DEV
   function getMinters() public view onlyRole(DEFAULT_ADMIN_ROLE) returns (address[] memory minters) {
-    return _minters;
+    return EnumerableSet.values(_minters);
   }
 
   function getManipulators() public view onlyRole(DEFAULT_ADMIN_ROLE) returns (address[] memory manipulators) {
-    return _manipulators;
+    return EnumerableSet.values(_manipulators);
   }
 
   function grantFactoryMintPermission(address addr) internal {
     IAccessControl instance = IAccessControl(addr);
-    for (uint256 i = 0; i < _minters.length; i++) {
-      instance.grantRole(MINTER_ROLE, _minters[i]);
+    for (uint256 i = 0; i < EnumerableSet.length(_minters); i++) {
+      instance.grantRole(MINTER_ROLE, EnumerableSet.at(_minters, i));
     }
   }
 
   function grantFactoryMetadataPermission(address addr) internal {
     IAccessControl instance = IAccessControl(addr);
-    for (uint256 i = 0; i < _manipulators.length; i++) {
-      instance.grantRole(METADATA_ROLE, _manipulators[i]);
+    for (uint256 i = 0; i < EnumerableSet.length(_manipulators); i++) {
+      instance.grantRole(METADATA_ROLE, EnumerableSet.at(_manipulators, i));
     }
   }
 
