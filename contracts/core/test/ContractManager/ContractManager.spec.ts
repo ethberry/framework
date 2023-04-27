97464ff20d2b2bc696e109efaeabf3baa3aefe22
TrejGun <trejgun@gmail.com>
2023-04-27 10:25:58 +0800

    test: contract manager

--- contracts/core/test/ContractManager/ContractManager.spec.ts
+++ contracts/core/test/ContractManager/ContractManager.spec.ts
@@ -1,6 +1,5 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { constants } from "ethers";
 
 import { shouldBehaveLikeAccessControl } from "@gemunion/contracts-mocha";
 
@@ -13,97 +12,210 @@ describe("ContractManager", function () {
 
   shouldBehaveLikeAccessControl(factory)(DEFAULT_ADMIN_ROLE);
 
-  it("should add factory", async function () {
-    const [_owner, receiver, stranger] = await ethers.getSigners();
+  describe("addFactory", function () {
+    describe("MINTER_ROLE", function () {
+      it("should set minters (zero)", async function () {
+        const contractInstance = await factory();
 
-    const contractInstance = await factory();
-    await contractInstance.addFactory(receiver.address, MINTER_ROLE);
-    await contractInstance.addFactory(stranger.address, METADATA_ROLE);
-    const minters = await contractInstance.getMinters();
-    const manipulators = await contractInstance.getManipulators();
-    expect(minters[0]).to.equal(receiver.address);
-    expect(manipulators[0]).to.equal(stranger.address);
+        const minters = await contractInstance.getMinters();
+        expect(minters).to.have.lengthOf(0);
+      });
+
+      it("factory already exists", async function () {
+        const [_owner, receiver] = await ethers.getSigners();
+
+        const contractInstance = await factory();
+        await contractInstance.addFactory(receiver.address, MINTER_ROLE);
+        const tx = contractInstance.addFactory(receiver.address, MINTER_ROLE);
+        await expect(tx).to.not.be.reverted;
+      });
+
+      it("should set minters (one)", async function () {
+        const [_owner, receiver] = await ethers.getSigners();
+
+        const contractInstance = await factory();
+        await contractInstance.addFactory(receiver.address, MINTER_ROLE);
+
+        const minters = await contractInstance.getMinters();
+        expect(minters).to.have.lengthOf(1);
+        expect(minters[0]).to.equal(receiver.address);
+      });
+    });
+
+    describe("METADATA_ROLE", function () {
+      it("should set manipulators (zero)", async function () {
+        const contractInstance = await factory();
+
+        const manipulators = await contractInstance.getManipulators();
+        expect(manipulators).to.have.lengthOf(0);
+      });
+
+      it("factory already exists", async function () {
+        const [_owner, receiver] = await ethers.getSigners();
+
+        const contractInstance = await factory();
+        await contractInstance.addFactory(receiver.address, METADATA_ROLE);
+        const tx = contractInstance.addFactory(receiver.address, METADATA_ROLE);
+        await expect(tx).to.not.be.reverted;
+      });
+
+      it("should set manipulators (one)", async function () {
+        const [_owner, receiver] = await ethers.getSigners();
+
+        const contractInstance = await factory();
+        await contractInstance.addFactory(receiver.address, METADATA_ROLE);
+
+        const manipulators = await contractInstance.getManipulators();
+        expect(manipulators).to.have.lengthOf(1);
+        expect(manipulators[0]).to.equal(receiver.address);
+      });
+    });
+
+    describe("DEFAULT_ADMIN_ROLE", function () {
+      it("should set minters and manipulators", async function () {
+        const [_owner, receiver, stranger] = await ethers.getSigners();
+
+        const contractInstance = await factory();
+        await contractInstance.addFactory(receiver.address, DEFAULT_ADMIN_ROLE);
+        await contractInstance.addFactory(stranger.address, DEFAULT_ADMIN_ROLE);
+
+        const minters = await contractInstance.getMinters();
+        expect(minters).to.have.lengthOf(2);
+        expect(minters[0]).to.equal(receiver.address);
+        expect(minters[1]).to.equal(stranger.address);
+
+        const manipulators = await contractInstance.getManipulators();
+        expect(manipulators).to.have.lengthOf(2);
+        expect(manipulators[0]).to.equal(receiver.address);
+        expect(manipulators[1]).to.equal(stranger.address);
+      });
+    });
+
+    describe("PREDICATE_ROLE", function () {
+      it("should fail add: wrong role", async function () {
+        const [_owner, receiver] = await ethers.getSigners();
+
+        const contractInstance = await factory();
+        const tx = contractInstance.addFactory(receiver.address, PREDICATE_ROLE);
+        await expect(tx).to.be.revertedWith("ContractManager: Wrong role");
+      });
+
+      it("should fail add: account is missing role", async function () {
+        const [_owner, receiver] = await ethers.getSigners();
+
+        const contractInstance = await factory();
+        const tx = contractInstance.connect(receiver).addFactory(receiver.address, PREDICATE_ROLE);
+        await expect(tx).to.be.revertedWith(
+          `AccessControl: account ${receiver.address.toLowerCase()} is missing role ${DEFAULT_ADMIN_ROLE}`,
+        );
+      });
+    });
   });
 
-  it("should fail add: wrong role", async function () {
-    const [_owner] = await ethers.getSigners();
+  describe("removeFactory", function () {
+    describe("MINTER_ROLE", function () {
+      it("should remove minters", async function () {
+        const [_owner, receiver, stranger] = await ethers.getSigners();
 
-    const contractInstance = await factory();
-    const tx = contractInstance.addFactory(_owner.address, DEFAULT_ADMIN_ROLE);
-    await expect(tx).to.be.revertedWith("ContractManager: Wrong role");
+        const contractInstance = await factory();
+        await contractInstance.addFactory(receiver.address, MINTER_ROLE);
+        await contractInstance.addFactory(receiver.address, METADATA_ROLE);
+        await contractInstance.addFactory(stranger.address, MINTER_ROLE);
+        await contractInstance.addFactory(stranger.address, METADATA_ROLE);
+
+        await contractInstance.removeFactory(receiver.address, MINTER_ROLE);
+
+        const minters = await contractInstance.getMinters();
+        expect(minters).to.have.lengthOf(1);
+        expect(minters[0]).to.equal(stranger.address);
+
+        const manipulators = await contractInstance.getManipulators();
+        expect(manipulators).to.have.lengthOf(2);
+        expect(manipulators[0]).to.equal(receiver.address);
+        expect(manipulators[1]).to.equal(stranger.address);
+      });
+    });
+
+    describe("METADATA_ROLE", function () {
+      it("should remove manipulators", async function () {
+        const [_owner, receiver, stranger] = await ethers.getSigners();
+
+        const contractInstance = await factory();
+        await contractInstance.addFactory(receiver.address, MINTER_ROLE);
+        await contractInstance.addFactory(receiver.address, METADATA_ROLE);
+        await contractInstance.addFactory(stranger.address, MINTER_ROLE);
+        await contractInstance.addFactory(stranger.address, METADATA_ROLE);
+
+        await contractInstance.removeFactory(receiver.address, METADATA_ROLE);
+
+        const minters = await contractInstance.getMinters();
+        expect(minters).to.have.lengthOf(2);
+        expect(minters[0]).to.equal(receiver.address);
+        expect(minters[1]).to.equal(stranger.address);
+
+        const manipulators = await contractInstance.getManipulators();
+        expect(manipulators).to.have.lengthOf(1);
+        expect(manipulators[0]).to.equal(stranger.address);
+      });
+    });
+
+    describe("DEFAULT_ADMIN_ROLE", function () {
+      it("should remove manipulators", async function () {
+        const [_owner, receiver, stranger] = await ethers.getSigners();
+
+        const contractInstance = await factory();
+        await contractInstance.addFactory(receiver.address, MINTER_ROLE);
+        await contractInstance.addFactory(receiver.address, METADATA_ROLE);
+        await contractInstance.addFactory(stranger.address, MINTER_ROLE);
+        await contractInstance.addFactory(stranger.address, METADATA_ROLE);
+
+        await contractInstance.removeFactory(receiver.address, DEFAULT_ADMIN_ROLE);
+
+        const minters = await contractInstance.getMinters();
+        expect(minters).to.have.lengthOf(1);
+        expect(minters[0]).to.equal(stranger.address);
+
+        const manipulators = await contractInstance.getManipulators();
+        expect(manipulators).to.have.lengthOf(1);
+        expect(manipulators[0]).to.equal(stranger.address);
+      });
+    });
+
+    describe("PREDICATE_ROLE", function () {
+      it("should fail add: wrong role", async function () {
+        const [_owner, receiver] = await ethers.getSigners();
+
+        const contractInstance = await factory();
+        const tx = contractInstance.removeFactory(receiver.address, PREDICATE_ROLE);
+        await expect(tx).to.be.revertedWith("ContractManager: Wrong role");
+      });
+
+      it("should fail add: account is missing role", async function () {
+        const [_owner, receiver] = await ethers.getSigners();
+
+        const contractInstance = await factory();
+        const tx = contractInstance.connect(receiver).removeFactory(receiver.address, PREDICATE_ROLE);
+        await expect(tx).to.be.revertedWith(
+          `AccessControl: account ${receiver.address.toLowerCase()} is missing role ${DEFAULT_ADMIN_ROLE}`,
+        );
+      });
+    });
   });
 
-  it("should remove factory", async function () {
-    const [owner, receiver, stranger] = await ethers.getSigners();
+  describe("destroy", function () {
+    it("should destroy", async function () {
+      const contractInstance = await factory();
+      const tx = await contractInstance.destroy();
+      await expect(tx).not.to.be.reverted;
+    });
 
-    const contractInstance = await factory();
-    await contractInstance.addFactory(owner.address, MINTER_ROLE);
-    await contractInstance.addFactory(receiver.address, MINTER_ROLE);
-    await contractInstance.addFactory(stranger.address, MINTER_ROLE);
-
-    await contractInstance.addFactory(owner.address, METADATA_ROLE);
-    await contractInstance.addFactory(stranger.address, METADATA_ROLE);
-    await contractInstance.addFactory(receiver.address, METADATA_ROLE);
-
-    const minters0 = await contractInstance.getMinters();
-    const manipulators0 = await contractInstance.getManipulators();
-
-    expect(minters0).to.deep.equal([owner.address, receiver.address, stranger.address]);
-    expect(manipulators0).to.deep.equal([owner.address, stranger.address, receiver.address]);
-
-    await contractInstance.removeFactory(receiver.address, constants.HashZero);
-
-    const minters1 = await contractInstance.getMinters();
-    const manipulators1 = await contractInstance.getManipulators();
-
-    expect(minters1).to.deep.equal([owner.address, stranger.address]);
-    expect(manipulators1).to.deep.equal([owner.address, stranger.address]);
-  });
-
-  it("should fail remove: wrong role", async function () {
-    const [owner, receiver] = await ethers.getSigners();
-
-    const contractInstance = await factory();
-    await contractInstance.addFactory(owner.address, MINTER_ROLE);
-    await contractInstance.addFactory(receiver.address, MINTER_ROLE);
-
-    const minters0 = await contractInstance.getMinters();
-
-    expect(minters0).to.deep.equal([owner.address, receiver.address]);
-
-    const tx = contractInstance.removeFactory(receiver.address, PREDICATE_ROLE);
-    await expect(tx).to.be.revertedWith("ContractManager: Wrong role");
-  });
-
-  it("should fail: factory exists", async function () {
-    const [_owner, receiver, stranger] = await ethers.getSigners();
-
-    const contractInstance = await factory();
-    await contractInstance.addFactory(receiver.address, MINTER_ROLE);
-    const tx0 = contractInstance.addFactory(receiver.address, MINTER_ROLE);
-    await expect(tx0).to.be.revertedWith("ContractManager: Factory exists");
-    await contractInstance.addFactory(stranger.address, METADATA_ROLE);
-    const tx1 = contractInstance.addFactory(stranger.address, METADATA_ROLE);
-    await expect(tx1).to.be.revertedWith("ContractManager: Factory exists");
-
-    const minters = await contractInstance.getMinters();
-    const manipulators = await contractInstance.getManipulators();
-    expect(minters[0]).to.equal(receiver.address);
-    expect(manipulators[0]).to.equal(stranger.address);
-  });
-
-  it("should fail: wrong role", async function () {
-    const [_owner, receiver] = await ethers.getSigners();
-    const contractInstance = await factory();
-    const tx = contractInstance.connect(receiver).destroy();
-    await expect(tx).to.be.revertedWith(
-      `AccessControl: account ${receiver.address.toLowerCase()} is missing role ${DEFAULT_ADMIN_ROLE}`,
-    );
-  });
-
-  it("should destroy", async function () {
-    const contractInstance = await factory();
-    const tx = await contractInstance.destroy();
-    await expect(tx).not.to.be.reverted;
+    it("should fail: wrong role", async function () {
+      const [_owner, receiver] = await ethers.getSigners();
+      const contractInstance = await factory();
+      const tx = contractInstance.connect(receiver).destroy();
+      await expect(tx).to.be.revertedWith(
+        `AccessControl: account ${receiver.address.toLowerCase()} is missing role ${DEFAULT_ADMIN_ROLE}`,
+      );
+    });
   });
 });
