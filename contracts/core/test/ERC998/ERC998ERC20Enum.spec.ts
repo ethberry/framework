14fe161b91171c82ff69b3df939e9e29a0d2f143
JenyaFomin <jenya.stasik@gmail.com>
2023-05-22 20:37:42 +0800

    fix: add tests for erc998

--- contracts/core/test/ERC998/ERC998ERC20Enum.spec.ts
+++ contracts/core/test/ERC998/ERC998ERC20Enum.spec.ts
@@ -1,33 +1,30 @@
 import { shouldBehaveLikeAccessControl, shouldSupportsInterface } from "@gemunion/contracts-mocha";
 import { DEFAULT_ADMIN_ROLE, InterfaceId, MINTER_ROLE } from "@gemunion/contracts-constants";
 import {
-  shouldBehaveLikeERC998ERC1155,
-  shouldBehaveLikeERC998ERC1155Enumerable,
   shouldBehaveLikeERC998ERC20,
   shouldBehaveLikeERC998ERC20Enumerable,
-  shouldStateHash,
+  shouldBehaveLikeERC998Enumerable,
 } from "@gemunion/contracts-erc998td";
 
 import { shouldMintCommon } from "../ERC721/shared/simple/base/mintCommon";
 import { deployERC721 } from "../ERC721/shared/fixtures";
 import { shouldBehaveLikeERC998 } from "./shared/simple/base";
 import { shouldBehaveLikeERC998Simple } from "./shared/simple";
-import { customMintCommonERC721 } from "../ERC721/shared/customMintFn";
 import { tokenId } from "../constants";
+import { customMintCommonERC721 } from "../ERC721/shared/customMintFn";
 
-describe("ERC998ERC1155ERC20HEnum", function () {
+describe("ERC998ERC20Enum", function () {
   const factory = () => deployERC721(this.title);
+  const options = { mint: customMintCommonERC721, tokenId };
 
   shouldBehaveLikeAccessControl(factory)(DEFAULT_ADMIN_ROLE, MINTER_ROLE);
 
   shouldBehaveLikeERC998(factory);
+  shouldBehaveLikeERC998Enumerable(factory, options);
   shouldBehaveLikeERC998Simple(factory);
   shouldMintCommon(factory);
-  shouldBehaveLikeERC998ERC20(factory, { mint: customMintCommonERC721, tokenId });
-  shouldBehaveLikeERC998ERC20Enumerable(factory, { mint: customMintCommonERC721, tokenId });
-  shouldBehaveLikeERC998ERC1155(factory, { mint: customMintCommonERC721, tokenId });
-  shouldBehaveLikeERC998ERC1155Enumerable(factory, { mint: customMintCommonERC721, tokenId });
-  shouldStateHash(factory, { mint: customMintCommonERC721, tokenId });
+  shouldBehaveLikeERC998ERC20(factory, options);
+  shouldBehaveLikeERC998ERC20Enumerable(factory, options);
 
   shouldSupportsInterface(factory)([InterfaceId.IERC165, InterfaceId.IAccessControl, InterfaceId.IERC721]);
 });
