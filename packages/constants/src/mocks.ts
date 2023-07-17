6a09e82ecb894d84305b045b2e57563b0cea09d5
TrejGun <trejgun@gmail.com>
2023-07-17 08:19:51 +0800

    chore: update imageUrl

--- packages/constants/src/mocks.ts
+++ packages/constants/src/mocks.ts
@@ -1,4 +1,4 @@
 export const imageUrl =
-  "https://firebasestorage.googleapis.com/v0/b/gemunion-firebase.appspot.com/o/DO_NOT_REMOVE%2Flogo.png?alt=media&token=f138edbb-602d-4714-87d3-e53bbfde3e19";
+  "https://firebasestorage.googleapis.com/v0/b/gemunion-firebase.appspot.com/o/DO_NOT_REMOVE%2Flogo.png?alt=media&token=73cdfd99-1374-45e0-8140-5c02fc4d8097";
 
 export const baseTokenURI = "http://localhost:3011/metadata/";
