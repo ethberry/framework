fb9daab95589fadbf0f9abc5e861cdc075477a83
TrejGun <trejgun@gmail.com>
2023-07-18 21:05:23 +0800

    fix: avatar url

--- packages/constants/src/mocks.ts
+++ packages/constants/src/mocks.ts
@@ -1,6 +1,6 @@
 export const imageUrl =
   "https://firebasestorage.googleapis.com/v0/b/gemunion-firebase.appspot.com/o/DO_NOT_REMOVE%2Flogo.png?alt=media&token=73cdfd99-1374-45e0-8140-5c02fc4d8097";
 export const avatarUrl =
-  "https://firebasestorage.googleapis.com/v0/b/gemunion-firebase.appspot.com/o/DO_NOT_REMOVE%2Favatar.png?alt=media&token=83acf54f-8d7f-4c81-8e6e-a59f0a1b963c";
+  "https://firebasestorage.googleapis.com/v0/b/gemunion-firebase.appspot.com/o/DO_NOT_REMOVE%2Favatar.png?alt=media&token=864b5125-bd4c-42fb-a53c-1b3f57d4c169";
 
 export const baseTokenURI = "http://localhost:3011/metadata/";
