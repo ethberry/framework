266054233f85710ab7ecfe99fa13d439879fc487
TrejGun <trejgun@gmail.com>
2023-07-27 20:42:01 +0800

    test: split lottery/raffle

--- contracts/core/test/Mechanics/Raffle/fixture.ts
+++ contracts/core/test/Mechanics/Raffle/fixture.ts
@@ -6,31 +6,31 @@ import { parseEther } from "ethers";
 import { getContractName } from "../../utils";
 import { deployERC721 } from "../../ERC721/shared/fixtures";
 import { deployERC20 } from "../../ERC20/shared/fixtures";
-import { wrapLotterySignature } from "./utils";
+import { wrapRaffleSignature } from "./utils";
 
 interface ILotteryConfig {
   timeLagBeforeRelease: number;
   commission: number;
 }
 
-export async function deployLottery(config: ILotteryConfig): Promise<{
+export async function deployRaffle(config: ILotteryConfig): Promise<{
   erc20Instance: any;
   erc721Instance: any;
-  lotteryInstance: any;
+  raffleInstance: any;
   generateSignature: any;
 }> {
   const [owner] = await ethers.getSigners();
-  const factory = await ethers.getContractFactory(getContractName("LotteryRandom", network.name));
+  const factory = await ethers.getContractFactory(getContractName("RaffleRandom", network.name));
 
   const erc20Instance: any = await deployERC20("ERC20Simple", { amount: parseEther("200000") });
-  const erc721TicketInstance: any = await deployERC721("ERC721LotteryTicket");
+  const erc721TicketInstance: any = await deployERC721("ERC721RaffleTicket");
 
-  const lotteryInstance: any = await factory.deploy(config);
+  const raffleInstance: any = await factory.deploy(config);
 
   return {
     erc20Instance,
     erc721Instance: erc721TicketInstance,
-    lotteryInstance,
-    generateSignature: wrapLotterySignature(await ethers.provider.getNetwork(), lotteryInstance, owner),
+    raffleInstance,
+    generateSignature: wrapRaffleSignature(await ethers.provider.getNetwork(), raffleInstance, owner),
   };
 }
--- contracts/core/test/Mechanics/Raffle/utils.ts
+++ contracts/core/test/Mechanics/Raffle/utils.ts
@@ -2,22 +2,21 @@ import { Network, Signer } from "ethers";
 
 import { tokenName } from "@gemunion/contracts-constants";
 
-export const wrapLotterySignature = (network: Network, contract: any, account: Signer) => {
-  return async (values: Record<string, any>) => {
+export const wrapRaffleSignature = (network: Network, contract: any, account: Signer) => {
+  return (values: Record<string, any>) => {
     return account.signTypedData(
       // Domain
       {
         name: tokenName,
         version: "1.0.0",
         chainId: network.chainId,
-        verifyingContract: await contract.getAddress(),
+        verifyingContract: contract.address,
       },
       // Types
       {
         EIP712: [
           { name: "account", type: "address" },
           { name: "params", type: "Params" },
-          { name: "numbers", type: "bool[36]" },
           { name: "price", type: "Asset" },
         ],
         Params: [
