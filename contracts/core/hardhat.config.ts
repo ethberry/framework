8f90622f9e128f461893a3f6b2b4eaaaafff16ed
Ubudragon <gemuniongroup@gmail.com>
2023-08-02 17:56:22 +0800

    feat: prod config

--- contracts/core/hardhat.config.ts
+++ contracts/core/hardhat.config.ts
@@ -38,6 +38,16 @@ export default {
         "0xae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f", // 0xf17f52151EbEF6C7334FAD080c5704D77216b732
       ],
     },
+
+    gemunionprod: {
+      url: process.env.JSON_RPC_ADDR_GEMUNION,
+      timeout: 30000,
+      accounts: [
+        process.env.GEMUNION_PRIVATE_KEY_PROD, // 0x437D65F05fdaBC1e4683ba0730B6213206B210A0
+        "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3", // 0x627306090abaB3A6e1400e9345bC60c78a8BEf57
+        "0xae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f", // 0xf17f52151EbEF6C7334FAD080c5704D77216b732
+      ],
+    },
     binance: {
       url: process.env.JSON_RPC_ADDR_BINANCE,
       chainId: 56,
