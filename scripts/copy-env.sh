502de19ff4447e50877fc44a2a58f9655dc79961
Ubudragon <gemuniongroup@gmail.com>
2023-08-03 18:40:35 +0800

    fix: test env

--- scripts/copy-env.sh
+++ scripts/copy-env.sh
@@ -4,6 +4,7 @@ echo -e "\033[34mCopy test env files to the services folders...\n\033[0m";
 
 set -e # this will cause the shell to exit immediately if any command exits with a nonzero exit value.
 
+cp -rf $PWD/contracts/core/.env.sample $PWD/contracts/core/.env.test
 cp -rf $PWD/services/admin-api/.env.sample $PWD/services/admin-api/.env.test
 cp -rf $PWD/services/admin-ui/.env.sample $PWD/services/admin-ui/.env.test
 cp -rf $PWD/services/mobile-api/.env.sample $PWD/services/mobile-api/.env.test
