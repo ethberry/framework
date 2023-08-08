79ca8f76399b0712718719e6fc5806d18ee8b485
TrejGun <trejgun@gmail.com>
2023-08-08 08:18:58 +0800

    fix: validation

--- services/office-ui/src/pages/hierarchy/tokens/erc1155/contract/edit/validation.ts
+++ services/office-ui/src/pages/hierarchy/tokens/erc1155/contract/edit/validation.ts
@@ -1,12 +1,14 @@
 import { object, string } from "yup";
 
 import { draftValidationSchema } from "@gemunion/yup-rules";
+import { addressValidationSchema } from "@gemunion/yup-rules-eth";
 
 import { dbIdValidationSchema, urlValidationSchema } from "../../../../../../components/validation";
 
 export const validationSchema = object().shape({
   title: string().required("form.validations.valueMissing"),
   description: draftValidationSchema,
+  address: addressValidationSchema,
   imageUrl: urlValidationSchema,
   merchantId: dbIdValidationSchema,
 });
--- services/office-ui/src/pages/hierarchy/tokens/erc721/contract/edit/validation.ts
+++ services/office-ui/src/pages/hierarchy/tokens/erc721/contract/edit/validation.ts
@@ -1,11 +1,15 @@
 import { object, string } from "yup";
 
 import { draftValidationSchema } from "@gemunion/yup-rules";
+import { addressValidationSchema } from "@gemunion/yup-rules-eth";
+
 import { dbIdValidationSchema, urlValidationSchema } from "../../../../../../components/validation";
 
 export const validationSchema = object().shape({
+  symbol: string().required("form.validations.valueMissing").max(32, "form.validations.tooLong"),
   title: string().required("form.validations.valueMissing"),
   description: draftValidationSchema,
+  address: addressValidationSchema,
   imageUrl: urlValidationSchema,
   merchantId: dbIdValidationSchema,
 });
