64fd693f1d958fe5e9ebc02e863817a24144ff0a
Karafizi Arthur <karafizi.artur@gmail.com>
2023-08-10 22:59:05 +0800

    fix: raffle contracts card set to equal height

--- services/market-ui/src/pages/mechanics/raffle/raffle-list/item/index.tsx
+++ services/market-ui/src/pages/mechanics/raffle/raffle-list/item/index.tsx
@@ -1,9 +1,11 @@
 import { FC } from "react";
-import { Card, CardActionArea, CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
+import { CardActionArea, CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
 
 import type { IContract } from "@framework/types";
 import { RichTextDisplay } from "@gemunion/mui-rte";
+
+import { StyledCard } from "./styled";
 
 interface IRaffleListItemProps {
   contract: IContract;
@@ -13,7 +15,7 @@ export const RaffleListItem: FC<IRaffleListItemProps> = props => {
   const { contract } = props;
 
   return (
-    <Card>
+    <StyledCard>
       <CardActionArea component={RouterLink} to={`/raffle/contracts/${contract.id}`}>
         <CardHeader title={contract.title} />
         <CardMedia sx={{ height: 140 }} image={contract.imageUrl} title={`${contract.title}`} />
@@ -23,6 +25,6 @@ export const RaffleListItem: FC<IRaffleListItemProps> = props => {
           </Typography>
         </CardContent>
       </CardActionArea>
-    </Card>
+    </StyledCard>
   );
 };
