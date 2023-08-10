1d778e6e90f719e82420729e00943cc9ec1781dc
Ubudragon <gemuniongroup@gmail.com>
2023-08-10 22:07:42 +0800

    fix: naming

--- contracts/core/contracts/ERC20/ERC20Ownable.sol
+++ contracts/core/contracts/ERC20/ERC20Ownable.sol
@@ -8,7 +8,7 @@ pragma solidity ^0.8.13;
 
 import "@gemunion/contracts-erc20/contracts/preset/ERC20OBCS.sol";
 
-contract ERC20Simple is ERC20OBCS {
+contract ERC20Ownable is ERC20OBCS {
   constructor(string memory name, string memory symbol, uint256 cap) ERC20OBCS(name, symbol, cap) {}
 
   receive() external payable {
