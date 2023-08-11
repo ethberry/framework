a2f64a0658ff3d5cee0f724123997d4883eee69b
Ubudragon <gemuniongroup@gmail.com>
2023-08-11 22:27:37 +0800

    fix: env prod

--- scripts/copy-env.sh
+++ scripts/copy-env.sh
@@ -7,9 +7,11 @@ set -e # this will cause the shell to exit immediately if any command exits with
 cp -rf $PWD/contracts/core/.env.sample $PWD/contracts/core/.env.test
 cp -rf $PWD/services/admin-api/.env.sample $PWD/services/admin-api/.env.test
 cp -rf $PWD/services/admin-ui/.env.sample $PWD/services/admin-ui/.env.test
+cp -rf $PWD/services/admin-ui/.env.sample $PWD/services/admin-ui/.env.production
 cp -rf $PWD/services/mobile-api/.env.sample $PWD/services/mobile-api/.env.test
 cp -rf $PWD/services/market-api/.env.sample $PWD/services/market-api/.env.test
 cp -rf $PWD/services/market-ui/.env.sample $PWD/services/market-ui/.env.test
+cp -rf $PWD/services/market-ui/.env.sample $PWD/services/market-ui/.env.production
 cp -rf $PWD/services/core-eth/.env.sample $PWD/services/core-eth/.env.test
 cp -rf $PWD/microservices/eml/.env.sample $PWD/microservices/eml/.env.test
 cp -rf $PWD/microservices/game/.env.sample $PWD/microservices/game/.env.test
