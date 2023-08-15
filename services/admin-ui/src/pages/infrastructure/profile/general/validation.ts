a2af399991f3f925ebc143f3a38137199c0d6dd0
TrejGun <trejgun@gmail.com>
2023-08-15 17:43:23 +0800

    fix: better validation for profile

--- services/admin-ui/src/pages/infrastructure/profile/general/validation.ts
+++ services/admin-ui/src/pages/infrastructure/profile/general/validation.ts
@@ -7,8 +7,8 @@ import { displayNameValidationSchema, emailValidationSchema, urlValidationSchema
 export const validationSchema = object().shape({
   email: emailValidationSchema,
   displayName: displayNameValidationSchema,
-  gender: string().oneOf(Object.values(EnabledGenders)),
-  country: string().oneOf(Object.values(EnabledCountries)),
-  language: string().oneOf(Object.values(EnabledLanguages)),
+  gender: string().oneOf(Object.values(EnabledGenders)).required("form.validations.valueMissing"),
+  country: string().oneOf(Object.values(EnabledCountries)).required("form.validations.valueMissing"),
+  language: string().oneOf(Object.values(EnabledLanguages)).required("form.validations.valueMissing"),
   imageUrl: urlValidationSchema,
 });
