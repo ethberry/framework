3b9985821c8f94a6bd882315e91ca058b83917f1
TrejGun <trejgun@gmail.com>
2023-08-15 19:06:01 +0800

    fix: туц ьныеукнищчуы щт ьфклуе

--- services/market-api/src/blockchain/mechanics/mystery/box/box.controller.ts
+++ services/market-api/src/blockchain/mechanics/mystery/box/box.controller.ts
@@ -1,22 +1,17 @@
 import { Controller, Get, Param, ParseIntPipe, Query, UseInterceptors } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
 
 import { NotFoundInterceptor, PaginationInterceptor, Public, User } from "@gemunion/nest-js-utils";
-import { testChainId } from "@framework/constants";
 
+import { UserEntity } from "../../../../infrastructure/user/user.entity";
+import { TemplateNewDto } from "../../../hierarchy/template/dto";
 import { MysteryboxSearchDto } from "./dto";
 import { MysteryBoxService } from "./box.service";
 import { MysteryBoxEntity } from "./box.entity";
-import { UserEntity } from "../../../../infrastructure/user/user.entity";
-import { TemplateNewDto } from "../../../hierarchy/template/dto/new";
 
 @Public()
 @Controller("/mystery/boxes")
 export class MysteryBoxController {
-  constructor(
-    private readonly mysteryBoxService: MysteryBoxService,
-    private readonly configService: ConfigService,
-  ) {}
+  constructor(private readonly mysteryBoxService: MysteryBoxService) {}
 
   @Get("/")
   @UseInterceptors(PaginationInterceptor)
@@ -24,8 +19,7 @@ export class MysteryBoxController {
     @Query() dto: MysteryboxSearchDto,
     @User() userEntity: UserEntity,
   ): Promise<[Array<MysteryBoxEntity>, number]> {
-    const chainId = ~~this.configService.get<number>("CHAIN_ID", Number(testChainId));
-    return this.mysteryBoxService.search(dto, userEntity?.chainId || chainId);
+    return this.mysteryBoxService.search(dto, userEntity?.chainId);
   }
 
   @Get("/autocomplete")
@@ -39,8 +33,7 @@ export class MysteryBoxController {
     @Query() dto: TemplateNewDto,
     @User() userEntity: UserEntity,
   ): Promise<[Array<MysteryBoxEntity>, number]> {
-    const chainId = ~~this.configService.get<number>("CHAIN_ID", Number(testChainId));
-    return this.mysteryBoxService.search({ take: 10 }, userEntity?.chainId || chainId);
+    return this.mysteryBoxService.search({ take: 10 }, userEntity.chainId);
   }
 
   @Get("/:id")
--- services/market-api/src/blockchain/mechanics/mystery/box/box.module.ts
+++ services/market-api/src/blockchain/mechanics/mystery/box/box.module.ts
@@ -1,5 +1,4 @@
 import { Logger, Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { MysteryBoxEntity } from "./box.entity";
@@ -8,7 +7,7 @@ import { TemplateModule } from "../../../hierarchy/template/template.module";
 import { MysteryBoxController } from "./box.controller";
 
 @Module({
-  imports: [TemplateModule, ConfigModule, TypeOrmModule.forFeature([MysteryBoxEntity])],
+  imports: [TemplateModule, TypeOrmModule.forFeature([MysteryBoxEntity])],
   providers: [Logger, MysteryBoxService],
   controllers: [MysteryBoxController],
   exports: [MysteryBoxService],
