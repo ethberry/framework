8f5953e6f8713e1815da79a80481a32751141a92
TrejGun <trejgun@gmail.com>
2023-08-17 14:42:02 +0800

    fix: 1inch

--- services/market-ui/src/pages/integrations/dex/1inch/index.tsx
+++ services/market-ui/src/pages/integrations/dex/1inch/index.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 import { Grid } from "@mui/material";
 
 import { Breadcrumbs, PageHeader } from "@gemunion/mui-page-layout";
-import { OneInch as OneInchSwap } from "@gemunion/1inch";
+import { OneInch as OneInchSwap } from "@framework/1inch";
 
 export const OneInch: FC = () => {
   return (
