6d5af15431caa8aed33d98e31dceb3348fa2a68e
Ubudragon <gemuniongroup@gmail.com>
2023-08-26 22:20:37 +0800

    feat: craft approve alert

--- services/market-ui/src/pages/mechanics/craft/craft-item/index.tsx
+++ services/market-ui/src/pages/mechanics/craft/craft-item/index.tsx
@@ -82,7 +82,7 @@ export const CraftItem: FC = () => {
                 </ListItem>
               ))}
               <Alert severity="warning">
-                <FormattedMessage id="alert.approve" />
+                <FormattedMessage id={selected.inverse ? "alert.approve-inverse" : "alert.approve"} />
               </Alert>
               <CraftButton craft={selected} />
             </StyledPaper>
