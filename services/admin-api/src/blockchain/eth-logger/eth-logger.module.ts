c6c0401586d2773cb3a1226417fa02e1fb5f902d
TrejGun <trejgun@gmail.com>
2023-09-20 18:02:40 +0800

    fix: providers are not updated

--- services/admin-api/src/blockchain/eth-logger/eth-logger.module.ts
+++ services/admin-api/src/blockchain/eth-logger/eth-logger.module.ts
@@ -1,13 +1,23 @@
 import { Logger, Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 
-import { coreEthServiceProvider, ethTxInServiceProvider } from "../../common/providers";
+import {
+  coreEthServiceProviderBesu,
+  coreEthServiceProviderBinance,
+  ethTxInServiceProvider,
+} from "../../common/providers";
 import { EthLoggerService } from "./eth-logger.service";
 import { EthLoggerController } from "./eth-logger.controller";
 
 @Module({
   imports: [ConfigModule],
-  providers: [coreEthServiceProvider, ethTxInServiceProvider, Logger, EthLoggerService],
+  providers: [
+    coreEthServiceProviderBesu,
+    coreEthServiceProviderBinance,
+    ethTxInServiceProvider,
+    Logger,
+    EthLoggerService,
+  ],
   controllers: [EthLoggerController],
   exports: [EthLoggerService],
 })
