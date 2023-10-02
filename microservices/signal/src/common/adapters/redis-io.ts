4bf138cc800e78a47ffea00e06ff191816117ee0
Ubudragon <gemuniongroup@gmail.com>
2023-10-02 21:00:18 +0800

    feat: signal var name  [skip ci]

--- microservices/signal/src/common/adapters/redis-io.ts
+++ microservices/signal/src/common/adapters/redis-io.ts
@@ -21,11 +21,11 @@ export class RedisIoAdapter extends IoAdapter {
     const configService = this.app.get(ConfigService);
 
     const nodeEnv = configService.get<NodeEnv>("NODE_ENV", NodeEnv.development);
-    const adminB2BBaseUrl = configService.get<string>("ADMIN_FE_URL_B2B", "http://localhost:3005");
-    const adminB2BCaseUrl = configService.get<string>("ADMIN_FE_URL_B2C", "http://localhost:3005");
-    const marketB2BBaseUrl = configService.get<string>("MARKET_FE_URL_B2B", "http://localhost:3005");
-    const marketB2CBaseUrl = configService.get<string>("MARKET_FE_URL_B2B", "http://localhost:3005");
-    const officeBaseUrl = configService.get<string>("OFFICE_FE_URL", "http://localhost:3005");
+    const adminB2BUrl = configService.get<string>("ADMIN_FE_URL_B2B", "http://localhost:3005");
+    const adminB2CUrl = configService.get<string>("ADMIN_FE_URL_B2C", "http://localhost:3005");
+    const marketB2BUrl = configService.get<string>("MARKET_FE_URL_B2B", "http://localhost:3005");
+    const marketB2CUrl = configService.get<string>("MARKET_FE_URL_B2B", "http://localhost:3005");
+    const officeUrl = configService.get<string>("OFFICE_FE_URL", "http://localhost:3005");
     const baseUrl = configService.get<string>("PUBLIC_FE_URL", "http://localhost:3005");
     const ioAdminUrl = configService.get<string>("SIGNAL_FE_URL", "http://localhost:3015");
 
@@ -38,15 +38,7 @@ export class RedisIoAdapter extends IoAdapter {
         origin:
           nodeEnv === NodeEnv.development
             ? ["https://admin.socket.io", "http://localhost:3002", "http://localhost:3004", "http://localhost:3006"]
-            : [
-                adminB2BBaseUrl,
-                adminB2BCaseUrl,
-                marketB2BBaseUrl,
-                marketB2CBaseUrl,
-                officeBaseUrl,
-                baseUrl,
-                ioAdminUrl,
-              ],
+            : [adminB2BUrl, adminB2CUrl, marketB2BUrl, marketB2CUrl, officeUrl, ioAdminUrl, baseUrl],
       },
     });
 
