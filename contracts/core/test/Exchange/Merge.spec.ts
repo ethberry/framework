0acb0d50610258def733a3632185dfe8803a2d4b
TrejGun <trejgun@gmail.com>
2023-10-22 19:18:39 +0800

    feat: Merge Facet, permutations

--- contracts/core/test/Exchange/Merge.spec.ts
+++ contracts/core/test/Exchange/Merge.spec.ts
@@ -185,6 +185,452 @@ describe("Diamond Exchange Merge", function () {
           .withArgs(receiver.address, ZeroAddress, tokenId + 2n);
       });
     });
+
+    describe("ERC721 > ERC998", function () {
+      it("should merge", async function () {
+        const [owner, receiver] = await ethers.getSigners();
+        const exchangeInstance = await factory();
+        const { generateManyToManySignature } = await getSignatures(exchangeInstance);
+        const erc721Instance = await deployErc721Base("ERC721Simple", exchangeInstance);
+        const erc998Instance = await deployErc721Base("ERC998Simple", exchangeInstance);
+
+        const tx01 = erc721Instance.mintCommon(receiver.address, templateId);
+        await expect(tx01)
+          .to.emit(erc721Instance, "Transfer")
+          .withArgs(ZeroAddress, receiver.address, tokenId + 0n);
+        await erc721Instance.connect(receiver).approve(await exchangeInstance.getAddress(), tokenId + 0n);
+
+        const tx02 = erc721Instance.mintCommon(receiver.address, templateId);
+        await expect(tx02)
+          .to.emit(erc721Instance, "Transfer")
+          .withArgs(ZeroAddress, receiver.address, tokenId + 1n);
+        await erc721Instance.connect(receiver).approve(await exchangeInstance.getAddress(), tokenId + 1n);
+
+        const tx03 = erc721Instance.mintCommon(receiver.address, templateId);
+        await expect(tx03)
+          .to.emit(erc721Instance, "Transfer")
+          .withArgs(ZeroAddress, receiver.address, tokenId + 2n);
+        await erc721Instance.connect(receiver).approve(await exchangeInstance.getAddress(), tokenId + 2n);
+
+        const signature = await generateManyToManySignature({
+          account: receiver.address,
+          params: {
+            nonce: encodeBytes32String("nonce"),
+            externalId,
+            expiresAt,
+            receiver: owner.address,
+            referrer: ZeroAddress,
+            extra: zeroPadValue(toBeHex(templateId), 32),
+          },
+          items: [
+            {
+              tokenType: 3,
+              token: await erc998Instance.getAddress(),
+              tokenId: 1,
+              amount: 1,
+            },
+          ],
+          price: [
+            {
+              tokenType: 2,
+              token: await erc721Instance.getAddress(),
+              tokenId: tokenId + 0n,
+              amount: 1,
+            },
+            {
+              tokenType: 2,
+              token: await erc721Instance.getAddress(),
+              tokenId: tokenId + 1n,
+              amount: 1,
+            },
+            {
+              tokenType: 2,
+              token: await erc721Instance.getAddress(),
+              tokenId: tokenId + 2n,
+              amount: 1,
+            },
+          ],
+        });
+
+        const tx1 = exchangeInstance.connect(receiver).merge(
+          {
+            nonce: encodeBytes32String("nonce"),
+            externalId,
+            expiresAt,
+            receiver: owner.address,
+            referrer: ZeroAddress,
+            extra: zeroPadValue(toBeHex(templateId), 32),
+          },
+          [
+            {
+              tokenType: 3,
+              token: await erc998Instance.getAddress(),
+              tokenId: 1,
+              amount: 1,
+            },
+          ],
+          [
+            {
+              tokenType: 2,
+              token: await erc721Instance.getAddress(),
+              tokenId: tokenId + 0n,
+              amount: 1,
+            },
+            {
+              tokenType: 2,
+              token: await erc721Instance.getAddress(),
+              tokenId: tokenId + 1n,
+              amount: 1,
+            },
+            {
+              tokenType: 2,
+              token: await erc721Instance.getAddress(),
+              tokenId: tokenId + 2n,
+              amount: 1,
+            },
+          ],
+          signature,
+        );
+
+        await expect(tx1)
+          .to.emit(exchangeInstance, "Merge")
+          .withArgs(
+            receiver.address,
+            externalId,
+            isEqualEventArgArrObj({
+              tokenType: 3n,
+              token: await erc998Instance.getAddress(),
+              tokenId: 1n,
+              amount: 1n,
+            }),
+            isEqualEventArgArrObj(
+              {
+                tokenType: 2n,
+                token: await erc721Instance.getAddress(),
+                tokenId: tokenId + 0n,
+                amount: 1n,
+              },
+              {
+                tokenType: 2n,
+                token: await erc721Instance.getAddress(),
+                tokenId: tokenId + 1n,
+                amount: 1n,
+              },
+              {
+                tokenType: 2n,
+                token: await erc721Instance.getAddress(),
+                tokenId: tokenId + 2n,
+                amount: 1n,
+              },
+            ),
+          )
+          .to.emit(erc998Instance, "Transfer")
+          .withArgs(ZeroAddress, receiver.address, 1)
+          .to.emit(erc721Instance, "Transfer")
+          .withArgs(receiver.address, ZeroAddress, tokenId + 0n)
+          .to.emit(erc721Instance, "Transfer")
+          .withArgs(receiver.address, ZeroAddress, tokenId + 1n)
+          .to.emit(erc721Instance, "Transfer")
+          .withArgs(receiver.address, ZeroAddress, tokenId + 2n);
+      });
+    });
+
+    describe("ERC998 > ERC998", function () {
+      it("should merge", async function () {
+        const [owner, receiver] = await ethers.getSigners();
+        const exchangeInstance = await factory();
+        const { generateManyToManySignature } = await getSignatures(exchangeInstance);
+        const erc998Instance = await deployErc721Base("ERC998Simple", exchangeInstance);
+
+        const tx01 = erc998Instance.mintCommon(receiver.address, templateId);
+        await expect(tx01)
+          .to.emit(erc998Instance, "Transfer")
+          .withArgs(ZeroAddress, receiver.address, tokenId + 0n);
+        await erc998Instance.connect(receiver).approve(await exchangeInstance.getAddress(), tokenId + 0n);
+
+        const tx02 = erc998Instance.mintCommon(receiver.address, templateId);
+        await expect(tx02)
+          .to.emit(erc998Instance, "Transfer")
+          .withArgs(ZeroAddress, receiver.address, tokenId + 1n);
+        await erc998Instance.connect(receiver).approve(await exchangeInstance.getAddress(), tokenId + 1n);
+
+        const tx03 = erc998Instance.mintCommon(receiver.address, templateId);
+        await expect(tx03)
+          .to.emit(erc998Instance, "Transfer")
+          .withArgs(ZeroAddress, receiver.address, tokenId + 2n);
+        await erc998Instance.connect(receiver).approve(await exchangeInstance.getAddress(), tokenId + 2n);
+
+        const signature = await generateManyToManySignature({
+          account: receiver.address,
+          params: {
+            nonce: encodeBytes32String("nonce"),
+            externalId,
+            expiresAt,
+            receiver: owner.address,
+            referrer: ZeroAddress,
+            extra: zeroPadValue(toBeHex(templateId), 32),
+          },
+          items: [
+            {
+              tokenType: 3,
+              token: await erc998Instance.getAddress(),
+              tokenId: 2,
+              amount: 1,
+            },
+          ],
+          price: [
+            {
+              tokenType: 3,
+              token: await erc998Instance.getAddress(),
+              tokenId: tokenId + 0n,
+              amount: 1,
+            },
+            {
+              tokenType: 3,
+              token: await erc998Instance.getAddress(),
+              tokenId: tokenId + 1n,
+              amount: 1,
+            },
+            {
+              tokenType: 3,
+              token: await erc998Instance.getAddress(),
+              tokenId: tokenId + 2n,
+              amount: 1,
+            },
+          ],
+        });
+
+        const tx1 = exchangeInstance.connect(receiver).merge(
+          {
+            nonce: encodeBytes32String("nonce"),
+            externalId,
+            expiresAt,
+            receiver: owner.address,
+            referrer: ZeroAddress,
+            extra: zeroPadValue(toBeHex(templateId), 32),
+          },
+          [
+            {
+              tokenType: 3,
+              token: await erc998Instance.getAddress(),
+              tokenId: 2,
+              amount: 1,
+            },
+          ],
+          [
+            {
+              tokenType: 3,
+              token: await erc998Instance.getAddress(),
+              tokenId: tokenId + 0n,
+              amount: 1,
+            },
+            {
+              tokenType: 3,
+              token: await erc998Instance.getAddress(),
+              tokenId: tokenId + 1n,
+              amount: 1,
+            },
+            {
+              tokenType: 3,
+              token: await erc998Instance.getAddress(),
+              tokenId: tokenId + 2n,
+              amount: 1,
+            },
+          ],
+          signature,
+        );
+
+        await expect(tx1)
+          .to.emit(exchangeInstance, "Merge")
+          .withArgs(
+            receiver.address,
+            externalId,
+            isEqualEventArgArrObj({
+              tokenType: 3n,
+              token: await erc998Instance.getAddress(),
+              tokenId: 2n,
+              amount: 1n,
+            }),
+            isEqualEventArgArrObj(
+              {
+                tokenType: 3n,
+                token: await erc998Instance.getAddress(),
+                tokenId: tokenId + 0n,
+                amount: 1n,
+              },
+              {
+                tokenType: 3n,
+                token: await erc998Instance.getAddress(),
+                tokenId: tokenId + 1n,
+                amount: 1n,
+              },
+              {
+                tokenType: 3n,
+                token: await erc998Instance.getAddress(),
+                tokenId: tokenId + 2n,
+                amount: 1n,
+              },
+            ),
+          )
+          .to.emit(erc998Instance, "Transfer")
+          .withArgs(ZeroAddress, receiver.address, tokenId + 3n)
+          .to.emit(erc998Instance, "Transfer")
+          .withArgs(receiver.address, ZeroAddress, tokenId + 0n)
+          .to.emit(erc998Instance, "Transfer")
+          .withArgs(receiver.address, ZeroAddress, tokenId + 1n)
+          .to.emit(erc998Instance, "Transfer")
+          .withArgs(receiver.address, ZeroAddress, tokenId + 2n);
+      });
+    });
+
+    describe("ERC998 > ERC721", function () {
+      it("should merge", async function () {
+        const [owner, receiver] = await ethers.getSigners();
+        const exchangeInstance = await factory();
+        const { generateManyToManySignature } = await getSignatures(exchangeInstance);
+        const erc721Instance = await deployErc721Base("ERC721Simple", exchangeInstance);
+        const erc998Instance = await deployErc721Base("ERC998Simple", exchangeInstance);
+
+        const tx01 = erc998Instance.mintCommon(receiver.address, templateId);
+        await expect(tx01)
+          .to.emit(erc998Instance, "Transfer")
+          .withArgs(ZeroAddress, receiver.address, tokenId + 0n);
+        await erc998Instance.connect(receiver).approve(await exchangeInstance.getAddress(), tokenId + 0n);
+
+        const tx02 = erc998Instance.mintCommon(receiver.address, templateId);
+        await expect(tx02)
+          .to.emit(erc998Instance, "Transfer")
+          .withArgs(ZeroAddress, receiver.address, tokenId + 1n);
+        await erc998Instance.connect(receiver).approve(await exchangeInstance.getAddress(), tokenId + 1n);
+
+        const tx03 = erc998Instance.mintCommon(receiver.address, templateId);
+        await expect(tx03)
+          .to.emit(erc998Instance, "Transfer")
+          .withArgs(ZeroAddress, receiver.address, tokenId + 2n);
+        await erc998Instance.connect(receiver).approve(await exchangeInstance.getAddress(), tokenId + 2n);
+
+        const signature = await generateManyToManySignature({
+          account: receiver.address,
+          params: {
+            nonce: encodeBytes32String("nonce"),
+            externalId,
+            expiresAt,
+            receiver: owner.address,
+            referrer: ZeroAddress,
+            extra: zeroPadValue(toBeHex(templateId), 32),
+          },
+          items: [
+            {
+              tokenType: 2,
+              token: await erc721Instance.getAddress(),
+              tokenId: 1,
+              amount: 1,
+            },
+          ],
+          price: [
+            {
+              tokenType: 3,
+              token: await erc998Instance.getAddress(),
+              tokenId: tokenId + 0n,
+              amount: 1,
+            },
+            {
+              tokenType: 3,
+              token: await erc998Instance.getAddress(),
+              tokenId: tokenId + 1n,
+              amount: 1,
+            },
+            {
+              tokenType: 3,
+              token: await erc998Instance.getAddress(),
+              tokenId: tokenId + 2n,
+              amount: 1,
+            },
+          ],
+        });
+
+        const tx1 = exchangeInstance.connect(receiver).merge(
+          {
+            nonce: encodeBytes32String("nonce"),
+            externalId,
+            expiresAt,
+            receiver: owner.address,
+            referrer: ZeroAddress,
+            extra: zeroPadValue(toBeHex(templateId), 32),
+          },
+          [
+            {
+              tokenType: 2,
+              token: await erc721Instance.getAddress(),
+              tokenId: 1,
+              amount: 1,
+            },
+          ],
+          [
+            {
+              tokenType: 3,
+              token: await erc998Instance.getAddress(),
+              tokenId: tokenId + 0n,
+              amount: 1,
+            },
+            {
+              tokenType: 3,
+              token: await erc998Instance.getAddress(),
+              tokenId: tokenId + 1n,
+              amount: 1,
+            },
+            {
+              tokenType: 3,
+              token: await erc998Instance.getAddress(),
+              tokenId: tokenId + 2n,
+              amount: 1,
+            },
+          ],
+          signature,
+        );
+
+        await expect(tx1)
+          .to.emit(exchangeInstance, "Merge")
+          .withArgs(
+            receiver.address,
+            externalId,
+            isEqualEventArgArrObj({
+              tokenType: 2n,
+              token: await erc721Instance.getAddress(),
+              tokenId: 1n,
+              amount: 1n,
+            }),
+            isEqualEventArgArrObj(
+              {
+                tokenType: 3n,
+                token: await erc998Instance.getAddress(),
+                tokenId: tokenId + 0n,
+                amount: 1n,
+              },
+              {
+                tokenType: 3n,
+                token: await erc998Instance.getAddress(),
+                tokenId: tokenId + 1n,
+                amount: 1n,
+              },
+              {
+                tokenType: 3n,
+                token: await erc998Instance.getAddress(),
+                tokenId: tokenId + 2n,
+                amount: 1n,
+              },
+            ),
+          )
+          .to.emit(erc721Instance, "Transfer")
+          .withArgs(ZeroAddress, receiver.address, 1)
+          .to.emit(erc998Instance, "Transfer")
+          .withArgs(receiver.address, ZeroAddress, tokenId + 0n)
+          .to.emit(erc998Instance, "Transfer")
+          .withArgs(receiver.address, ZeroAddress, tokenId + 1n)
+          .to.emit(erc998Instance, "Transfer")
+          .withArgs(receiver.address, ZeroAddress, tokenId + 2n);
+      });
+    });
   });
 
   describe("ERROR", function () {
