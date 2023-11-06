04966baf6d54c421a3e1183ad3f91b2448467605
TrejGun <trejgun@gmail.com>
2023-11-06 12:03:10 +0800

    fix: add header to feedback

--- services/market-ui/src/pages/infrastructure/feedback/index.tsx
+++ services/market-ui/src/pages/infrastructure/feedback/index.tsx
@@ -1,8 +1,9 @@
-import { FC } from "react";
+import { FC, Fragment } from "react";
 
 import { FormWrapper } from "@gemunion/mui-form";
 import { TextArea } from "@gemunion/mui-inputs-core";
 import { useApiCall } from "@gemunion/react-hooks";
+import { Breadcrumbs, PageHeader } from "@gemunion/mui-page-layout";
 
 import { validationSchema } from "./validation";
 
@@ -22,16 +23,22 @@ export const Feedback: FC = () => {
   };
 
   return (
-    <FormWrapper
-      initialValues={{
-        text: "",
-      }}
-      validationSchema={validationSchema}
-      onSubmit={handleSubmit}
-      showPrompt={false}
-      testId="FeedbackForm"
-    >
-      <TextArea name="text" />
-    </FormWrapper>
+    <Fragment>
+      <Breadcrumbs path={["dashboard", "feedback"]} />
+
+      <PageHeader message={"pages.feedback.title"} />
+
+      <FormWrapper
+        initialValues={{
+          text: "",
+        }}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+        showPrompt={false}
+        testId="FeedbackForm"
+      >
+        <TextArea name="text" />
+      </FormWrapper>
+    </Fragment>
   );
 };
