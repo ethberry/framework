7b664a62ecfc334f84d6ac4079fbde2967ba8456
Karafizi Arthur <karafizi.artur@gmail.com>
2023-11-07 15:34:47 +0800

    fix: better validation for edit staking rules

--- services/admin-ui/src/components/buttons/mechanics/lottery/contract/round-start/round-dialog/validation.ts
+++ services/admin-ui/src/components/buttons/mechanics/lottery/contract/round-start/round-dialog/validation.ts
@@ -1,9 +1,11 @@
 import { number, object } from "yup";
+
 import { templateAssetValidationSchema } from "@gemunion/mui-inputs-asset";
 import { addressValidationSchema } from "@gemunion/yup-rules-eth";
+import { dbIdValidationSchema } from "@gemunion/yup-rules";
 
-// TODO validations
 export const validationSchema = object().shape({
+  contractId: dbIdValidationSchema,
   address: addressValidationSchema,
   ticket: templateAssetValidationSchema,
   price: templateAssetValidationSchema,
--- services/admin-ui/src/pages/mechanics/staking/rules/edit/validation.ts
+++ services/admin-ui/src/pages/mechanics/staking/rules/edit/validation.ts
@@ -1,9 +1,16 @@
-import { object, string } from "yup";
+import { mixed, object, string } from "yup";
 
-import { draftValidationSchema } from "@gemunion/yup-rules";
+import { templateAssetValidationSchema } from "@gemunion/mui-inputs-asset";
+import { dbIdValidationSchema, draftValidationSchema } from "@gemunion/yup-rules";
+import { StakingRuleStatus } from "@framework/types";
 
-// TODO add more validation
 export const validationSchema = object().shape({
   title: string().required("form.validations.valueMissing"),
   description: draftValidationSchema,
+  stakingRuleStatus: mixed<StakingRuleStatus>()
+    .oneOf(Object.values(StakingRuleStatus))
+    .required("form.validations.valueMissing"),
+  deposit: templateAssetValidationSchema,
+  reward: templateAssetValidationSchema,
+  contractId: dbIdValidationSchema,
 });
