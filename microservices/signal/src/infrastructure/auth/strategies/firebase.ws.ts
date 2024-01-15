179bda856484ba3ec70304dd139440c2d8e9b679
Ubudragon <gemuniongroup@gmail.com>
2024-01-15 18:55:49 +0800

    fix: firebase.ws types [skip ci]

--- microservices/signal/src/infrastructure/auth/strategies/firebase.ws.ts
+++ microservices/signal/src/infrastructure/auth/strategies/firebase.ws.ts
@@ -11,7 +11,7 @@ import { UserEntity } from "../../user/user.entity";
 import { APP_PROVIDER } from "../auth.constants";
 
 // https://socket.io/docs/v4/handling-cors/
-const ExtractJwtFromSocketIoHandshake: JwtFromRequestFunction = (req: Request) => {
+const ExtractJwtFromSocketIoHandshake: JwtFromRequestFunction<any> = (req: Request) => {
   // @ts-ignore
   return ExtractJwt.fromAuthHeaderWithScheme("bearer")(req.handshake);
 };
