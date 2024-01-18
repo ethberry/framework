0acbe99b16d3b642542a1a2361a6b0546f0f60be
Ubudragon <gemuniongroup@gmail.com>
2024-01-18 19:36:23 +0800

    fix: CI TEST [skip ci]

--- scripts/copy-env.sh
+++ scripts/copy-env.sh
@@ -12,6 +12,8 @@ cp -rf $PWD/services/mobile-api/.env.sample $PWD/services/mobile-api/.env.test
 cp -rf $PWD/services/market-api/.env.sample $PWD/services/market-api/.env.test
 cp -rf $PWD/services/market-ui/.env.sample $PWD/services/market-ui/.env.test
 cp -rf $PWD/services/market-ui/.env.sample $PWD/services/market-ui/.env.production
+cp -rf $PWD/services/office-ui/.env.sample $PWD/services/office-ui/.env.test
+cp -rf $PWD/services/office-ui/.env.sample $PWD/services/office-ui/.env.production
 cp -rf $PWD/services/core-eth/.env.sample $PWD/services/core-eth/.env.test
 cp -rf $PWD/microservices/eml/.env.sample $PWD/microservices/eml/.env.test
 cp -rf $PWD/microservices/game/.env.sample $PWD/microservices/game/.env.test
