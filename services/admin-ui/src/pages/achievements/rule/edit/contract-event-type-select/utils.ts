b42c1fb3c3d1ce998417b7c295cc9ee636eea505
Ubudragon <gemuniongroup@gmail.com>
2024-01-30 17:36:17 +0800

    feat: achievements events select [skip ci]

--- services/admin-ui/src/pages/achievements/rule/edit/contract-event-type-select/utils.ts
+++ services/admin-ui/src/pages/achievements/rule/edit/contract-event-type-select/utils.ts
@@ -1,27 +1,136 @@
-import { type IContract, ModuleType, TokenType } from "@framework/types";
+import { ContractFeatures, type IContract, ModuleType, TokenType } from "@framework/types";
 
 export enum ContractHierarchyERC20EventType {
   Approval = "Approval",
   Transfer = "Transfer",
+  Blacklisted = "Blacklisted",
+  UnBlacklisted = "UnBlacklisted",
+  Whitelisted = "Whitelisted",
+  UnWhitelisted = "UnWhitelisted",
+  RoleGranted = "RoleGranted",
+  RoleRevoked = "RoleRevoked",
+}
+
+export enum ContractHierarchyERC1155EventType {
+  TransferSingle = "TransferSingle",
+  TransferBatch = "TransferBatch",
+  ApprovalForAll = "ApprovalForAll",
+  URI = "URI",
 }
 
 export enum ContractHierarchyERC721EventType {
-  ApprovalForAll = "ApprovalForAll",
-  DefaultRoyaltyInfo = "DefaultRoyaltyInfo",
-  MintRandom = "MintRandom",
-  TokenRoyaltyInfo = "TokenRoyaltyInfo",
-  ConsecutiveTransfer = "ConsecutiveTransfer",
+  Approval = "Approval",
+  RedeemClaim = "RedeemClaim",
+  UnpackClaim = "UnpackClaim",
+  UnpackMysteryBox = "UnpackMysteryBox",
+  LevelUp = "LevelUp",
+  Blacklisted = "Blacklisted",
+  UnBlacklisted = "UnBlacklisted",
+  Transfer = "Transfer",
 }
 
+export enum ContractHierarchyERC721RandomEventType {
+  MintRandom = "MintRandom",
+  Approval = "Approval",
+  RedeemClaim = "RedeemClaim",
+  UnpackClaim = "UnpackClaim",
+  UnpackMysteryBox = "UnpackMysteryBox",
+  LevelUp = "LevelUp",
+  Blacklisted = "Blacklisted",
+  UnBlacklisted = "UnBlacklisted",
+  Transfer = "Transfer",
+}
+
+export enum ContractHierarchyERC998EventType {
+  Approval = "Approval",
+  Blacklisted = "Blacklisted",
+  LevelUp = "LevelUp",
+  ReceivedChild = "ReceivedChild",
+  RedeemClaim = "RedeemClaim",
+  Transfer = "Transfer",
+  TransferChild = "TransferChild",
+  UnBlacklisted = "UnBlacklisted",
+  UnWhitelistedChild = "UnWhitelistedChild",
+  UnpackClaim = "UnpackClaim",
+  UnpackMysteryBox = "UnpackMysteryBox",
+  WhitelistedChild = "WhitelistedChild",
+}
+
+export enum ContractHierarchyERC998RandomEventType {
+  Approval = "Approval",
+  Blacklisted = "Blacklisted",
+  LevelUp = "LevelUp",
+  MintRandom = "MintRandom",
+  ReceivedChild = "ReceivedChild",
+  RedeemClaim = "RedeemClaim",
+  Transfer = "Transfer",
+  TransferChild = "TransferChild",
+  UnBlacklisted = "UnBlacklisted",
+  UnWhitelistedChild = "UnWhitelistedChild",
+  UnpackClaim = "UnpackClaim",
+  UnpackMysteryBox = "UnpackMysteryBox",
+  WhitelistedChild = "WhitelistedChild",
+}
+
+// EXCHANGE ACHIEVEMENT EVENTS
+export enum ContractExchangeEventType {
+  Breed = "Breed",
+  Claim = "Claim",
+  Craft = "Craft",
+  Dismantle = "Dismantle",
+  Lend = "Lend",
+  Merge = "Merge",
+  Purchase = "Purchase",
+  PurchaseLottery = "PurchaseLottery",
+  PurchaseMysteryBox = "PurchaseMysteryBox",
+  PurchaseRaffle = "PurchaseRaffle",
+  ReferralEvent = "ReferralEvent",
+  Upgrade = "Upgrade",
+}
+
+// CM ACHIEVEMENT EVENTS
+export enum ContractManagerEventType {
+  CollectionDeployed = "CollectionDeployed",
+  ERC1155TokenDeployed = "ERC1155TokenDeployed",
+  ERC20TokenDeployed = "ERC20TokenDeployed",
+  ERC721TokenDeployed = "ERC721TokenDeployed",
+  ERC998TokenDeployed = "ERC998TokenDeployed",
+  LotteryDeployed = "LotteryDeployed",
+  MysteryBoxDeployed = "MysteryBoxDeployed",
+  PaymentSplitterDeployed = "PaymentSplitterDeployed",
+  PonziDeployed = "PonziDeployed",
+  RaffleDeployed = "RaffleDeployed",
+  StakingDeployed = "StakingDeployed",
+  VestingDeployed = "VestingDeployed",
+  WaitListDeployed = "WaitListDeployed",
+}
+
+// HIERARCHY ACHIEVEMENT EVENTS
 export const getHierarchyEventType = (contract: IContract): Record<string, string> => {
-  const { contractType } = contract;
+  const { contractType, contractFeatures } = contract;
 
   switch (contractType) {
     case TokenType.ERC721:
-      return ContractHierarchyERC721EventType;
+      switch (contractFeatures.includes(ContractFeatures.RANDOM) || contractFeatures.includes(ContractFeatures.GENES)) {
+        case true:
+          return ContractHierarchyERC721RandomEventType;
+        default:
+          return ContractHierarchyERC721EventType;
+      }
+    case TokenType.ERC998:
+      switch (contractFeatures.includes(ContractFeatures.RANDOM) || contractFeatures.includes(ContractFeatures.GENES)) {
+        case true:
+          return ContractHierarchyERC998RandomEventType;
+        default:
+          return ContractHierarchyERC998EventType;
+      }
     case TokenType.ERC20:
-    default:
       return ContractHierarchyERC20EventType;
+    case TokenType.ERC1155:
+      return ContractHierarchyERC1155EventType;
+    case TokenType.NATIVE:
+    default:
+      return {};
   }
 };
 
@@ -36,7 +145,14 @@ export const getEventTypeByContractModule = (contract?: IContract): Record<strin
 
   switch (contractModule) {
     case ModuleType.HIERARCHY:
-    default:
       return getHierarchyEventType(contract);
+    case ModuleType.EXCHANGE:
+      return ContractExchangeEventType;
+    case ModuleType.CONTRACT_MANAGER:
+      return ContractManagerEventType;
+    default: {
+      // throw new Error("wrong contract");
+      return {};
+    }
   }
 };
