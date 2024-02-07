22d7b44e2bd846a1ff8bd1b966a446a6428efc86
Karafizi Arthur <karafizi.artur@gmail.com>
2024-02-07 10:58:08 +0800

    fix: market - remove transfer button from erc20 token list item

--- services/market-ui/src/pages/hierarchy/erc20/token-list/item/index.tsx
+++ services/market-ui/src/pages/hierarchy/erc20/token-list/item/index.tsx
@@ -1,11 +1,11 @@
 import { FC } from "react";
 import { Card, CardActionArea, CardActions, CardContent, CardHeader, Grid } from "@mui/material";
 
-import { RichTextDisplay } from "@gemunion/mui-rte";
-import { ListActionVariant, StyledCardContentDescription, StyledCardMedia } from "@framework/styled";
 import type { IToken } from "@framework/types";
+import { StyledCardContentDescription, StyledCardMedia } from "@framework/styled";
+import { RichTextDisplay } from "@gemunion/mui-rte";
 
-import { Erc20AddToMetamaskButton, Erc20TransferButton } from "../../../../../components/buttons";
+import { Erc20AddToMetamaskButton } from "../../../../../components/buttons";
 
 interface IErc20TokenListItemProps {
   token: IToken;
@@ -28,7 +28,6 @@ export const Erc20TokenListItem: FC<IErc20TokenListItemProps> = props => {
       <CardActions>
         <Grid container alignItems="center">
           <Grid item xs={12}>
-            <Erc20TransferButton token={token} variant={ListActionVariant.iconButton} />
             <Erc20AddToMetamaskButton token={token} />
           </Grid>
         </Grid>
