13289aafad9a64affe794b11bdd5a230a9921480
Karafizi Arthur <karafizi.artur@gmail.com>
2024-02-26 14:27:12 +0800

    fix: duration unit were hidden - set duration for staking/vesting always in days

--- services/office-ui/src/components/inputs/duration/index.tsx
+++ services/office-ui/src/components/inputs/duration/index.tsx
@@ -1,9 +1,9 @@
-import { FC, useCallback, useEffect, useMemo } from "react";
-import { InputAdornment } from "@mui/material";
-import { useFormContext, useWatch } from "react-hook-form";
+import { FC, useCallback } from "react";
+import { useIntl } from "react-intl";
+import { useWatch } from "react-hook-form";
 
 import { DurationUnit } from "@framework/types";
-import { NumberInput, SelectInput } from "@gemunion/mui-inputs-core";
+import { NumberInput } from "@gemunion/mui-inputs-core";
 
 import { formatDuration, normalizeDuration } from "../../../utils/time";
 
@@ -14,9 +14,10 @@ export interface IDurationInput {
 export const DurationInput: FC<IDurationInput> = props => {
   const { readOnly = false } = props;
 
-  const form = useFormContext();
+  // const form = useFormContext();
+  const { formatMessage } = useIntl();
 
-  const durationAmount: number = useWatch({ name: "durationAmount" });
+  // const durationAmount: number = useWatch({ name: "durationAmount" });
   const durationUnit: DurationUnit = useWatch({ name: "durationUnit" });
 
   const normalizeValue = useCallback(
@@ -35,29 +36,31 @@ export const DurationInput: FC<IDurationInput> = props => {
 
   // memoize value to handle changing durationUnit
   // and setting correct value to the form according to the new durationUnit
-  const memoizedValue = useMemo(() => normalizeValue(durationAmount), [durationAmount]);
-  useEffect(() => {
-    form.setValue("durationAmount", formatValue(memoizedValue), { shouldTouch: true });
-  }, [durationUnit]);
+  // const memoizedValue = useMemo(() => normalizeValue(durationAmount), [durationAmount]);
+  // useEffect(() => {
+  //   form.setValue("durationAmount", formatValue(memoizedValue), { shouldTouch: true });
+  // }, [durationUnit]);
 
   return (
     <NumberInput
       name="durationAmount"
+      label={formatMessage({ id: "form.labels.durationInDays" })}
       formatValue={formatValue}
       normalizeValue={normalizeValue}
       onBlur={() => {}}
-      InputProps={{
-        endAdornment: (
-          <InputAdornment position="start" sx={{ ml: 1 }}>
-            <SelectInput
-              sx={{ width: 90, mr: -1, mb: 2 }}
-              name="durationUnit"
-              options={DurationUnit}
-              readOnly={readOnly}
-            />
-          </InputAdornment>
-        ),
-      }}
+      // DurationUnit is hidden for now
+      // InputProps={{
+      //   endAdornment: (
+      //     <InputAdornment position="start" sx={{ ml: 1 }}>
+      //       <SelectInput
+      //         sx={{ width: 90, mr: -1, mb: 2 }}
+      //         name="durationUnit"
+      //         options={DurationUnit}
+      //         readOnly={readOnly}
+      //       />
+      //     </InputAdornment>
+      //   ),
+      // }}
       readOnly={readOnly}
     />
   );
