3df1602a51bf5ca500126eacd6d629de1eb7391b
TrejGun <trejgun@gmail.com>
2024-03-18 19:51:19 +0800

    feat: enable erc998, part 2

--- services/office-ui/src/pages/dashboard/hierarchy/erc998.tsx
+++ services/office-ui/src/pages/dashboard/hierarchy/erc998.tsx
@@ -4,15 +4,7 @@ import { AutoAwesomeMotion, Collections, Extension, Storage } from "@mui/icons-m
 import { Link as RouterLink } from "react-router-dom";
 import { FormattedMessage } from "react-intl";
 
-import { NodeEnv } from "@framework/types";
-
 export const Erc998Section: FC = () => {
-  const isProd = process.env.NODE_ENV === NodeEnv.production;
-
-  if (isProd) {
-    return null;
-  }
-
   return (
     <Paper sx={{ mb: 2 }}>
       <List
