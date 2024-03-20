e5a20144855710cae772d25e4d1f07887e32a1d7
TrejGun <trejgun@gmail.com>
2024-03-20 10:23:53 +0800

    chore: use external baseTokenURI

--- services/admin-api/src/migrations/02-blockchain/04-mechanics/18-payment-splitter/1697876719370-seed-contract-payment-splitter.ts
+++ services/admin-api/src/migrations/02-blockchain/04-mechanics/18-payment-splitter/1697876719370-seed-contract-payment-splitter.ts
@@ -1,8 +1,9 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 import { wallet } from "@gemunion/constants";
+import { baseTokenURI } from "@gemunion/contracts-constants";
 import { simpleFormatting } from "@gemunion/draft-js-utils";
-import { baseTokenURI, imageUrl, ns, testChainId } from "@framework/constants";
+import { imageUrl, ns, testChainId } from "@framework/constants";
 import { NodeEnv } from "@framework/types";
 
 export class SeedContractPaymentSplitterAt1697876719370 implements MigrationInterface {
