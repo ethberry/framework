af5ce9e7c4afba299f6ff0d9e8a09c04a6a626a9
TrejGun <trejgun@gmail.com>
2024-03-30 07:30:43 +0800

    chore: remove leftovers

--- packages/constants/src/company.ts
+++ packages/constants/src/company.ts
@@ -1,6 +1,5 @@
 export const companyName = "Gemunion Framework";
 export const ns = "gemunion";
 
-export const availableChains = [1, 5, 56, 97, 137, 80001, 42161, 10001, 10000, 11155111];
 export const testChainId = 10001n;
 export const defaultChainId = 56n;
