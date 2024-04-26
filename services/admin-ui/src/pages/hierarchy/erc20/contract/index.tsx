6ade9963c9efa248c435dad7f4190c6a6d1f156a
Karafizi Arthur <karafizi.artur@gmail.com>
2024-04-27 07:23:34 +0800

    fix(admin): hide create erc20 contract button for b2b

--- services/admin-ui/src/pages/hierarchy/erc20/contract/index.tsx
+++ services/admin-ui/src/pages/hierarchy/erc20/contract/index.tsx
@@ -9,8 +9,14 @@ import { DeleteDialog } from "@gemunion/mui-dialog-delete";
 import { useCollection } from "@gemunion/react-hooks";
 import { emptyStateString } from "@gemunion/draft-js-utils";
 import { ListAction, ListActions, StyledListItem, StyledListWrapper, StyledPagination } from "@framework/styled";
-import type { IContract, IContractSearchDto, ITemplate } from "@framework/types";
-import { ContractStatus, Erc20ContractFeatures } from "@framework/types";
+import {
+  BusinessType,
+  IContract,
+  IContractSearchDto,
+  ITemplate,
+  ContractStatus,
+  Erc20ContractFeatures,
+} from "@framework/types";
 
 import { Erc20ContractDeployButton } from "../../../../components/buttons";
 import { ContractSearchForm } from "../../../../components/forms/contract-search";
@@ -93,9 +99,13 @@ export const Erc20Contract: FC = () => {
         <Button startIcon={<FilterList />} onClick={handleToggleFilters} data-testid="ToggleFilterButton">
           <FormattedMessage id={`form.buttons.${isFiltersOpen ? "hideFilters" : "showFilters"}`} />
         </Button>
-        <Button variant="outlined" startIcon={<Add />} onClick={handleCreate} data-testid="Erc20TokenCreateButton">
-          <FormattedMessage id="form.buttons.create" />
-        </Button>
+        {process.env.BUSINESS_TYPE === BusinessType.B2B ? (
+          <></>
+        ) : (
+          <Button variant="outlined" startIcon={<Add />} onClick={handleCreate} data-testid="Erc20TokenCreateButton">
+            <FormattedMessage id="form.buttons.create" />
+          </Button>
+        )}
         <Erc20ContractDeployButton />
       </PageHeader>
 
