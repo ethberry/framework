af2f92ab3693f9c94b9ce052cdfbec645e0e9560
JenyaFomin <jenya.stasik@gmail.com>
2024-05-06 12:15:19 -0400

    feat(market-ui): save referrer in localStorage

--- services/market-ui/src/components/common/layout/index.tsx
+++ services/market-ui/src/components/common/layout/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 
 import { Header } from "../header";
@@ -6,6 +6,24 @@ import ErrorBoundary from "../error-boundary";
 import { Root, StyledContainer } from "./styled";
 
 export const Layout: FC = () => {
+  useEffect(() => {
+    // Get the current URL
+    const url = window.location.href;
+    const searchParams = url.includes("?") ? url.split("?")[1] : null;
+
+    if (searchParams) {
+      const queryParams = new URLSearchParams(searchParams);
+      // Check if a refferer query parameter exists
+      if (queryParams.has("referrer")) {
+        // Get the value of the refferer
+        const paramValue = queryParams.get("referrer");
+        // Save in LocalStorage
+        localStorage.setItem("refferer", paramValue!);
+        // console.log("Refferer:", paramValue);
+      }
+    }
+  }, []);
+
   return (
     <Root>
       <Header />
