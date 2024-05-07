3ab227f95c20004dd5c221861b29babbbfcbd62c
JenyaFomin <jenya.stasik@gmail.com>
2024-05-07 00:52:17 -0400

    fix(market-ui): remove erroneous code

--- services/market-ui/src/components/common/layout/index.tsx
+++ services/market-ui/src/components/common/layout/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect } from "react";
+import { FC } from "react";
 import { Outlet } from "react-router-dom";
 
 import { Header } from "../header";
@@ -6,24 +6,6 @@ import ErrorBoundary from "../error-boundary";
 import { Root, StyledContainer } from "./styled";
 
 export const Layout: FC = () => {
-  useEffect(() => {
-    // Get the current URL
-    const url = window.location.href;
-    const searchParams = url.includes("?") ? url.split("?")[1] : null;
-
-    if (searchParams) {
-      const queryParams = new URLSearchParams(searchParams);
-      // Check if a refferer query parameter exists
-      if (queryParams.has("referrer")) {
-        // Get the value of the refferer
-        const paramValue = queryParams.get("referrer");
-        // Save in LocalStorage
-        localStorage.setItem("refferer", paramValue!);
-        // console.log("Refferer:", paramValue);
-      }
-    }
-  }, []);
-
   return (
     <Root>
       <Header />
