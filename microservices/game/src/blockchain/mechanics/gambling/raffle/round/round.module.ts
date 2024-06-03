b8c9f63da24ec791f64379b8a465bf5096ac016a
Ubudragon <gemuniongroup@gmail.com>
2024-06-03 18:42:44 +0800

    fix: game ms import

--- microservices/game/src/blockchain/mechanics/gambling/raffle/round/round.module.ts
+++ microservices/game/src/blockchain/mechanics/gambling/raffle/round/round.module.ts
@@ -5,10 +5,10 @@ import { RaffleRoundService } from "./round.service";
 import { RaffleRoundEntity } from "./round.entity";
 import { RaffleRoundController } from "./round.controller";
 import { ContractModule } from "../../../../hierarchy/contract/contract.module";
-import { RaffleTokenModule } from "../ticket/token/token.module";
+import { RaffleTicketTokenModule } from "../ticket/token/token.module";
 
 @Module({
-  imports: [ContractModule, RaffleTokenModule, TypeOrmModule.forFeature([RaffleRoundEntity])],
+  imports: [ContractModule, RaffleTicketTokenModule, TypeOrmModule.forFeature([RaffleRoundEntity])],
   providers: [RaffleRoundService],
   controllers: [RaffleRoundController],
   exports: [RaffleRoundService],
