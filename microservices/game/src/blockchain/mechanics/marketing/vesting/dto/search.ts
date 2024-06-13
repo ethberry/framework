9acc2e78143e7cd4a57d48820cecdb81f13cb257
Ubudragon <gemuniongroup@gmail.com>
2024-06-13 13:33:47 +0800

    fix: dto

--- microservices/game/src/blockchain/mechanics/marketing/vesting/dto/search.ts
+++ microservices/game/src/blockchain/mechanics/marketing/vesting/dto/search.ts
@@ -2,8 +2,10 @@ import { Mixin } from "ts-mixer";
 
 import { PaginationDto } from "@gemunion/collection";
 import { AccountDto } from "@gemunion/nest-js-validators";
-import { IVestingSearchDto } from "@framework/types";
+import { IVestingSearchDto, VestingContractFeatures } from "@framework/types";
 
 export class VestingSearchDto extends Mixin(AccountDto, PaginationDto) implements IVestingSearchDto {
+  public contractFeatures: Array<VestingContractFeatures>;
+  public account: string;
   public merchantId: number;
 }
