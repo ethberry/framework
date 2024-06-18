bf619a06d09a4d19ccb73981d973946bb13951ab
TrejGun <trejgun@gmail.com>
2024-06-18 11:12:30 +0800

    test: ChainLinkV2PlusSupportedChains

--- packages/types/src/entities/integrations/chain-link/supported.ts
+++ packages/types/src/entities/integrations/chain-link/supported.ts
@@ -1,23 +1,13 @@
-// https://docs.chain.link/resources/link-token-contracts
-
+// https://docs.chain.link/vrf/v2/subscription/supported-networks
 export enum ChainLinkV2SupportedChains {
-  // BESU_LOCALHOST = 10001,
-  // BESU_GEMUNION = 10000,
-  //
-  // TELOS_TEST = 41, // gemunion's vrf v2
-
   ETHEREUM = 1,
-  // ETHEREUM_GOERLI = 5,
   ETHEREUM_SEPOLIA = 11155111,
 
   BINANCE = 56,
   BINANCE_TEST = 97,
 
   POLYGON = 137,
-  // POLYGON_MUMBAI = 80001,
   POLYGON_AMOY = 80002,
-
-  // GNOSIS = 100,
 
   AVALANCHE = 43114,
   AVALANCHE_FUJI = 43113,
@@ -27,41 +17,22 @@ export enum ChainLinkV2SupportedChains {
 
   ARBITRUM = 42161,
   ARBITRUM_SEPOLIA = 421614,
-  // ARBITRUM_GOERLI = 421613, removed
+}
 
-  // OPTIMISM = 10,
-  // OPTIMISM_SEPOLIA = 11155420,
-  // OPTIMISM_GOERLI = 420, removed
+// https://docs.chain.link/vrf/v2-5/supported-networks
+export enum ChainLinkV2PlusSupportedChains {
+  ETHEREUM = 1,
+  ETHEREUM_SEPOLIA = 11155111,
 
-  // MOONRIVER = 1285,
+  BINANCE = 56,
+  BINANCE_TEST = 97,
 
-  // MOONBEAM = 1284,
+  POLYGON = 137,
+  POLYGON_AMOY = 80002,
 
-  // METIS = 1088, // Andromeda
+  AVALANCHE = 43114,
+  AVALANCHE_FUJI = 43113,
 
-  // BASE = 8453,
-  // BASE_SEPOLIA = 84532,
-  // BASE_GOERLI = 84531, removed
-
-  // CELO = 42220,
-  // CELO_ALFAJORES = 44787,
-
-  // SCROLL = 534352,
-  // SCROLL_SEPOLIA = 534351,
-
-  // LINEA = 59144,
-
-  // ZKSYNC_ERA = 324,
-  // ZKSYNC_GOERLI = 280,
-
-  // POLYGON_ZKEVM = 1101,
-  // POLYGON_ZKEVM_TEST = 1442,
-
-  // WEMIX = 1111,
-  // WEMIX_TEST = 1112,
-
-  // KROMA = 255,
-  // KROMA_SEPOLIA = 2358,
-
-  // SOLANA = null, not EVM
+  ARBITRUM = 42161,
+  ARBITRUM_SEPOLIA = 421614,
 }
