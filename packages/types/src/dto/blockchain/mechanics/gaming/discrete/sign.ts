25a2ae89ba8f7c565b57b781149e1044366ede58
JenyaFomin <jenya.stasik@gmail.com>
2024-06-24 16:40:17 -0400

    feat(market): 'discreate' simple-sign

--- packages/types/src/dto/blockchain/mechanics/gaming/discrete/sign.ts
+++ packages/types/src/dto/blockchain/mechanics/gaming/discrete/sign.ts
@@ -1,7 +1,7 @@
 export interface IDiscreteSignDto {
-  chainId: number;
-  account: string;
   referrer: string;
   tokenId: number;
   attribute: string;
+  chainId?: number;
+  account?: string;
 }
