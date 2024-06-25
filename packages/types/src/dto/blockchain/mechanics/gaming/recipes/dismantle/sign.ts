17849919a92e6e10ff058b548423596a74f58c76
JenyaFomin <jenya.stasik@gmail.com>
2024-06-24 21:34:04 -0400

    feat(market): 'dismantle' simple-sign

--- packages/types/src/dto/blockchain/mechanics/gaming/recipes/dismantle/sign.ts
+++ packages/types/src/dto/blockchain/mechanics/gaming/recipes/dismantle/sign.ts
@@ -1,7 +1,7 @@
 export interface IDismantleSignDto {
-  chainId: number;
-  account: string;
   referrer: string;
   dismantleId: number;
   tokenId: number;
+  chainId?: number;
+  account?: string;
 }
--- services/market-api/src/blockchain/mechanics/gaming/recipes/dismantle/dto/sign.ts
+++ services/market-api/src/blockchain/mechanics/gaming/recipes/dismantle/dto/sign.ts
@@ -2,10 +2,10 @@ import { ApiProperty } from "@nestjs/swagger";
 import { IsInt, Min } from "class-validator";
 import { Mixin } from "ts-mixer";
 
-import { AccountDto, ReferrerOptionalDto, ChainIdDto } from "@gemunion/nest-js-validators";
+import { ReferrerOptionalDto } from "@gemunion/nest-js-validators";
 import type { IDismantleSignDto } from "@framework/types";
 
-export class DismantleSignDto extends Mixin(AccountDto, ReferrerOptionalDto, ChainIdDto) implements IDismantleSignDto {
+export class DismantleSignDto extends Mixin(ReferrerOptionalDto) implements IDismantleSignDto {
   @ApiProperty({
     minimum: 1,
   })
