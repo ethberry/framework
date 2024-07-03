a68ff4f6f292dd6779175ba1eb18b964ed8b59d7
Ubudragon <gemuniongroup@gmail.com>
2024-07-03 23:31:01 +0800

    fix: typo

--- services/admin-ui/src/pages/hierarchy/erc20/contract/edit/index.tsx
+++ services/admin-ui/src/pages/hierarchy/erc20/contract/edit/index.tsx
@@ -71,7 +71,6 @@ export const Erc20ContractEditDialog: FC<IErc20TokenEditDialogProps> = props => 
             name={name}
             symbol={symbol}
             address={address}
-            dock
             decimals={decimals}
             cap={formatEther(template.cap, decimals, "")}
             chainId={chainId}
