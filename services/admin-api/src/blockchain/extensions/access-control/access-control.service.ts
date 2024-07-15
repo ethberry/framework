999f4751e5ba895280b62f0dbf346a8ec263d457
Ubudragon <gemuniongroup@gmail.com>
2024-07-15 18:46:50 +0800

    fix: AccessControlService query

--- services/admin-api/src/blockchain/extensions/access-control/access-control.service.ts
+++ services/admin-api/src/blockchain/extensions/access-control/access-control.service.ts
@@ -6,6 +6,7 @@ import { UserEntity } from "../../../infrastructure/user/user.entity";
 import { ContractEntity } from "../../hierarchy/contract/contract.entity";
 import { AccessControlEntity } from "./access-control.entity";
 import type { IAccessControlCheckDto, IAccessControlSearchDto } from "./interfaces";
+import { LootBoxEntity } from "../../mechanics/marketing/loot/box/box.entity";
 
 @Injectable()
 export class AccessControlService {
@@ -15,15 +16,32 @@ export class AccessControlService {
   ) {}
 
   public async search(dto: IAccessControlSearchDto, userEntity: UserEntity): Promise<Array<AccessControlEntity>> {
+    const { address, account } = dto;
     const queryBuilder = this.accessControlEntityRepository.createQueryBuilder("roles");
 
     queryBuilder.select();
 
-    queryBuilder.where(dto);
+    queryBuilder.where({ address: address.toLowerCase(), account: account.toLowerCase() });
 
-    queryBuilder.leftJoin("roles.address_contract", "address_contract", "address_contract.chain_id = :chainId", {
-      chainId: userEntity.chainId,
-    });
+    // queryBuilder.leftJoin("roles.address_contract", "address_contract", "address_contract.chain_id = :chainId", {
+    //   chainId: userEntity.chainId,
+    // });
+
+    queryBuilder.leftJoinAndMapOne(
+      "roles.address_contract",
+      ContractEntity,
+      "address_contract",
+      `roles.address = address_contract.address AND address_contract.chain_id = :chainId`,
+      { chainId: userEntity.chainId },
+    );
+
+    queryBuilder.leftJoinAndMapOne(
+      "roles.account_contract",
+      ContractEntity,
+      "account_contract",
+      `roles.address = account_contract.address AND account_contract.chain_id = :chainId`,
+      { chainId: userEntity.chainId },
+    );
 
     queryBuilder.orderBy("roles.createdAt", "DESC");
 
