240508a4d8324ac13d0cebd8f6b4fe1002a02639
pavelnikolaichyk <nikolaj4ik.pasha@yandex.ru>
2024-07-17 01:40:59 +0300

    BF-1 feat: added logic to vesting contracts

--- services/admin-ui/src/pages/mechanics/marketing/vesting/contract/index.tsx
+++ services/admin-ui/src/pages/mechanics/marketing/vesting/contract/index.tsx
@@ -1,15 +1,23 @@
 import { FC } from "react";
 import { Grid, ListItemText } from "@mui/material";
 import { Visibility } from "@mui/icons-material";
+import { useWeb3React } from "@web3-react/core";
 
 import { Breadcrumbs, PageHeader, ProgressOverlay } from "@gemunion/mui-page-layout";
 import { SelectInput } from "@gemunion/mui-inputs-core";
 import { CollectionActions, useCollection } from "@gemunion/react-hooks";
 import { AddressLink } from "@gemunion/mui-scanner";
 import { CommonSearchForm } from "@gemunion/mui-form-search";
-import { ListAction, ListActions, StyledListItem, StyledListWrapper, StyledPagination } from "@framework/styled";
+import {
+  ListAction,
+  ListActions,
+  ListWrapperProvider,
+  StyledListItem,
+  StyledListWrapper,
+  StyledPagination,
+} from "@framework/styled";
 import type { IContract, IVestingSearchDto } from "@framework/types";
-import { TokenType, VestingContractFeatures } from "@framework/types";
+import { IAccessControl, TokenType, VestingContractFeatures } from "@framework/types";
 
 import { emptyVestingContract } from "../../../../../components/common/interfaces";
 import {
@@ -19,6 +27,7 @@ import {
   VestingDeployButton,
 } from "../../../../../components/buttons";
 import { VestingViewDialog } from "./view";
+import { useCheckPermissions } from "../../../../../shared";
 
 export const VestingContracts: FC = () => {
   const {
@@ -42,56 +51,61 @@ export const VestingContracts: FC = () => {
     empty: emptyVestingContract,
   });
 
+  const { checkPermissions } = useCheckPermissions();
+  const { account = "" } = useWeb3React();
+
   return (
-    <Grid>
-      <Breadcrumbs path={["dashboard", "vesting", "vesting.contracts"]} />
+    <ListWrapperProvider<IAccessControl> callback={checkPermissions}>
+      <Grid>
+        <Breadcrumbs path={["dashboard", "vesting", "vesting.contracts"]} />
 
-      <PageHeader message="pages.vesting.contracts.title">
-        <VestingDeployButton />
-      </PageHeader>
+        <PageHeader message="pages.vesting.contracts.title">
+          <VestingDeployButton />
+        </PageHeader>
 
-      <CommonSearchForm onSubmit={handleSearch} initialValues={search} name="account">
-        <Grid container spacing={2} alignItems="flex-end">
-          <Grid item xs={12}>
-            <SelectInput name="contractFeatures" options={VestingContractFeatures} multiple />
+        <CommonSearchForm onSubmit={handleSearch} initialValues={search} name="account">
+          <Grid container spacing={2} alignItems="flex-end">
+            <Grid item xs={12}>
+              <SelectInput name="contractFeatures" options={VestingContractFeatures} multiple />
+            </Grid>
           </Grid>
-        </Grid>
-      </CommonSearchForm>
+        </CommonSearchForm>
 
-      <ProgressOverlay isLoading={isLoading}>
-        <StyledListWrapper count={rows.length} isLoading={isLoading}>
-          {rows.map(vesting => (
-            <StyledListItem key={vesting.id} wrap>
-              <ListItemText sx={{ mr: 0.5, overflowX: "auto", width: 0.5 }}>
-                <AddressLink address={vesting.parameters.account as string} />
-              </ListItemText>
-              <ListActions dataTestId="VestingActionsMenuButton">
-                <ListAction onClick={handleView(vesting)} message="form.tips.view" icon={Visibility} />
-                <AllowanceButton
-                  contract={vesting}
-                  disabledTokenTypes={[TokenType.ERC721, TokenType.ERC998, TokenType.ERC1155]}
-                />
-                <TopUpButton contract={vesting} />
-                <TransferOwnershipButton contract={vesting} />
-              </ListActions>
-            </StyledListItem>
-          ))}
-        </StyledListWrapper>
-      </ProgressOverlay>
+        <ProgressOverlay isLoading={isLoading}>
+          <StyledListWrapper count={rows.length} isLoading={isLoading} rows={rows} account={account} path={"address"}>
+            {rows.map(vesting => (
+              <StyledListItem key={vesting.id} wrap>
+                <ListItemText sx={{ mr: 0.5, overflowX: "auto", width: 0.5 }}>
+                  <AddressLink address={vesting.parameters.account as string} />
+                </ListItemText>
+                <ListActions dataTestId="VestingActionsMenuButton">
+                  <ListAction onClick={handleView(vesting)} message="form.tips.view" icon={Visibility} />
+                  <AllowanceButton
+                    contract={vesting}
+                    disabledTokenTypes={[TokenType.ERC721, TokenType.ERC998, TokenType.ERC1155]}
+                  />
+                  <TopUpButton contract={vesting} />
+                  <TransferOwnershipButton contract={vesting} />
+                </ListActions>
+              </StyledListItem>
+            ))}
+          </StyledListWrapper>
+        </ProgressOverlay>
 
-      <StyledPagination
-        shape="rounded"
-        page={search.skip / search.take + 1}
-        count={Math.ceil(count / search.take)}
-        onChange={handleChangePage}
-      />
+        <StyledPagination
+          shape="rounded"
+          page={search.skip / search.take + 1}
+          count={Math.ceil(count / search.take)}
+          onChange={handleChangePage}
+        />
 
-      <VestingViewDialog
-        onCancel={handleViewCancel}
-        onConfirm={handleViewConfirm}
-        open={action === CollectionActions.view}
-        initialValues={selected}
-      />
-    </Grid>
+        <VestingViewDialog
+          onCancel={handleViewCancel}
+          onConfirm={handleViewConfirm}
+          open={action === CollectionActions.view}
+          initialValues={selected}
+        />
+      </Grid>
+    </ListWrapperProvider>
   );
 };
