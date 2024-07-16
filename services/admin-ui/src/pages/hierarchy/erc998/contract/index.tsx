51186ef3af19cd0da7007ffc28dccd76744a1767
pavelnikolaichyk <nikolaj4ik.pasha@yandex.ru>
2024-07-17 00:21:01 +0300

    BF-1 feat: added logic to erc998 contracts and templates

--- services/admin-ui/src/pages/hierarchy/erc998/contract/index.tsx
+++ services/admin-ui/src/pages/hierarchy/erc998/contract/index.tsx
@@ -2,13 +2,21 @@ import { FC } from "react";
 import { FormattedMessage } from "react-intl";
 import { Button, Grid, ListItemText } from "@mui/material";
 import { Create, Delete, FilterList } from "@mui/icons-material";
+import { useWeb3React } from "@web3-react/core";
 
 import { Breadcrumbs, PageHeader, ProgressOverlay } from "@gemunion/mui-page-layout";
 import { DeleteDialog } from "@gemunion/mui-dialog-delete";
 import { CollectionActions, useCollection } from "@gemunion/react-hooks";
-import { ListAction, ListActions, StyledListItem, StyledListWrapper, StyledPagination } from "@framework/styled";
+import {
+  ListAction,
+  ListActions,
+  ListWrapperProvider,
+  StyledListItem,
+  StyledListWrapper,
+  StyledPagination,
+} from "@framework/styled";
 import type { IContract, IContractSearchDto } from "@framework/types";
-import { ContractStatus, Erc998ContractFeatures } from "@framework/types";
+import { ContractStatus, Erc998ContractFeatures, IAccessControl } from "@framework/types";
 
 import {
   BlacklistButton,
@@ -30,6 +38,7 @@ import {
 } from "../../../../components/buttons";
 import { ContractSearchForm } from "../../../../components/forms/contract-search";
 import { Erc998ContractEditDialog } from "./edit";
+import { useCheckPermissions } from "../../../../shared";
 
 export const Erc998Contract: FC = () => {
   const {
@@ -68,85 +77,90 @@ export const Erc998Contract: FC = () => {
     }),
   });
 
+  const { checkPermissions } = useCheckPermissions();
+  const { account = "" } = useWeb3React();
+
   return (
-    <Grid>
-      <Breadcrumbs path={["dashboard", "erc998", "erc998.contracts"]} />
+    <ListWrapperProvider<IAccessControl> callback={checkPermissions}>
+      <Grid>
+        <Breadcrumbs path={["dashboard", "erc998", "erc998.contracts"]} />
 
-      <PageHeader message="pages.erc998.contracts.title">
-        <Button startIcon={<FilterList />} onClick={handleToggleFilters} data-testid="ToggleFilterButton">
-          <FormattedMessage id={`form.buttons.${isFiltersOpen ? "hideFilters" : "showFilters"}`} />
-        </Button>
-        <Erc998ContractDeployButton />
-      </PageHeader>
+        <PageHeader message="pages.erc998.contracts.title">
+          <Button startIcon={<FilterList />} onClick={handleToggleFilters} data-testid="ToggleFilterButton">
+            <FormattedMessage id={`form.buttons.${isFiltersOpen ? "hideFilters" : "showFilters"}`} />
+          </Button>
+          <Erc998ContractDeployButton />
+        </PageHeader>
 
-      <ContractSearchForm
-        onSubmit={handleSearch}
-        initialValues={search}
-        open={isFiltersOpen}
-        contractFeaturesOptions={Erc998ContractFeatures}
-        onRefreshPage={handleRefreshPage}
-      />
+        <ContractSearchForm
+          onSubmit={handleSearch}
+          initialValues={search}
+          open={isFiltersOpen}
+          contractFeaturesOptions={Erc998ContractFeatures}
+          onRefreshPage={handleRefreshPage}
+        />
 
-      <ProgressOverlay isLoading={isLoading}>
-        <StyledListWrapper count={rows.length} isLoading={isLoading}>
-          {rows.map(contract => (
-            <StyledListItem key={contract.id}>
-              <ListItemText>{contract.title}</ListItemText>
-              <ListActions dataTestId="ContractActionsMenuButton">
-                <ListAction
-                  onClick={handleEdit(contract)}
-                  message="form.buttons.edit"
-                  dataTestId="ContractEditButton"
-                  icon={Create}
-                />
-                <ListAction
-                  onClick={handleDelete(contract)}
-                  message="form.buttons.delete"
-                  dataTestId="ContractDeleteButton"
-                  icon={Delete}
-                  disabled={contract.contractStatus === ContractStatus.INACTIVE}
-                />
-                <GrantRoleButton contract={contract} />
-                <RevokeRoleButton contract={contract} />
-                <RenounceRoleButton contract={contract} />
-                <BlacklistButton contract={contract} />
-                <UnBlacklistButton contract={contract} />
-                <WhitelistButton contract={contract} />
-                <UnWhitelistButton contract={contract} />
-                <ContractMintButton contract={contract} />
-                <ContractAllowanceButton contract={contract} />
-                <TransferButton contract={contract} />
-                <RoyaltyButton contract={contract} />
-                <SetBaseTokenURIButton contract={contract} />
-                <ChainLinkSetSubscriptionButton contract={contract} />
-                <EthListenerAddButton contract={contract} />
-                <EthListenerRemoveButton contract={contract} />
-              </ListActions>
-            </StyledListItem>
-          ))}
-        </StyledListWrapper>
-      </ProgressOverlay>
+        <ProgressOverlay isLoading={isLoading}>
+          <StyledListWrapper count={rows.length} isLoading={isLoading} rows={rows} account={account} path={"address"}>
+            {rows.map(contract => (
+              <StyledListItem key={contract.id}>
+                <ListItemText>{contract.title}</ListItemText>
+                <ListActions dataTestId="ContractActionsMenuButton">
+                  <ListAction
+                    onClick={handleEdit(contract)}
+                    message="form.buttons.edit"
+                    dataTestId="ContractEditButton"
+                    icon={Create}
+                  />
+                  <ListAction
+                    onClick={handleDelete(contract)}
+                    message="form.buttons.delete"
+                    dataTestId="ContractDeleteButton"
+                    icon={Delete}
+                    disabled={contract.contractStatus === ContractStatus.INACTIVE}
+                  />
+                  <GrantRoleButton contract={contract} />
+                  <RevokeRoleButton contract={contract} />
+                  <RenounceRoleButton contract={contract} />
+                  <BlacklistButton contract={contract} />
+                  <UnBlacklistButton contract={contract} />
+                  <WhitelistButton contract={contract} />
+                  <UnWhitelistButton contract={contract} />
+                  <ContractMintButton contract={contract} />
+                  <ContractAllowanceButton contract={contract} />
+                  <TransferButton contract={contract} />
+                  <RoyaltyButton contract={contract} />
+                  <SetBaseTokenURIButton contract={contract} />
+                  <ChainLinkSetSubscriptionButton contract={contract} />
+                  <EthListenerAddButton contract={contract} />
+                  <EthListenerRemoveButton contract={contract} />
+                </ListActions>
+              </StyledListItem>
+            ))}
+          </StyledListWrapper>
+        </ProgressOverlay>
 
-      <StyledPagination
-        shape="rounded"
-        page={search.skip / search.take + 1}
-        count={Math.ceil(count / search.take)}
-        onChange={handleChangePage}
-      />
+        <StyledPagination
+          shape="rounded"
+          page={search.skip / search.take + 1}
+          count={Math.ceil(count / search.take)}
+          onChange={handleChangePage}
+        />
 
-      <DeleteDialog
-        onCancel={handleDeleteCancel}
-        onConfirm={handleDeleteConfirm}
-        open={action === CollectionActions.delete}
-        initialValues={selected}
-      />
+        <DeleteDialog
+          onCancel={handleDeleteCancel}
+          onConfirm={handleDeleteConfirm}
+          open={action === CollectionActions.delete}
+          initialValues={selected}
+        />
 
-      <Erc998ContractEditDialog
-        onCancel={handleEditCancel}
-        onConfirm={handleEditConfirm}
-        open={action === CollectionActions.edit}
-        initialValues={selected}
-      />
-    </Grid>
+        <Erc998ContractEditDialog
+          onCancel={handleEditCancel}
+          onConfirm={handleEditConfirm}
+          open={action === CollectionActions.edit}
+          initialValues={selected}
+        />
+      </Grid>
+    </ListWrapperProvider>
   );
 };
