791d0e48c87b80a7f1af920a36181e729bfe4e5a
pavelnikolaichyk <nikolaj4ik.pasha@yandex.ru>
2024-07-17 01:19:09 +0300

    BF-1 feat: added logic to collections contracts and templates

--- services/admin-ui/src/pages/mechanics/marketing/collection/contract/index.tsx
+++ services/admin-ui/src/pages/mechanics/marketing/collection/contract/index.tsx
@@ -2,13 +2,27 @@ import { FC } from "react";
 import { FormattedMessage } from "react-intl";
 import { Button, Grid, ListItemText } from "@mui/material";
 import { Create, Delete, FilterList } from "@mui/icons-material";
+import { useWeb3React } from "@web3-react/core";
 
 import { Breadcrumbs, PageHeader, ProgressOverlay } from "@gemunion/mui-page-layout";
 import { DeleteDialog } from "@gemunion/mui-dialog-delete";
 import { CollectionActions, useCollection } from "@gemunion/react-hooks";
 import { emptyStateString } from "@gemunion/draft-js-utils";
-import { ListAction, ListActions, StyledListItem, StyledListWrapper, StyledPagination } from "@framework/styled";
-import { CollectionContractFeatures, ContractStatus, IContract, IContractSearchDto } from "@framework/types";
+import {
+  ListAction,
+  ListActions,
+  ListWrapperProvider,
+  StyledListItem,
+  StyledListWrapper,
+  StyledPagination,
+} from "@framework/styled";
+import {
+  CollectionContractFeatures,
+  ContractStatus,
+  IAccessControl,
+  IContract,
+  IContractSearchDto,
+} from "@framework/types";
 
 import {
   BlacklistButton,
@@ -27,6 +41,7 @@ import {
 } from "../../../../../components/buttons";
 import { ContractSearchForm } from "../../../../../components/forms/contract-search";
 import { Erc721CollectionEditDialog } from "./edit";
+import { useCheckPermissions } from "../../../../../shared";
 
 export const CollectionContract: FC = () => {
   const {
@@ -71,83 +86,88 @@ export const CollectionContract: FC = () => {
     }),
   });
 
+  const { checkPermissions } = useCheckPermissions();
+  const { account = "" } = useWeb3React();
+
   return (
-    <Grid>
-      <Breadcrumbs path={["dashboard", "collection", "collection.contract"]} />
+    <ListWrapperProvider<IAccessControl> callback={checkPermissions}>
+      <Grid>
+        <Breadcrumbs path={["dashboard", "collection", "collection.contract"]} />
 
-      <PageHeader message="pages.collection.contracts">
-        <Button startIcon={<FilterList />} onClick={handleToggleFilters} data-testid="ToggleFilterButton">
-          <FormattedMessage id={`form.buttons.${isFiltersOpen ? "hideFilters" : "showFilters"}`} />
-        </Button>
-        <CollectionContractDeployButton />
-      </PageHeader>
+        <PageHeader message="pages.collection.contracts">
+          <Button startIcon={<FilterList />} onClick={handleToggleFilters} data-testid="ToggleFilterButton">
+            <FormattedMessage id={`form.buttons.${isFiltersOpen ? "hideFilters" : "showFilters"}`} />
+          </Button>
+          <CollectionContractDeployButton />
+        </PageHeader>
 
-      <ContractSearchForm
-        onSubmit={handleSearch}
-        initialValues={search}
-        open={isFiltersOpen}
-        contractFeaturesOptions={CollectionContractFeatures}
-        onRefreshPage={handleRefreshPage}
-      />
+        <ContractSearchForm
+          onSubmit={handleSearch}
+          initialValues={search}
+          open={isFiltersOpen}
+          contractFeaturesOptions={CollectionContractFeatures}
+          onRefreshPage={handleRefreshPage}
+        />
 
-      <ProgressOverlay isLoading={isLoading}>
-        <StyledListWrapper count={rows.length} isLoading={isLoading}>
-          {rows.map(contract => (
-            <StyledListItem key={contract.id}>
-              <ListItemText sx={{ width: 0.6 }}>{contract.title}</ListItemText>
-              <ListItemText>{contract.parameters.batchSize}</ListItemText>
-              <ListActions dataTestId="CollectionActionsMenuButton">
-                <ListAction
-                  onClick={handleEdit(contract)}
-                  message="form.buttons.edit"
-                  dataTestId="ContractEditButton"
-                  icon={Create}
-                />
-                <ListAction
-                  onClick={handleDelete(contract)}
-                  message="form.buttons.delete"
-                  dataTestId="ContractDeleteButton"
-                  icon={Delete}
-                  disabled={contract.contractStatus === ContractStatus.INACTIVE}
-                />
-                <GrantRoleButton contract={contract} />
-                <RevokeRoleButton contract={contract} />
-                <RenounceRoleButton contract={contract} />
-                <ContractAllowanceButton contract={contract} />
-                <RoyaltyButton contract={contract} />
-                <SetBaseTokenURIButton contract={contract} />
-                <BlacklistButton contract={contract} />
-                <UnBlacklistButton contract={contract} />
-                <TransferButton contract={contract} />
-                <CollectionUploadButton contract={contract} onRefreshPage={handleRefreshPage} />
-                <EthListenerAddButton contract={contract} />
-                <EthListenerRemoveButton contract={contract} />
-              </ListActions>
-            </StyledListItem>
-          ))}
-        </StyledListWrapper>
-      </ProgressOverlay>
+        <ProgressOverlay isLoading={isLoading}>
+          <StyledListWrapper count={rows.length} isLoading={isLoading} rows={rows} account={account} path={"address"}>
+            {rows.map(contract => (
+              <StyledListItem key={contract.id}>
+                <ListItemText sx={{ width: 0.6 }}>{contract.title}</ListItemText>
+                <ListItemText>{contract.parameters.batchSize}</ListItemText>
+                <ListActions dataTestId="CollectionActionsMenuButton">
+                  <ListAction
+                    onClick={handleEdit(contract)}
+                    message="form.buttons.edit"
+                    dataTestId="ContractEditButton"
+                    icon={Create}
+                  />
+                  <ListAction
+                    onClick={handleDelete(contract)}
+                    message="form.buttons.delete"
+                    dataTestId="ContractDeleteButton"
+                    icon={Delete}
+                    disabled={contract.contractStatus === ContractStatus.INACTIVE}
+                  />
+                  <GrantRoleButton contract={contract} />
+                  <RevokeRoleButton contract={contract} />
+                  <RenounceRoleButton contract={contract} />
+                  <ContractAllowanceButton contract={contract} />
+                  <RoyaltyButton contract={contract} />
+                  <SetBaseTokenURIButton contract={contract} />
+                  <BlacklistButton contract={contract} />
+                  <UnBlacklistButton contract={contract} />
+                  <TransferButton contract={contract} />
+                  <CollectionUploadButton contract={contract} onRefreshPage={handleRefreshPage} />
+                  <EthListenerAddButton contract={contract} />
+                  <EthListenerRemoveButton contract={contract} />
+                </ListActions>
+              </StyledListItem>
+            ))}
+          </StyledListWrapper>
+        </ProgressOverlay>
 
-      <StyledPagination
-        shape="rounded"
-        page={search.skip / search.take + 1}
-        count={Math.ceil(count / search.take)}
-        onChange={handleChangePage}
-      />
+        <StyledPagination
+          shape="rounded"
+          page={search.skip / search.take + 1}
+          count={Math.ceil(count / search.take)}
+          onChange={handleChangePage}
+        />
 
-      <DeleteDialog
-        onCancel={handleDeleteCancel}
-        onConfirm={handleDeleteConfirm}
-        open={action === CollectionActions.delete}
-        initialValues={selected}
-      />
+        <DeleteDialog
+          onCancel={handleDeleteCancel}
+          onConfirm={handleDeleteConfirm}
+          open={action === CollectionActions.delete}
+          initialValues={selected}
+        />
 
-      <Erc721CollectionEditDialog
-        onCancel={handleEditCancel}
-        onConfirm={handleEditConfirm}
-        open={action === CollectionActions.edit}
-        initialValues={selected}
-      />
-    </Grid>
+        <Erc721CollectionEditDialog
+          onCancel={handleEditCancel}
+          onConfirm={handleEditConfirm}
+          open={action === CollectionActions.edit}
+          initialValues={selected}
+        />
+      </Grid>
+    </ListWrapperProvider>
   );
 };
--- services/admin-ui/src/pages/mechanics/marketing/collection/template/index.tsx
+++ services/admin-ui/src/pages/mechanics/marketing/collection/template/index.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import { FormattedMessage } from "react-intl";
 import { Button, Grid, ListItemText } from "@mui/material";
 import { Add, Create, Delete, FilterList } from "@mui/icons-material";
+import { useWeb3React } from "@web3-react/core";
 
 import { Breadcrumbs, PageHeader, ProgressOverlay } from "@gemunion/mui-page-layout";
 import { DeleteDialog } from "@gemunion/mui-dialog-delete";
@@ -9,13 +10,21 @@ import { useCollection, CollectionActions } from "@gemunion/react-hooks";
 import { emptyStateString } from "@gemunion/draft-js-utils";
 import { emptyPrice } from "@gemunion/mui-inputs-asset";
 import { cleanUpAsset } from "@framework/exchange";
-import { ListAction, ListActions, StyledListItem, StyledListWrapper, StyledPagination } from "@framework/styled";
+import {
+  ListAction,
+  ListActions,
+  ListWrapperProvider,
+  StyledListItem,
+  StyledListWrapper,
+  StyledPagination,
+} from "@framework/styled";
 import type { ITemplate, ITemplateSearchDto } from "@framework/types";
-import { ModuleType, TemplateStatus, TokenType } from "@framework/types";
+import { IAccessControl, ModuleType, TemplateStatus, TokenType } from "@framework/types";
 
 import { TemplateSearchForm } from "../../../../../components/forms/template-search";
 import { TemplateMintButton } from "../../../../../components/buttons";
 import { CollectionTemplateEditDialog } from "./edit";
+import { useCheckPermissions } from "../../../../../shared";
 
 export const CollectionTemplate: FC = () => {
   const {
@@ -71,81 +80,92 @@ export const CollectionTemplate: FC = () => {
           },
   });
 
+  const { checkPermissions } = useCheckPermissions();
+  const { account = "" } = useWeb3React();
+
   return (
-    <Grid>
-      <Breadcrumbs path={["dashboard", "collection", "collection.template"]} />
+    <ListWrapperProvider<IAccessControl> callback={checkPermissions}>
+      <Grid>
+        <Breadcrumbs path={["dashboard", "collection", "collection.template"]} />
 
-      <PageHeader message="pages.collection.templates">
-        <Button startIcon={<FilterList />} onClick={handleToggleFilters} data-testid="ToggleFilterButton">
-          <FormattedMessage id={`form.buttons.${isFiltersOpen ? "hideFilters" : "showFilters"}`} />
-        </Button>
-        <Button
-          variant="outlined"
-          startIcon={<Add />}
-          onClick={handleCreate}
-          data-testid="CollectionTemplateCreateButton"
-          disabled
-        >
-          <FormattedMessage id="form.buttons.create" />
-        </Button>
-      </PageHeader>
+        <PageHeader message="pages.collection.templates">
+          <Button startIcon={<FilterList />} onClick={handleToggleFilters} data-testid="ToggleFilterButton">
+            <FormattedMessage id={`form.buttons.${isFiltersOpen ? "hideFilters" : "showFilters"}`} />
+          </Button>
+          <Button
+            variant="outlined"
+            startIcon={<Add />}
+            onClick={handleCreate}
+            data-testid="CollectionTemplateCreateButton"
+            disabled
+          >
+            <FormattedMessage id="form.buttons.create" />
+          </Button>
+        </PageHeader>
 
-      <TemplateSearchForm
-        onSubmit={handleSearch}
-        initialValues={search}
-        open={isFiltersOpen}
-        contractType={[TokenType.ERC721]}
-        contractModule={[ModuleType.COLLECTION]}
-        onRefreshPage={handleRefreshPage}
-      />
+        <TemplateSearchForm
+          onSubmit={handleSearch}
+          initialValues={search}
+          open={isFiltersOpen}
+          contractType={[TokenType.ERC721]}
+          contractModule={[ModuleType.COLLECTION]}
+          onRefreshPage={handleRefreshPage}
+        />
 
-      <ProgressOverlay isLoading={isLoading}>
-        <StyledListWrapper count={rows.length} isLoading={isLoading}>
-          {rows.map(template => (
-            <StyledListItem key={template.id} wrap>
-              <ListItemText sx={{ width: 0.6 }}>{template.title}</ListItemText>
-              <ListItemText sx={{ width: { xs: 0.6, md: 0.2 } }}>{template.contract?.title}</ListItemText>
-              <ListActions dataTestId="TemplateActionsMenuButton">
-                <ListAction
-                  onClick={handleEdit(template)}
-                  message="form.buttons.edit"
-                  dataTestId="TemplateEditButton"
-                  icon={Create}
-                />
-                <ListAction
-                  onClick={handleDelete(template)}
-                  message="form.buttons.delete"
-                  dataTestId="TemplateDeleteButton"
-                  icon={Delete}
-                  disabled={template.templateStatus === TemplateStatus.INACTIVE}
-                />
-                <TemplateMintButton template={template} />
-              </ListActions>
-            </StyledListItem>
-          ))}
-        </StyledListWrapper>
-      </ProgressOverlay>
+        <ProgressOverlay isLoading={isLoading}>
+          <StyledListWrapper
+            count={rows.length}
+            isLoading={isLoading}
+            rows={rows}
+            account={account}
+            path={"contract.address"}
+          >
+            {rows.map(template => (
+              <StyledListItem key={template.id} wrap>
+                <ListItemText sx={{ width: 0.6 }}>{template.title}</ListItemText>
+                <ListItemText sx={{ width: { xs: 0.6, md: 0.2 } }}>{template.contract?.title}</ListItemText>
+                <ListActions dataTestId="TemplateActionsMenuButton">
+                  <ListAction
+                    onClick={handleEdit(template)}
+                    message="form.buttons.edit"
+                    dataTestId="TemplateEditButton"
+                    icon={Create}
+                  />
+                  <ListAction
+                    onClick={handleDelete(template)}
+                    message="form.buttons.delete"
+                    dataTestId="TemplateDeleteButton"
+                    icon={Delete}
+                    disabled={template.templateStatus === TemplateStatus.INACTIVE}
+                  />
+                  <TemplateMintButton template={template} />
+                </ListActions>
+              </StyledListItem>
+            ))}
+          </StyledListWrapper>
+        </ProgressOverlay>
 
-      <StyledPagination
-        shape="rounded"
-        page={search.skip / search.take + 1}
-        count={Math.ceil(count / search.take)}
-        onChange={handleChangePage}
-      />
+        <StyledPagination
+          shape="rounded"
+          page={search.skip / search.take + 1}
+          count={Math.ceil(count / search.take)}
+          onChange={handleChangePage}
+        />
 
-      <DeleteDialog
-        onCancel={handleDeleteCancel}
-        onConfirm={handleDeleteConfirm}
-        open={action === CollectionActions.delete}
-        initialValues={selected}
-      />
+        <DeleteDialog
+          onCancel={handleDeleteCancel}
+          onConfirm={handleDeleteConfirm}
+          open={action === CollectionActions.delete}
+          initialValues={selected}
+        />
 
-      <CollectionTemplateEditDialog
-        onCancel={handleEditCancel}
-        onConfirm={handleEditConfirm}
-        open={action === CollectionActions.edit}
-        initialValues={selected}
-      />
-    </Grid>
+        <CollectionTemplateEditDialog
+          onCancel={handleEditCancel}
+          onConfirm={handleEditConfirm}
+          open={action === CollectionActions.edit}
+          initialValues={selected}
+        />
+      </Grid>
+    </ListWrapperProvider>
   );
 };
