1f811b3f11a2501d294b338dfa218acf6bf0323c
pavelnikolaichyk <nikolaj4ik.pasha@yandex.ru>
2024-07-17 11:49:04 +0300

    BF-1 feat: added logic to staking contracts

--- services/admin-ui/src/components/buttons/mechanics/staking/withdraw-penalty/index.tsx
+++ services/admin-ui/src/components/buttons/mechanics/staking/withdraw-penalty/index.tsx
@@ -1,12 +1,12 @@
-import { FC, Fragment, useEffect, useState } from "react";
+import { FC, Fragment, useState } from "react";
 import { PriceChange } from "@mui/icons-material";
-import { useWeb3React } from "@web3-react/core";
 
 import { ListAction, ListActionVariant } from "@framework/styled";
 import type { IContract } from "@framework/types";
+import { AccessControlRoleType } from "@framework/types";
 
 import { StakingWithdrawPenaltyDialog } from "./dialog";
-import { useCheckPermissions } from "../../../../../shared/hooks/use-check-permissions";
+import { useSetButtonPermission } from "../../../../../shared";
 
 export interface IStakingWithdrawPenaltyProps {
   contract: IContract;
@@ -25,11 +25,7 @@ export const StakingWithdrawPenaltyButton: FC<IStakingWithdrawPenaltyProps> = pr
 
   const [isStakingWithdrawPenaltyDialogOpen, setIsStakingWithdrawPenaltyDialogOpen] = useState(false);
 
-  const [hasAccess, setHasAccess] = useState(false);
-
-  const { account = "" } = useWeb3React();
-
-  const { checkPermissions } = useCheckPermissions();
+  const { isButtonAvailable } = useSetButtonPermission(AccessControlRoleType.DEFAULT_ADMIN_ROLE, id);
 
   const handleWithdrawPenalty = (): void => {
     setIsStakingWithdrawPenaltyDialogOpen(true);
@@ -43,21 +39,6 @@ export const StakingWithdrawPenaltyButton: FC<IStakingWithdrawPenaltyProps> = pr
     setIsStakingWithdrawPenaltyDialogOpen(false);
   };
 
-  useEffect(() => {
-    if (account && address) {
-      void checkPermissions({
-        account,
-        address,
-      }).then((json: { hasRole: boolean }) => {
-        setHasAccess(json?.hasRole);
-      });
-    }
-  }, [address, account]);
-
-  // if (contractSecurity !== ContractSecurity.ACCESS_CONTROL) {
-  //   return null;
-  // }
-
   return (
     <Fragment>
       <ListAction
@@ -66,7 +47,7 @@ export const StakingWithdrawPenaltyButton: FC<IStakingWithdrawPenaltyProps> = pr
         message="form.buttons.withdrawPenalty"
         className={className}
         dataTestId="WithdrawPenaltyButton"
-        disabled={disabled || !hasAccess}
+        disabled={disabled || !isButtonAvailable}
         variant={variant}
       />
       <StakingWithdrawPenaltyDialog
--- services/admin-ui/src/pages/mechanics/marketing/staking/contract/index.tsx
+++ services/admin-ui/src/pages/mechanics/marketing/staking/contract/index.tsx
@@ -2,14 +2,22 @@ import { FC } from "react";
 import { FormattedMessage } from "react-intl";
 import { Button, Grid, ListItemText } from "@mui/material";
 import { Create, Delete, FilterList } from "@mui/icons-material";
+import { useWeb3React } from "@web3-react/core";
 
 import { emptyStateString } from "@gemunion/draft-js-utils";
 import { Breadcrumbs, PageHeader, ProgressOverlay } from "@gemunion/mui-page-layout";
 import { DeleteDialog } from "@gemunion/mui-dialog-delete";
 import { CollectionActions, useCollection } from "@gemunion/react-hooks";
-import { ListAction, ListActions, StyledListItem, StyledListWrapper, StyledPagination } from "@framework/styled";
+import {
+  ListAction,
+  ListActions,
+  ListWrapperProvider,
+  StyledListItem,
+  StyledListWrapper,
+  StyledPagination,
+} from "@framework/styled";
 import type { IContract, IContractSearchDto } from "@framework/types";
-import { ContractStatus, StakingContractFeatures } from "@framework/types";
+import { ContractStatus, IAccessControl, StakingContractFeatures } from "@framework/types";
 
 import {
   EthListenerAddButton,
@@ -27,6 +35,7 @@ import {
 } from "../../../../../components/buttons";
 import { ContractSearchForm } from "../../../../../components/forms/contract-search";
 import { StakingEditDialog } from "./edit";
+import { useCheckPermissions } from "../../../../../shared";
 
 export const StakingContracts: FC = () => {
   const {
@@ -67,81 +76,86 @@ export const StakingContracts: FC = () => {
     }),
   });
 
+  const { checkPermissions } = useCheckPermissions();
+  const { account = "" } = useWeb3React();
+
   return (
-    <Grid>
-      <Breadcrumbs path={["dashboard", "staking", "staking.contracts"]} />
+    <ListWrapperProvider<IAccessControl> callback={checkPermissions}>
+      <Grid>
+        <Breadcrumbs path={["dashboard", "staking", "staking.contracts"]} />
 
-      <PageHeader message="pages.staking.title">
-        <Button startIcon={<FilterList />} onClick={handleToggleFilters} data-testid="ToggleFilterButton">
-          <FormattedMessage id={`form.buttons.${isFiltersOpen ? "hideFilters" : "showFilters"}`} />
-        </Button>
-        <StakingDeployButton />
-      </PageHeader>
+        <PageHeader message="pages.staking.title">
+          <Button startIcon={<FilterList />} onClick={handleToggleFilters} data-testid="ToggleFilterButton">
+            <FormattedMessage id={`form.buttons.${isFiltersOpen ? "hideFilters" : "showFilters"}`} />
+          </Button>
+          <StakingDeployButton />
+        </PageHeader>
 
-      <ContractSearchForm
-        onSubmit={handleSearch}
-        initialValues={search}
-        open={isFiltersOpen}
-        contractFeaturesOptions={StakingContractFeatures}
-        onRefreshPage={handleRefreshPage}
-      />
+        <ContractSearchForm
+          onSubmit={handleSearch}
+          initialValues={search}
+          open={isFiltersOpen}
+          contractFeaturesOptions={StakingContractFeatures}
+          onRefreshPage={handleRefreshPage}
+        />
 
-      <ProgressOverlay isLoading={isLoading}>
-        <StyledListWrapper count={rows.length} isLoading={isLoading}>
-          {rows.map(contract => (
-            <StyledListItem key={contract.id}>
-              <ListItemText sx={{ width: 0.6 }}>{contract.title}</ListItemText>
-              <ListActions dataTestId="StakingActionsMenuButton">
-                <ListAction
-                  onClick={handleEdit(contract)}
-                  message="form.buttons.edit"
-                  dataTestId="ContractEditButton"
-                  icon={Create}
-                />
-                <ListAction
-                  onClick={handleDelete(contract)}
-                  message="form.buttons.delete"
-                  dataTestId="ContractDeleteButton"
-                  icon={Delete}
-                  disabled={contract.contractStatus === ContractStatus.INACTIVE}
-                />
-                <GrantRoleButton contract={contract} />
-                <RevokeRoleButton contract={contract} />
-                <RenounceRoleButton contract={contract} />
-                <PauseButton contract={contract} />
-                <UnPauseButton contract={contract} />
-                <AllowanceButton contract={contract} />
-                <TopUpButton contract={contract} />
-                <StakingWithdrawPenaltyButton contract={contract} />
-                <StakingCheckBalanceButton contract={contract} />
-                <EthListenerAddButton contract={contract} />
-                <EthListenerRemoveButton contract={contract} />
-              </ListActions>
-            </StyledListItem>
-          ))}
-        </StyledListWrapper>
-      </ProgressOverlay>
+        <ProgressOverlay isLoading={isLoading}>
+          <StyledListWrapper count={rows.length} isLoading={isLoading} rows={rows} account={account} path={"address"}>
+            {rows.map(contract => (
+              <StyledListItem key={contract.id}>
+                <ListItemText sx={{ width: 0.6 }}>{contract.title}</ListItemText>
+                <ListActions dataTestId="StakingActionsMenuButton">
+                  <ListAction
+                    onClick={handleEdit(contract)}
+                    message="form.buttons.edit"
+                    dataTestId="ContractEditButton"
+                    icon={Create}
+                  />
+                  <ListAction
+                    onClick={handleDelete(contract)}
+                    message="form.buttons.delete"
+                    dataTestId="ContractDeleteButton"
+                    icon={Delete}
+                    disabled={contract.contractStatus === ContractStatus.INACTIVE}
+                  />
+                  <GrantRoleButton contract={contract} />
+                  <RevokeRoleButton contract={contract} />
+                  <RenounceRoleButton contract={contract} />
+                  <PauseButton contract={contract} />
+                  <UnPauseButton contract={contract} />
+                  <AllowanceButton contract={contract} />
+                  <TopUpButton contract={contract} />
+                  <StakingWithdrawPenaltyButton contract={contract} />
+                  <StakingCheckBalanceButton contract={contract} />
+                  <EthListenerAddButton contract={contract} />
+                  <EthListenerRemoveButton contract={contract} />
+                </ListActions>
+              </StyledListItem>
+            ))}
+          </StyledListWrapper>
+        </ProgressOverlay>
 
-      <StyledPagination
-        shape="rounded"
-        page={search.skip / search.take + 1}
-        count={Math.ceil(count / search.take)}
-        onChange={handleChangePage}
-      />
+        <StyledPagination
+          shape="rounded"
+          page={search.skip / search.take + 1}
+          count={Math.ceil(count / search.take)}
+          onChange={handleChangePage}
+        />
 
-      <DeleteDialog
-        onCancel={handleDeleteCancel}
-        onConfirm={handleDeleteConfirm}
-        open={action === CollectionActions.delete}
-        initialValues={selected}
-      />
+        <DeleteDialog
+          onCancel={handleDeleteCancel}
+          onConfirm={handleDeleteConfirm}
+          open={action === CollectionActions.delete}
+          initialValues={selected}
+        />
 
-      <StakingEditDialog
-        onCancel={handleEditCancel}
-        onConfirm={handleEditConfirm}
-        open={action === CollectionActions.edit}
-        initialValues={selected}
-      />
-    </Grid>
+        <StakingEditDialog
+          onCancel={handleEditCancel}
+          onConfirm={handleEditConfirm}
+          open={action === CollectionActions.edit}
+          initialValues={selected}
+        />
+      </Grid>
+    </ListWrapperProvider>
   );
 };
