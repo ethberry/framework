2daf8f561743e9b46cf18a25a2dc3385730d49b1
pavelnikolaichyk <nikolaj4ik.pasha@yandex.ru>
2024-07-17 11:57:55 +0300

    BF-1 feat: added logic to ponzi contracts

--- services/office-ui/src/pages/mechanics/gambling/ponzi/contract/index.tsx
+++ services/office-ui/src/pages/mechanics/gambling/ponzi/contract/index.tsx
@@ -2,15 +2,23 @@ import { FC } from "react";
 import { FormattedMessage } from "react-intl";
 import { Button, Grid, ListItemText } from "@mui/material";
 import { Create, Delete, FilterList } from "@mui/icons-material";
+import { useWeb3React } from "@web3-react/core";
 
 import { emptyStateString } from "@gemunion/draft-js-utils";
 import { Breadcrumbs, PageHeader, ProgressOverlay } from "@gemunion/mui-page-layout";
 import { DeleteDialog } from "@gemunion/mui-dialog-delete";
 import { CollectionActions, useCollection } from "@gemunion/react-hooks";
 import { useUser } from "@gemunion/provider-user";
-import { ListAction, ListActions, StyledListItem, StyledListWrapper, StyledPagination } from "@framework/styled";
+import {
+  ListAction,
+  ListActions,
+  ListWrapperProvider,
+  StyledListItem,
+  StyledListWrapper,
+  StyledPagination,
+} from "@framework/styled";
 import type { IContract, IContractSearchDto, IUser } from "@framework/types";
-import { ContractStatus, PonziContractFeatures } from "@framework/types";
+import { ContractStatus, IAccessControl, PonziContractFeatures } from "@framework/types";
 
 import { ContractSearchForm } from "../../../../../components/forms/contract-search";
 import {
@@ -68,78 +76,83 @@ export const PonziContract: FC = () => {
     }),
   });
 
+  const { checkPermissions } = useCheckPermissions();
+  const { account = "" } = useWeb3React();
+
   return (
-    <Grid>
-      <Breadcrumbs path={["dashboard", "ponzi", "ponzi.contracts"]} />
+    <ListWrapperProvider<IAccessControl> callback={checkPermissions}>
+      <Grid>
+        <Breadcrumbs path={["dashboard", "ponzi", "ponzi.contracts"]} />
 
-      <PageHeader message="pages.ponzi.title">
-        <Button startIcon={<FilterList />} onClick={handleToggleFilters} data-testid="ToggleFilterButton">
-          <FormattedMessage id={`form.buttons.${isFiltersOpen ? "hideFilters" : "showFilters"}`} />
-        </Button>
-        <PonziContractDeployButton />
-      </PageHeader>
+        <PageHeader message="pages.ponzi.title">
+          <Button startIcon={<FilterList />} onClick={handleToggleFilters} data-testid="ToggleFilterButton">
+            <FormattedMessage id={`form.buttons.${isFiltersOpen ? "hideFilters" : "showFilters"}`} />
+          </Button>
+          <PonziContractDeployButton />
+        </PageHeader>
 
-      <ContractSearchForm
-        onSubmit={handleSearch}
-        initialValues={search}
-        open={isFiltersOpen}
-        contractFeaturesOptions={PonziContractFeatures}
-      />
+        <ContractSearchForm
+          onSubmit={handleSearch}
+          initialValues={search}
+          open={isFiltersOpen}
+          contractFeaturesOptions={PonziContractFeatures}
+        />
 
-      <ProgressOverlay isLoading={isLoading}>
-        <StyledListWrapper count={rows.length} isLoading={isLoading}>
-          {rows.map(contract => {
-            return (
-              <StyledListItem key={contract.id}>
-                <ListItemText sx={{ width: 0.6 }}>{contract.title}</ListItemText>
-                <ListActions dataTestId="PonziActionsMenuButton">
-                  <ListAction
-                    onClick={handleEdit(contract)}
-                    message="form.buttons.edit"
-                    dataTestId="ContractEditButton"
-                    icon={Create}
-                  />
-                  <ListAction
-                    onClick={handleDelete(contract)}
-                    message="form.buttons.delete"
-                    dataTestId="ContractDeleteButton"
-                    icon={Delete}
-                  />
-                  <GrantRoleButton contract={contract} />
-                  <RevokeRoleButton contract={contract} />
-                  <RenounceRoleButton contract={contract} />
-                  <AllowanceButton contract={contract} />
-                  <TopUpButton contract={contract} />
-                  <PonziBalanceButton contract={contract} />
-                  <EthListenerAddButton contract={contract} />
-                  <EthListenerRemoveButton contract={contract} />
-                </ListActions>
-              </StyledListItem>
-            );
-          })}
-        </StyledListWrapper>
-      </ProgressOverlay>
+        <ProgressOverlay isLoading={isLoading}>
+          <StyledListWrapper count={rows.length} isLoading={isLoading} rows={rows} account={account} path={"address"}>
+            {rows.map(contract => {
+              return (
+                <StyledListItem key={contract.id}>
+                  <ListItemText sx={{ width: 0.6 }}>{contract.title}</ListItemText>
+                  <ListActions dataTestId="PonziActionsMenuButton">
+                    <ListAction
+                      onClick={handleEdit(contract)}
+                      message="form.buttons.edit"
+                      dataTestId="ContractEditButton"
+                      icon={Create}
+                    />
+                    <ListAction
+                      onClick={handleDelete(contract)}
+                      message="form.buttons.delete"
+                      dataTestId="ContractDeleteButton"
+                      icon={Delete}
+                    />
+                    <GrantRoleButton contract={contract} />
+                    <RevokeRoleButton contract={contract} />
+                    <RenounceRoleButton contract={contract} />
+                    <AllowanceButton contract={contract} />
+                    <TopUpButton contract={contract} />
+                    <PonziBalanceButton contract={contract} />
+                    <EthListenerAddButton contract={contract} />
+                    <EthListenerRemoveButton contract={contract} />
+                  </ListActions>
+                </StyledListItem>
+              );
+            })}
+          </StyledListWrapper>
+        </ProgressOverlay>
 
-      <StyledPagination
-        shape="rounded"
-        page={search.skip / search.take + 1}
-        count={Math.ceil(count / search.take)}
-        onChange={handleChangePage}
-      />
+        <StyledPagination
+          shape="rounded"
+          page={search.skip / search.take + 1}
+          count={Math.ceil(count / search.take)}
+          onChange={handleChangePage}
+        />
 
-      <DeleteDialog
-        onCancel={handleDeleteCancel}
-        onConfirm={handleDeleteConfirm}
-        open={action === CollectionActions.delete}
-        initialValues={selected}
-      />
+        <DeleteDialog
+          onCancel={handleDeleteCancel}
+          onConfirm={handleDeleteConfirm}
+          open={action === CollectionActions.delete}
+          initialValues={selected}
+        />
 
-      <PonziContractEditDialog
-        onCancel={handleEditCancel}
-        onConfirm={handleEditConfirm}
-        open={action === CollectionActions.edit}
-        initialValues={selected}
-      />
-    </Grid>
+        <PonziContractEditDialog
+          onCancel={handleEditCancel}
+          onConfirm={handleEditConfirm}
+          open={action === CollectionActions.edit}
+          initialValues={selected}
+        />
+      </Grid>
+    </ListWrapperProvider>
   );
 };
