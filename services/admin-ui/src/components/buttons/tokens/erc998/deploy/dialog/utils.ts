d1963a261066182021aab908696c5f67a73b526d
TrejGun <trejgun@gmail.com>
2024-07-31 21:19:37 +0100

    fix: allow to deploy erc998 + erc1155 + erc20 on test networks, part 2

--- services/admin-ui/src/components/buttons/tokens/erc998/deploy/dialog/utils.ts
+++ services/admin-ui/src/components/buttons/tokens/erc998/deploy/dialog/utils.ts
@@ -1,14 +1,17 @@
 import { ChainLinkV2SupportedChains, Erc998ContractTemplates, NodeEnv } from "@framework/types";
+import { Networks } from "@gemunion/types-blockchain";
 
 export const isTemplateDisabled = (chainId: number) => {
   const disabledTemplates = [];
 
-  // These templates have too long code
-  disabledTemplates.push(
-    Erc998ContractTemplates.ERC1155OWNER_ERC20OWNER,
-    Erc998ContractTemplates.ERC20OWNER,
-    Erc998ContractTemplates.ERC1155OWNER,
-  );
+  // These templates have too long code so can be deployed only on private network
+  if (!(chainId === Number(Networks.GEMUNION) || chainId === Number(Networks.GEMUNION_BESU))) {
+    disabledTemplates.push(
+      Erc998ContractTemplates.ERC20OWNER,
+      Erc998ContractTemplates.ERC1155OWNER,
+      Erc998ContractTemplates.ERC1155OWNER_ERC20OWNER,
+    );
+  }
 
   if (process.env.NODE_ENV === NodeEnv.production) {
     // Rent (is not tested yet)
