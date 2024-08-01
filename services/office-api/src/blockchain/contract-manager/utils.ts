99bf700401a301a61c1cc609b03265726a66b9b1
TrejGun <trejgun@gmail.com>
2024-08-01 23:22:53 +0100

    fix: use Network enum

--- services/office-api/src/blockchain/contract-manager/utils.ts
+++ services/office-api/src/blockchain/contract-manager/utils.ts
@@ -1,26 +1,29 @@
 import { NotFoundException } from "@nestjs/common";
 
+import { Networks } from "@gemunion/types-blockchain";
 import { ChainLinkV2SupportedChains, NodeEnv } from "@framework/types";
 
 export const chainIdToSuffix = (chainId: bigint | number) => {
   switch (chainId) {
-    case 1:
+    case Networks.ETHEREUM:
       return "Ethereum";
-    case 11155111:
+    case Networks.ETHEREUM_SEPOLIA:
       return "EthereumSepolia";
-    case 56:
+    case Networks.BINANCE:
       return "Binance";
-    case 97:
+    case Networks.BINANCE_TEST:
       return "BinanceTestnet";
-    case 137:
+    case Networks.POLYGON:
       return "Polygon";
-    case 80002:
+    case Networks.POLYGON_AMOY:
       return "PolygonAmoy";
-    case 42161:
+    case Networks.ARBITRUM:
       return "Arbitrum";
-    case 10000:
+    case Networks.ARBITRUM_SEPOLIA:
+      return "ArbitrumSepolia";
+    case Networks.GEMUNION:
       return "Gemunion";
-    case 10001:
+    case Networks.GEMUNION_BESU:
       return "GemunionBesu";
     default:
       return "";
