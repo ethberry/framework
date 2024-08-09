7c16b568eb41d73a965f02f111ffe7bf7ac67d5f
TrejGun <trejgun@gmail.com>
2024-08-09 08:51:13 +0100

    fix: try to fix claim validation, part 2

--- services/office-api/src/blockchain/exchange/asset/dto/bc-asset.ts
+++ services/office-api/src/blockchain/exchange/asset/dto/bc-asset.ts
@@ -1,10 +1,30 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsInt, IsNotIn, Min, ValidateIf } from "class-validator";
+import {
+  IsInt,
+  Validate,
+  Min,
+  ValidateIf,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  ValidationArguments,
+} from "class-validator";
 import { decorate, Mixin } from "ts-mixer";
 
 import { AddressDto, IsBigInt, TokenTypeDto } from "@gemunion/nest-js-validators";
 import type { IBlockChainAssetDto, IBlockChainAssetTemplateDto, IBlockChainAssetTokenDto } from "@framework/types";
 import { TokenType } from "@framework/types";
+
+@ValidatorConstraint({ name: "ForbidEnumValues" })
+export class ForbidEnumValues implements ValidatorConstraintInterface {
+  validate(value: string, { object }: ValidationArguments) {
+    // console.log(JSON.stringify(this, null, "\t"));
+    return !(object as any).forbiddenTypes.includes(value);
+  }
+
+  defaultMessage() {
+    return "badInput";
+  }
+}
 
 export class AmountDto {
   @decorate(
@@ -19,7 +39,7 @@ export class AmountDto {
 export class BlockChainAssetDto extends Mixin(AddressDto, AmountDto, TokenTypeDto) implements IBlockChainAssetDto {
   protected forbiddenTypes: Array<TokenType> = [];
 
-  @IsNotIn(this.forbiddenTypes)
+  @Validate(ForbidEnumValues)
   public tokenType: TokenType;
 
   @decorate(ValidateIf(o => [TokenType.NATIVE, TokenType.ERC20, TokenType.ERC1155].includes(o.tokenType)))
