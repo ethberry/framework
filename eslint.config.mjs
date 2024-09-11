d83e8d502bedb7a3fe3df82b0ab66e296ffd3490
TrejGun <trejgun@gmail.com>
2024-09-11 13:34:28 +0800

    chore: finish eslint migration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,59 +1,101 @@
 import globals from "globals";
 import js from "@eslint/js";
-import stylisticJs from "@stylistic/eslint-plugin-js";
 import stylisticTs from "@stylistic/eslint-plugin-ts";
 import stylisticJsx from "@stylistic/eslint-plugin-jsx";
+import stylisticJs from "@stylistic/eslint-plugin-js";
 import eslintConfigPrettier from "eslint-config-prettier";
 import tsEslint from "typescript-eslint";
 import reactPlugin from "eslint-plugin-react";
+import jestPlugin from "eslint-plugin-jest";
+import nodePlugin from "eslint-plugin-n";
+import pluginPromise from "eslint-plugin-promise";
 
-// DON'T disable any rules! Fix your shit!
+// DON'T ADD ANY RULES!
+// FIX YOUR SHIT!!!
+
 export default [
   {
     ignores: [
       "**/dist",
       "**/static",
       "**/x-license",
-      "framework-contracts",
+      "framework-contracts"
     ]
   },
-  js.configs.recommended,
+
+  {
+    rules: {
+      ...js.configs.recommended.rules,
+      "no-console": [
+        "error",
+        {
+          allow: ["error", "warn", "info"],
+        },
+      ],
+      "no-unused-vars": [
+        "error",
+        {
+          varsIgnorePattern: "^_",
+          argsIgnorePattern: "^_",
+          args: "after-used",
+          vars: "all",
+        },
+      ],
+      "no-void": "off",
+    }
+  },
+
   ...tsEslint.configs.recommendedTypeChecked,
   ...tsEslint.configs.stylisticTypeChecked,
+
   {
     files: ["**/*.{js,mjs,cjs,jsx}"],
     ...tsEslint.configs.disableTypeChecked
   },
+
   {
+    ...pluginPromise.configs["flat/recommended"],
+    rules: {
+      ...pluginPromise.configs["flat/recommended"].rules,
+      "promise/always-return": "off"
+    }
+  },
+
+  {
+    ...nodePlugin.configs["flat/recommended-module"],
+    rules: {
+      ...nodePlugin.configs["flat/recommended-module"].rules,
+      "n/exports-style": ["error", "exports"],
+      // https://github.com/eslint-community/eslint-plugin-n/issues/314
+      "n/no-missing-import": "off"
+    }
+  },
+
+  {
+    ...jestPlugin.configs["flat/recommended"],
+    files: ["**/*.spec.ts"],
+    rules: {
+      ...jestPlugin.configs["flat/recommended"].rules,
+      "jest/no-focused-tests": "error",
+      "jest/valid-expect": "error"
+    }
+  },
+
+  {
+    ignores: ["eslint.config.mjs"],
     languageOptions: {
       parserOptions: {
         project: [
-          "./tsconfig.json",
-          "./packages/*/tsconfig.json",
-          "./services/*/tsconfig.json",
-          "./framework-contracts/contracts/core/tsconfig.json"
+          "./tsconfig.test.json",
+          "./packages/*/tsconfig.test.json",
+          "./services/*/tsconfig.test.json",
+          "./microservices/*/tsconfig.test.json"
         ],
         tsconfigRootDir: import.meta.dirname
       },
-      globals: {
-        ...globals.serviceworker,
-        ...globals.browser
-      }
     }
   },
-  {
-    plugins: {
-      "@stylistic/ts": stylisticTs
-    }
-  },
-  {
-    plugins: {
-      "@stylistic/jsx": stylisticJsx
-    },
-    rules: {
-      "@stylistic/jsx/jsx-indent": ["error", 2]
-    }
-  },
+
   {
     files: ["**/*.{ts,tsx,mtsx}"],
     rules: {
@@ -92,12 +134,82 @@ export default [
       ]
     }
   },
+
   {
+    "settings": {
+      "react": {
+        "version": "detect",
+      },
+    },
     files: ["**/*.{jsx,mjsx,tsx,mtsx}"],
     ...reactPlugin.configs.flat.recommended,
     rules: {
-      "react/react-in-jsx-scope": "off"
+      ...reactPlugin.configs.flat.recommended.rules,
+      "react/react-in-jsx-scope": "off",
+      "react/display-name": "off",
+      "react/prop-types": "off",
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+
+  {
+    plugins: {
+      "@stylistic/js": stylisticJs
+    },
+    rules: {
+      "max-len": [
+        "error",
+        {
+          code: 120,
+          ignoreRegExpLiterals: true,
+          ignoreTemplateLiterals: true,
+          ignorePattern: '^\\s+d="', // ignore path in svg icons
+        },
+      ],
+      "arrow-parens": ["error", "as-needed"],
+      "comma-dangle": ["error", "always-multiline"],
+      indent: [
+        "error",
+        2,
+        {
+          MemberExpression: 1,
+          SwitchCase: 1,
+        },
+      ],
+      "linebreak-style": ["error", "unix"],
+      "multiline-ternary": ["error", "always-multiline"],
+      "no-multiple-empty-lines": [
+        "error",
+        {
+          max: 2,
+          maxEOF: 1,
+        },
+      ],
+      "object-curly-spacing": ["error", "never"],
+      "operator-linebreak": ["error", "before"],
+      quotes: ["error", "double"],
+      semi: ["error", "always"]
+    },
+  },
+
+  {
+    plugins: {
+      "@stylistic/jsx": stylisticJsx
+    },
+    rules: {
+      "@stylistic/jsx/jsx-indent": ["error", 2]
     }
   },
+
+  {
+    plugins: {
+      "@stylistic/ts": stylisticTs
+    }
+  },
+
   eslintConfigPrettier
 ];
--- services/admin-ui/@types/mui.ts
+++
@@ -1,2 +0,0 @@
-// https://github.com/mui/mui-x/issues/12640#issuecomment-2039434173
-import type {} from "@mui/x-date-pickers/AdapterDateFnsV3";
