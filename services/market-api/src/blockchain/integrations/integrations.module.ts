483335fb2e9204b8f8b830229361995040ffa558
TrejGun <trejgun@gmail.com>
2024-09-29 18:05:03 +0400

    fix: remove web3 storage bacause it is dead

--- services/market-api/src/blockchain/integrations/integrations.module.ts
+++ services/market-api/src/blockchain/integrations/integrations.module.ts
@@ -4,9 +4,8 @@ import { ChainLinkModule } from "./chain-link/chain-link.module";
 import { InfuraModule } from "./infura/infura.module";
 import { NftStorageModule } from "./nft-storage/nft-storage.module";
 import { PinataModule } from "./pinata/pinata.module";
-import { Web3StorageModule } from "./web3-storage/web3-storage.module";
 
 @Module({
-  imports: [ChainLinkModule, InfuraModule, NftStorageModule, PinataModule, Web3StorageModule],
+  imports: [ChainLinkModule, InfuraModule, NftStorageModule, PinataModule],
 })
 export class IntegrationsModule {}
